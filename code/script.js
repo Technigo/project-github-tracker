3dae05eaafdbb7179948901b29f948bc9e70db46
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-09-26 21:41:30 +0200

    added assets and played with displaying lists

--- code/script.js
+++ code/script.js
@@ -1,10 +1,16 @@
 const repoURL = 'https://api.github.com/users/KaraHowes/repos'
 
+
 fetch(repoURL)
 .then((response)=>{
 return response.json()
 })
-.then((data)=>{
-    console.log('DATA', data)
+.then((repo)=>{
+    console.log(repo)
+    projects.innerHTML = `The name of your first project was ${repo[0].name}`
+
+   repo.forEach(reponames => {
+    projects.innerHTML += `<p> Project name ${reponames.name}</p>`
+   });
 
 })
