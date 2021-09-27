4879d76ffbd9f81302b6e1a6f16ac665f81788e2
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-09-27 16:18:05 +0200

    add profile and some styling

--- code/script.js
+++ code/script.js
@@ -1,8 +1,10 @@
 // API_URL = 'https://api.github.com';
 const username = 'katiewu1';
 const API_REPOS = `https://api.github.com/users/${username}/repos`;
+const API_USER = `https://api.github.com/users/${username}`;
 
 const projectContainer = document.getElementById('projectContainer');
+const profile = document.getElementById('profile');
 
 const getRepos = () => {
   fetch(API_REPOS)
@@ -40,4 +42,49 @@ const getRepos = () => {
     });
 };
 
+const getUserInfo = () => {
+  fetch(API_USER)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      // display profile image
+      profile.innerHTML += `
+      <div class="profile-info">
+        <table> 
+          <tr>
+            <td>Username:</td>
+            <td>${json.login}</td>
+          </tr>
+          <tr>
+            <td>Name:</td>
+            <td>${json.name}</td>
+          </tr>        
+          <tr>
+            <td>Location:</td>
+            <td>${json.location}</td>
+          </tr>
+        </table>
+        <p>Bio: ${json.bio}</p>
+      </div>
+      <div class="profile-image-icons">
+        <img class="profile-image" src="${json.avatar_url}"/>
+        <div class="contact-icons"> 
+          <a href="https://www.linkedin.com/in/katie-wu-213a82150/" target="blank">
+            <img class="icon" src="./linkedin.png" alt="linkedin icon" width="16px" height="16px"/>
+          </a>
+          <a href="https://github.com/katiewu1/" target="blank">
+            <img class="icon" src="./github.png" alt="github icon" width="16px" height="16px"/>
+          </a>
+          <a href="mailto:katiewu.92@gmail.com">
+            <img class="icon" src="./email.png" alt="email icon" width="16px" height="16px"/>
+          </a>
+        </div>
+      </div>
+
+      `;
+    });
+};
+
 getRepos();
+getUserInfo();
