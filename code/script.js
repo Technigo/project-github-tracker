8219d53ade9d52ab8137d430182929a8b52e3622
hemmahosjessi <you@example.com>
2021-09-27 22:45:37 +0200

    profile

--- code/script.js
+++ code/script.js
@@ -1,6 +1,26 @@
 const USER = 'hemmahosjessi'
 const REPOS_URL = `https://api.github.com/users/${USER}/repos`
 const projectsContainer = document.getElementById('projects')
+const profileContainer = document.getElementById('profile')
+// const PULLS_URL = `https://api.github.com/repos/technigo/${repo.name}/pulls`
+
+
+// ---- PROFILE -----
+
+const getProfile = () => {
+    fetch (`https://api.github.com/users/${USER}`)
+    .then (response => response.json())
+    .then(data => {
+        console.log(data)
+    profileContainer.innerHTML += `
+    <h2>${data.login}</h2>
+    <img src=${data.avatar_url}></img>
+    `
+    })
+}
+
+getProfile()
+
 
 const getRepos = () => {
     fetch(REPOS_URL)
@@ -15,5 +35,33 @@ const getRepos = () => {
 }
 
 getRepos()
+
+
+
+// const getPullRequests = (repos) => {
+//     //Get all the PRs for each project.
+//     repos.forEach(repo => {
+//       fetch(PULLS_URL + repo.name + PULLS)
+//       .then(res => res.json())
+//       .then(data => {
+//         console.log(data)
+//       })
+    
+//     })
+
+// }
+
+//     getPullRequests()
+
+
+// const getPullRequests = () => {
+//     fetch(PULLS_URL)
+//     .then(response => (response.json()))
+//     .then(data => {
+//         console.log(data)
+//     })
+// }
+
+// getPullRequests()
 
 
