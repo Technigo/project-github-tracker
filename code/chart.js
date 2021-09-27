0cb0a328f57bd6cf03760dd608aa57f9da8c5503
Linnea Isebrink <alisebrink@Mac-mini-som-tillhor-Linnea.local>
2021-09-27 16:29:13 +0200

    Changed to a dynamic chart that gets numberfrom API fetch

--- code/chart.js
+++ code/chart.js
@@ -2,23 +2,24 @@
 const ctx = document.getElementById("myChart").getContext("2d");
 
 //"Draw" the chart here 👇
-const config = {
-  type: "doughnut",
-  data: {
-  labels: ["Red", "Blue", "Yellow"],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [300, 50, 100],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
+const drawChart = (amount) => {
+  const config = {
+    type: "doughnut",
+    data: {
+      labels: ["Red", "Blue"],
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: [amount, 20-amount],
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+          ],
+          hoverOffset: 4,
+        },
       ],
-      hoverOffset: 4,
     },
-  ],
-    },
-};
+  };
 
-const myChart = new Chart(ctx, config);
+  const myChart = new Chart(ctx, config);
+};
