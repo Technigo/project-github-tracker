85f739f795ba9014a4194e2d26d252af60eedfb3
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-09-27 11:57:56 +0200

    some changes after today's live session

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,8 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById('chart').getContext('2d');
 
 //"Draw" the chart here 👇
+
+// const config {}
+// const data = {}
+// const myChart = new Chat(ctx, config)
--- code/script.js
+++ code/script.js
@@ -1,37 +1,43 @@
-API_URL = 'https://api.github.com';
+// API_URL = 'https://api.github.com';
+const username = 'katiewu1';
+const API_REPOS = `https://api.github.com/users/${username}/repos`;
 
 const projectContainer = document.getElementById('projectContainer');
 
-const username = 'katiewu1';
-fetch(`https://api.github.com/users/${username}/repos`)
-  .then((response) => {
-    return response.json();
-  })
-  .then((json) => {
-    console.log(json);
+const getRepos = () => {
+  fetch(API_REPOS)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
 
-    // filter out the repos that have been forked
-    // in 'json[index].fork' we can find the boolean (true/false)
-    // we want all the repo with boolean true
-    const filteredForkedRepos = json.filter((repo) => repo.fork);
-    console.log(filteredForkedRepos);
+      // filter out the repos that have been forked
+      // in 'json[index].fork' we can find the boolean (true/false)
+      // we want all the repo with boolean true
+      const filteredForkedRepos = json.filter((repo) => repo.fork);
+      console.log(filteredForkedRepos);
 
-    // filter it one more time with just only repos from Technigo
-    // the repos from Technigo always start with 'project-'
-    const filteredTechnigoRepos = json.filter((repo) =>
-      repo.name.includes('project-')
-    );
-    console.log(filteredTechnigoRepos);
+      // filter it one more time with just only repos from Technigo
+      // the repos from Technigo always start with 'project-'
+      const filteredTechnigoRepos = json.filter(
+        (repo) => repo.name.includes('project-')
+        // can also use repo.name.startsWith('project-')
+      );
+      console.log(filteredTechnigoRepos);
 
-    const namesOfTheRepos = filteredTechnigoRepos.map((repo) => repo.name);
+      const namesOfTheRepos = filteredTechnigoRepos.map((repo) => repo.name);
 
-    console.log(namesOfTheRepos);
+      console.log(namesOfTheRepos);
 
-    namesOfTheRepos.forEach((projectName) => {
-      projectContainer.innerHTML += `
+      namesOfTheRepos.forEach((projectName) => {
+        projectContainer.innerHTML += `
           <div class="project-card">
-              ${projectName} 👆 
+              ${projectName} <div class="project-emoji">👆</div> 
           </div>
           `;
+      });
     });
-  });
+};
+
+getRepos();
