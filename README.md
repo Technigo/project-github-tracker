97cd2ab35b7e054b88997c15c5ac766696a60156
Linnea Isebrink <alisebrink@Mac-mini-som-tillhor-Linnea.local>
2021-09-27 15:43:43 +0200

    First commit. I have fetched data from the GitHub API

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # GitHub Tracker
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This project is a tracker made with Javascript. 
+It is made to fetch data from the GitHub API.
 
 ## The problem
 
@@ -10,4 +9,4 @@ Describe how you approached to problem, and what tools and techniques you used t
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+https://alisebrink-project-github-tracker.netlify.app/
---
+++ code/script.js
@@ -0,0 +1,58 @@
+const USER = "Alisebrink";
+const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
+const USER_URL = `https://api.github.com/users/${USER}`;
+
+const projectsContainer = document.getElementById("projects");
+
+const presentRepoData = () => {
+  fetch(REPOS_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      const technigoRepos = json.filter(
+        (project) => project.fork && project.name.startsWith("project-")
+      );
+
+      technigoRepos.forEach((project) => {
+        let currentProject = project.name;
+        const COMMIT_COUNT_URL = `https://api.github.com/repos/${USER}/${currentProject}/commits`;
+        fetch(COMMIT_COUNT_URL)
+          .then((response) => {
+            return response.json();
+          })
+          .then((json) => {
+            const filteredCommits = json.filter(
+              (project) => project.commit.committer.name === "Linnea Isebrink"
+            );
+            console.log(filteredCommits);
+            projectsContainer.innerHTML += `
+                <h3>${project.name}</h3>
+                <p>${project.default_branch}</p>
+                <a href="${project.html_url}">${project.html_url}</a>
+                <p>This repo has been commited to ${filteredCommits.length} times</p>
+        `;
+          });
+      });
+    });
+};
+
+const presentUserData = () => {
+  fetch(USER_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      projectsContainer.innerHTML = `
+      <img src="${json.avatar_url}"/>
+      <h1>${json.login}</h1>
+      <p>${json.name}</p>
+      <p>This account has a total of ${json.public_repos}</p>)
+      `;
+    });
+};
+
+presentRepoData();
+//presentUserData();
