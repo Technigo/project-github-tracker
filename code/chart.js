c2b11dfdb7b511c6227765f9ac9b87981df978e5
hemmahosjessi <you@example.com>
2021-09-27 19:20:37 +0200

    setting up project after live session

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,30 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+
+
+  const drawChart = (amount)=> {
+    const config = {
+        type: 'doughnut',
+        data: {
+            labels: [
+              'Finished projects',
+              'Projects left',
+            ],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [amount, 20-amount],
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+              ],
+              hoverOffset: 4
+            }]
+          },
+      };
+      
+    const theChart = new Chart (ctx, config)
+  }
+
+
+  
---
+++ code/script.js
@@ -0,0 +1,19 @@
+const USER = 'hemmahosjessi'
+const REPOS_URL = `https://api.github.com/users/${USER}/repos`
+const projectsContainer = document.getElementById('projects')
+
+const getRepos = () => {
+    fetch(REPOS_URL)
+    .then(response => (response.json()))
+    .then(data => {
+        console.log(data)
+        // data.forEach(repo => console.log(repo.name))
+        const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith('project-'))
+        forkedRepos.forEach(repo => projectsContainer.innerHTML += `<h3>${repo.name}</h3>`)
+        drawChart(forkedRepos.length)
+    })
+}
+
+getRepos()
+
+
