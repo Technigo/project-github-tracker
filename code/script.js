5114cde62ee08c06869189fc1bcfbeaa041dda9d
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-09-27 13:03:48 +0200

    filtered forked projects

--- code/script.js
+++ code/script.js
@@ -1,22 +1,29 @@
 const repoURL = 'https://api.github.com/users/KaraHowes/repos'
 
 
+
+
 fetch(repoURL)
 .then((response)=>{
 return response.json()
 })
-.then((forked)=>{
-    console.log(forked)
-    projects.innerHTML = `The name of your first project was ${forked[0].name}`
-    const forkedProjects = forked.filter(()=>)
-
+.then((data)=>{
+    console.log(data)
+    projects.innerHTML = `The name of your first project was ${data[0].name}`
     
+    const forkedProjects = data.filter(repo => repo.fork)
+    
+    forkedProjects.forEach((forkName)=> {
+    projects.innerHTML += `<p> forked projects: ${forkName.name}</p>`
+    
+})
+}
 
-   //repo.forEach(reponames => {
+   //repo.forEach((reponames) => {
     //projects.innerHTML += `<p> Project name: ${reponames.name}</p>`
    //});
 
-})
 
-.catch((error) => console.error(error))
+
+).catch((error) => console.error(error))
 .then(() => console.log('Request finished'));
