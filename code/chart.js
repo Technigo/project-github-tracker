3588418bdcb55ee8b2fc07f2d3cb4aa74a2ca955
Ida Aspen <ida.aspen@outlook.com>
2021-09-27 14:23:58 +0200

    Done fetch all repos and filtered them +  set up chart

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,27 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById("chart").getContext("2d");
 
 //"Draw" the chart here 👇
+// X of 20 done
+
+const drawChart = (amount) => {
+  const config = {
+    type: "doughnut",
+    data: {
+      labels: ["Finished projects", "Projects left"],
+      datasets: [
+        {
+          label: "Repos chart",
+          data: [amount, 20 - amount], //length of repo array ,
+          backgroundColor: [
+            "rgb(255, 99, 132)", //own colors
+            "rgb(54, 162, 235)",
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  };
+
+  const repoChart = new Chart(ctx, config);
+};
