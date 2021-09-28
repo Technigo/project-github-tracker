e30cc5afc5fa4e88de3e808a80885d0f2ec96a12
Gustav Frid <gustav.frid@gmail.com>
2021-09-28 08:57:50 +0200

    added avatar + link to github from avatar

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
 const projects = document.getElementById('projects');
+const userData = document.getElementById('userData');
 
 const username = 'gustavfrid';
+const parentOwner = 'Technigo';
 
 const fetchAllRepos = () => {
 	fetch(`https://api.github.com/users/${username}/repos`)
@@ -8,16 +10,34 @@ const fetchAllRepos = () => {
 			return response.json();
 		})
 		.then((res) => {
-			let forkedRepos = filterRepos(res);
-			forkedRepos.forEach((repo) => {
-				projects.innerHTML += /*html*/ `<p>${repo.name}</p>`;
+			// filter out repos that are forked
+			let filteredRepos = filterForkedRepos(res);
+			// fetch all detailed info for each forked repo, to be able to get and filter the owner
+			filteredRepos.forEach((repo) => {
+				fetch(`https://api.github.com/repos/${username}/${repo.name}`)
+					.then((response) => {
+						return response.json();
+					})
+					.then((res) => {
+						// only pick repos with owner = Technigo
+						if (res.parent.owner.login === parentOwner) {
+							projects.innerHTML += /*html*/ `<p>${repo.name} from ${res.parent.owner.login} </p>`;
+						}
+					})
+					.catch((err) => alert(err));
 			});
-
-			console.log(forkedRepos);
-		});
+			userData.innerHTML = /*html*/ `
+			<p>${username}</p>
+			<a href="${filteredRepos[0].owner.html_url}" target="_blank">
+				<img class="avatar" src="${filteredRepos[0].owner.avatar_url}" alt="avatar" />
+			</a>
+				`;
+			console.log(filteredRepos);
+		})
+		.catch((err) => alert(err));
 };
 
-const filterRepos = (repos) => {
+const filterForkedRepos = (repos) => {
 	return repos.filter((repo) => repo.fork);
 };
 
