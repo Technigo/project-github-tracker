2616438d6737241b86ac3128aabbac601ebc3cdd
Reinis <reinis@Reiniss-MacBook-Pro.local>
2021-09-28 21:15:24 +0300

    Blue requirments wokring

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,20 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById("chart").getContext("2d");
 
 //"Draw" the chart here 👇
+const config = {
+  type: "doughnut",
+  data: {
+    labels: ["Finished", "Upcoming"],
+    datasets: [
+      {
+        label: "Bootcamp projects",
+        data: [5, 15],
+        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
+        hoverOffset: 4,
+      },
+    ],
+  },
+};
+
+const MyChart = new Chart(ctx, config);
--- code/script.js
+++ code/script.js
@@ -6,10 +6,6 @@ const API_KEY = "https://api.github.com/users/Meeteyes/repos";
 let API_KEY_PULL = "";
 let API_KEY_COMMENTS = [];
 
-let repoArray = [];
-let pullsByUser = [];
-let comments = [];
-
 //first function that runs a fetch and gets all the repos of the user
 // it returns an array with only Meeteyes technigo repos
 const getRepos = async () => {
@@ -30,6 +26,7 @@ const fetchPulls = async (key) => {
   // return filteredArray.url;
 };
 
+// This function will get comments, commits, etc
 const fetchDetails = async (key) => {
   const res = await fetch(key);
   const data = await res.json();
@@ -37,7 +34,7 @@ const fetchDetails = async (key) => {
 };
 
 // This is the function that controls the flow of the script.
-// We run other function which fetches data and we display data with this function
+// We run other functions which fetches data and we display data with this function
 const callDataInOrder = async () => {
   //returns array
   const myRepos = await getRepos();
@@ -45,7 +42,7 @@ const callDataInOrder = async () => {
   // this displays the user information, picture and username:
   userInfo.innerHTML = `
   <img src=${myRepos[0].owner.avatar_url} alt="user picture" class="user-image" />
-  <p>${myRepos[0].owner.login}</p>
+  <h2 class="user-name">${myRepos[0].owner.login}</h2>
   `;
 
   myRepos.forEach((repo) => {
@@ -61,6 +58,7 @@ const callDataInOrder = async () => {
         `;
       });
 
+      //Haven't done anything with this yet
       API_KEY_COMMENTS = data.comments_url;
       const myComments = fetchDetails(API_KEY_COMMENTS);
       myComments.then((data) => {
@@ -68,42 +66,15 @@ const callDataInOrder = async () => {
       });
     });
 
-    // allThePulls = allThePulls.filter((item) => item.user.login === "Meeteyes");
-
     projectInfo.innerHTML += `
     <div id=${repo.name} class="repo-card">
-    <a href=${repo.svn_url}>${repo.name}</a>
+    <a href=${repo.svn_url} class="link">${repo.name}</a>
     <p> Default branch: ${repo.default_branch}</p>
-    <p>Latest push: ${repo.pushed_at.slice(0, 10)} at ${repo.pushed_at.slice(
-      11,
-      19
-    )}</p>
+    <p>Latest push: ${repo.pushed_at.slice(0, 10)}</p>
     <p id="numberOfCommits></p>
     <div>
     `;
   });
-
-  // console.log(myRepos);
-  // for (let i = 0; i < myRepos.length; i++) {
-  //   API_KEY_PULL = `https://api.github.com/repos/technigo/${myRepos[i].name}/pulls?per_page=100`;
-  //   const allProjectPulls = await fetchData(API_KEY_PULL);
-  //   const myPull = allProjectPulls.filter(
-  //     (pull) => pull.user.login === "Meeteyes"
-  //   );
-  //   if (myPull.length === 0) {
-  //     continue;
-  //   }
-  //   document.getElementById(`${myPull[0].head.repo.name}`).innerHTML += `
-  //     <div>
-  //       <p>Name of the pull request: ${myPull[0].title}</p>
-  //       <p>Link to the pull: ${myPull[0].url}</p></div>
-  //     `;
-  //   const myCommitsPerProject = await fetchData(myPull[0].commits_url);
-  //   document.getElementById(`${myPull[0].head.repo.name}`).innerHTML += `
-  //     <div><p>Number of commits: ${myCommitsPerProject.length}</p></div>
-  //   `;
-  //   console.log(myCommitsPerProject);
-  // }
 };
 
 callDataInOrder();
