0b6405606ad5d161471dfdd0bef474a6eca3864e
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-09-28 20:28:26 +0200

    chart

--- code/chart.js
+++ code/chart.js
@@ -1,6 +1,20 @@
 //DOM-selector for the canvas 👇
 const ctx = document.getElementById("chart").getContext("2d");
 
-//"Draw" the chart here 👇
+// Chart
+const config = {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Yellow"], //Change the label
+    datasets: [
+      {
+        label: "My First Dataset", //Change the label
+        data: [5, 19 - 5], // Change the data
+        backgroundColor: ["rgb(255, 99, 132)", "rgb(255, 205, 86)"],
+        hoverOffset: 4,
+      },
+    ],
+  },
+};
 
-console.log("chart is heart");
+const reposChart = new Chart(ctx, config);
