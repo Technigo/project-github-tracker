9dd9004d870430b9a847a77413e09f71b9bf4657
Jakob Lindström <Snake@Jakobs-MacBook-Pro-2.local>
2021-09-28 23:41:49 +0200

    added dates and button

--- code/chart.js
+++ code/chart.js
@@ -1,21 +1,42 @@
 //DOM-selector for the canvas 👇
 const ctx = document.getElementById('chart').getContext('2d')
+const ctx2 = document.getElementById('chart2').getContext('2d')
 
 //"Draw" the chart here 👇
+const drawChart = (amount) => {
+  const config = {
+    type: 'doughnut',
+    data: {
+      labels: ['finished', 'not finished'],
+      datasets: [
+        {
+          label: 'My technigo projects',
+          data: [amount, 19 - amount],
+          backgroundColor: ['rgb(255, 99, 132)', '#e6e6e6'],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  }
 
-const config = {
-  type: 'doughnut',
-  data: {
-    labels: ['finished', 'not finished'],
-    datasets: [
-      {
-        label: 'My technigo projects',
-        data: [5, 20 - 5],
-        backgroundColor: ['rgb(255, 99, 132)', '#e6e6e6'],
-        hoverOffset: 4,
-      },
-    ],
-  },
+  const myProjects = new Chart(ctx, config)
 }
 
-const myProjects = new Chart(ctx, config)
+const drawTimeLine = (created) => {
+  const config = {
+    type: 'pie',
+    data: {
+      labels: ['Javascript', 'HTML', 'CSS', 'REACT'],
+      datasets: [
+        {
+          label: 'Created projects',
+          data: [created],
+          backgroundColor: ['#444444', '#e6e6e6'],
+          hoverOffset: 100,
+        },
+      ],
+    },
+  }
+
+  const line = new Chart(ctx2, config)
+}
--- code/script.js
+++ code/script.js
@@ -23,29 +23,57 @@ const getRepos = () => {
       const filtered = data.filter(
         (repo) => repo.fork && repo.name.startsWith('project-')
       )
-      filtered
-        .forEach((repo) => {
-          projects.innerHTML += `<p><a href="${repo.html_url}" target="blank"> my Repo ${repo.name}</a></p>
-        <p>Branch ${repo.default_branch}`
-          getPR(filtered)
-        })
-        .catch(error)
-      projects.innerHTML += `<p> This is a error message beacuse there is an error. </p>`
+      filtered.forEach((repo) => {
+        const pushedDate = new Date(repo.pushed_at).toLocaleDateString(
+          'en-se',
+          {
+            hour: '2-digit',
+            minute: '2-digit',
+            weekday: 'short',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+          }
+        )
+
+        projects.innerHTML += `<div id="repoCards" class="repo-cards">
+        <button id="commits">Show latest commit message</button>
+        <p>Amount of commits..</p><p><a href="${repo.html_url}" target="blank"> My Repo ${repo.name}</a></p>
+        <p>Branch ${repo.default_branch}</p> <p>${pushedDate}</p></div>
+      `
+      })
+      drawTimeLine(filtered.length)
+      drawChart(filtered.length)
+      getPR(filtered)
     })
 }
 const getPR = (repos) => {
   repos.forEach((repo) => {
-    fetch(`https://api.github.com/repos/technigo/${repo.name}/pulls`)
+    fetch(
+      `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`
+    )
       .then((res) => res.json())
       .then((data) => {
-        const myPR = data.filter((pull) => pull.user.login === repo.owner.login)
+        console.log(data.review_comments_url)
+        const myPR = data.find((pull) => pull.user.login === repo.owner.login)
         const myCommits = myPR.commits_url
+        console.log(myCommits)
+        getCommits(myCommits)
       })
   })
 }
 
+const getCommits = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const amountOfCommits = data.length
+      console.log(amountOfCommits)
+      projects.innerHTML += `<div id="repoCards" class="repo-cards"><p>Commits from each pull requests ${data.length}</p></div>`
+    })
+}
+
 getUserData()
 getRepos()
-getPR()
 
 // add eventlistener here
