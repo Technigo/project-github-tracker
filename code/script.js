9c4d440b6d60257b5df9aa9b4ca783dfb3acc3ea
Naushin <malik.naushin@gmail.com>
2021-09-28 16:29:11 +0200

    user name, avatar and github link added

--- code/script.js
+++ code/script.js
@@ -7,34 +7,51 @@ const progressChart = document.getElementById('chart')
 let userName = 'nama0027'
 
 // API variables
-const USER_REPO_URL = `https://api.github.com/users/nama0027/repos`
-
+const USER_REPO_URL = `https://api.github.com/users/${userName}/repos`;
+const USER_URL = `https://api.github.com/users/${userName}`;
 
 
 //-------------functions deceleration----------------------------//
 
 //---- for fetch----//
 
-
-const getRepos = () =>{
-fetch(USER_REPO_URL)
-    .then ((res) => {
-        return res.json();
-
-})
-    .then((data) => {
-        console.log(data);
-        const technigoProjects = data.filter(item => (item.fork && item.name.startsWith('project-') ))
-        technigoProjects.forEach((repo) => {
-            
-            console.log(repo.name);
-            userProjects.innerHTML += `
-            <div class ="repo-card"> 
-                <h2> ${repo.name}</h2>
+const getUser = () => {
+    fetch(USER_URL)
+        .then((res)=>{return res.json()})
+        .then((data) => {
+            console.log('user', data)
+            userProfile.innerHTML = `
+            <div class= "user">
+            <img class= "photo" src= "${data.avatar_url}" alt= "${data.name} avatar" />
+            <h2 class = "name"> ${data.name}</h2>
             </div>
+            <a href="${data.html_url}" class="link-github">
+                <span class="connect-label"> My GitHub Page </span>
+            </a>
             `
-        })
-})
+
+        }) 
 }
 
-getRepos();
+const getRepos = () =>{
+    fetch(USER_REPO_URL)
+        .then ((res) => {
+            return res.json();
+
+    })
+        .then((data) => {
+            console.log(data);
+            const technigoProjects = data.filter(item => (item.fork && item.name.startsWith('project-') ))
+            technigoProjects.forEach((repo) => {
+                console.log(repo.name);
+                userProjects.innerHTML += `
+                <div class ="repo-card"> 
+                    <h3> ${repo.name}</h3>
+                </div>
+                `
+            })
+    })
+}
+
+//getRepos();
+getUser();
