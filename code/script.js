5fc57bf42bfec7d006bc4c25c4be433b07fdc75c
camekman <ekman.camilla@gmail.com>
2021-09-28 15:06:34 +0200

    added function for PR

--- code/script.js
+++ code/script.js
@@ -3,6 +3,7 @@
 const USER = "camekman";
 const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
 const INFO_URL = `https://api.github.com/users/${USER}`;
+//const COMMIT_URL = `https://api.github.com/repos/Technigo/project-news-site/pulls/227/commits`;
 const projectsContainer = document.getElementById("projects");
 const profileContainer = document.getElementById("profile-container");
 const image = document.getElementById("image");
@@ -22,12 +23,12 @@ const getRepos = () => {
           (projectsContainer.innerHTML += `<div class="repositories" id="${
             repo.name
           }"> <h3>${repo.name}</h3> 
-          <h5>${repo.default_branch}</h5> <h5> Recent update ${new Date(
+          <h5>${repo.default_branch}</h5> <h5> latest update: ${new Date(
             repo.updated_at
           ).toLocaleDateString()} </h5></div>`)
       );
       drawChart(forkedRepos.length);
-      //getPullRequests(forkedRepos);
+      getPullRequests(forkedRepos);
     });
 };
 
@@ -54,14 +55,32 @@ const getInfo = () => {
 getInfo();
 
 // pull requests
-// const getPullRequests = (forkedRepos) => {
-//   forkedRepos.forEach((repo) => {
-//     fetch(
-//       "https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100"
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//       });
-//   });
-// };
+const getPullRequests = (forkedRepos) => {
+  forkedRepos.forEach((repo) => {
+    fetch(
+      `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data);
+        const myPulls = data.filter(
+          (pulls) => pulls.user.login === repo.owner.login
+        );
+        //console.log(myPulls);
+        const commitsURL = myPulls[0].commits_url;
+        //console.log(commitsURL);
+        getCommits(commitsURL, repo);
+      });
+  });
+};
+
+const getCommits = (commitsURL, repo) => {
+  fetch(commitsURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById(
+        `${repo.name}`
+      ).innerHTML += ` <h5> number of pull requests: ${data.length} </h5>`;
+    });
+};
