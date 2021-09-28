3c5060765407dd6bf93d80a3bd97b820f6bf74fb
Emelie <Emelie@Jophees-MacBook-Pro.local>
2021-09-28 11:42:09 +0200

    first api fetch and doughnut chart.

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,26 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById("chart").getContext("2d")
 
 //"Draw" the chart here 👇
+
+const drawChart = (amount) => {
+  const config = {
+    type: "doughnut",
+    data: {
+      labels: ["finished projects", "projects left"],
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: [amount, 20 - 4],
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  }
+  const myChart = new Chart(ctx, config)
+}
