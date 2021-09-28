277fe975decad8ee7f613a79b82477dbf4197474
Darya <darya.lapata@gmail.com>
2021-09-28 17:35:16 +0200

    added getCommits function and drawChart function

--- code/chart.js
+++ code/chart.js
@@ -6,29 +6,50 @@ const ctx = document.getElementById('chart').getContext('2d')
 
 
 //"Draw" the chart here 👇
-const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
+// const data = {
+//     labels: [
+//       'Red',
+//       'Blue',
+//       'Yellow'
+//     ],
+//     datasets: [{
+//       label: 'My First Dataset',
+//       data: [400, 50, 100],
+//       backgroundColor: [
+//         'rgb(255, 99, 132)',
+//         'rgb(54, 162, 235)',
+//         'rgb(255, 205, 86)'
+//       ],
+//       hoverOffset: 4
+//     }]
+//   };
+
+// const config = {
+//     type: 'doughnut',
+//     data: data,
+//   };
+
+
+
+//   var myChart = new Chart(ctx, config)
+
+  const drawChart = (number) => {
+    const config = {
+      type: 'doughnut',
+      data: {
+        labels: ["Projects done", "Still to do"],
+        datasets: [
+          {
+            label: "My first Dataset", 
+            data: [number, 20 - number], // divide donut to 2 different parts
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+            ],
+            hoverOffset:4,
+          },
+        ],
+      },
+    }
+    const myChart = new Chart(ctx, config);
   };
-
-const config = {
-    type: 'doughnut',
-    data: data,
-  };
-
-
-
-  var myChart = new Chart(ctx, config)
--- code/script.js
+++ code/script.js
@@ -1,49 +1,80 @@
 // main fetch and function and put the DOM here
-
-const projects = document.getElementById('projects')
-
+const projectsContainer = document.getElementById('projects')
+const headerContainer = document.getElementById('header')
 const ALL_MY_REPOS = `https://api.github.com/users/DALA746/repos`
+
+
 
 const getRepos = () => {
     fetch(ALL_MY_REPOS)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith('project-'))
-            forkedRepos.forEach(repo => projects.innerHTML += `<h3>${repo.name}</h3>`)
-            console.log(forkedRepos)
-            getPullRequests(forkedRepos)
+        .then(res => res.json())
+        .then((json) => { // This json shows all of my repos on GitHub
+            // console.log(json)
+            // filter repos so I can only get tehcnigos repos 
+            const forkedRepos = json.filter(repo => repo.fork && repo.name.startsWith('project-'))
+            drawChart(forkedRepos.length)
+            console.log(forkedRepos.length)
+
+            forkedRepos.forEach((repo) => { 
+                // console.log(repo)
+
+                // need to do convert to right format? 
+                const pushUpdates = repo.pushed_at
+                const defaultBranch = repo.default_branch
+                const gitHubRepo = repo.clone_url 
+
+                // header container 
+                headerContainer.innerHTML = `
+                <div>
+                    <h1>${repo.owner.login}</h1>
+                    <img class="profile-img"src="${repo.owner.avatar_url}" />
+                </div>` 
+
+                // project container 
+                projectsContainer.innerHTML += `
+
+                <h3>Project Name: ${repo.name}</h3>
+                <p>Latest push update: ${pushUpdates}</p>
+                <p>Default branch: ${defaultBranch}</p>
+                <p>GitHub link: ${gitHubRepo}</p>
+                `
+            })
+            // console.log(forkedRepos)
+            getPullRequests(forkedRepos) // passing all my forked repos to this function 
+
         })
 }
 
+// calling getRepos function
 getRepos()
 
-// GETTING MY COMMENTS FUNCTION 
-const getCommits = (commit) => {
-    const COMMITS = commit.commits_url 
-    console.log(COMMITS)
-
-    console.log(commit)
-}
-
-
-
-
 // GETTING ALL MY PULL REQUESTS 
-const getPullRequests = (repos) => { // repos är samma som forkedRepos
-    repos.forEach(repo => {
-        console.log(repo) // alla mina forked pull requests 
-        fetch(`https://api.github.com/repos/technigo/${repo.name}/pulls`) // getting all pullrequest to the specific project
+const getPullRequests = (forkedRepos) => { // repos är samma som forkedRepos
+    forkedRepos.forEach(repo => {
+        // console.log(repo) // alla mina forked pull requests 
+        fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`) // getting all pullrequest to the specific project
             .then(res => res.json())
             .then(data => {
-                console.log(data) // 
-                const myPullRequest = data.filter(pull => pull.user.login === repo.owner.login) // sorting data by comparing my user name 
-                console.log(myPullRequest)
-                getCommits(myPullRequest)
+                const myPullRequests = data.find(pull => pull.user.login === repo.owner.login) // there is a different between filter and find function
+                // console.log(myPullRequests)
+                getCommits(myPullRequests.commits_url) // link 
             })
-
     })
+}
+
+const getCommits = (url) => {
+    console.log(url)
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const numberOfCommits = data.length
+            projectsContainer.innerHTML += `
+            <p>Number of commits ${numberOfCommits}</p>
+
+            `
+            console.log(data.length)
+        })
 }
 
 
