767923c4324c169b18148a47924f0a56f6def291
annathunberg <83536062+annathunberg@users.noreply.github.com>
2021-09-28 23:04:23 +0200

    added chart

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,24 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+
+const config = {
+    type: 'doughnut',
+    data: {
+        labels: [
+          'Finished projects', // lime green
+          'Projects left' // lighter
+        ],
+        datasets: [{
+          label: 'progress chart',
+          data: [6, 20-6],
+          backgroundColor: [
+            '#daff05', // lime green
+            '#f1fabd' // lighter
+          ],
+          hoverOffset: 4
+        }]
+      },
+  };
+
+  const progressChart = new Chart(ctx, config);
--- code/script.js
+++ code/script.js
@@ -5,56 +5,81 @@ const fullName = document.getElementById('full-name')
 const username = document.getElementById('username')
 
 // LIST OF APIS TO FETCH FROM
+const user = "annathunberg"
 const API_MY_REPOS = 'https://api.github.com/users/annathunberg/repos'
 const API_MY_PROFILE = 'https://api.github.com/users/annathunberg'
-const USER = username
-const API_PULLS = 'https://api.github.com/repos/Technigo/${user}/pulls'
-const API_COMMENTS = 'https://api.github.com/repos/Technigo/project-news-site/pulls/247/comments '
-const API_COMMITS = 'https://api.github.com/repos/Technigo/project-news-site/pulls/227/commits'
+const API_PULL_REQUESTS = ''
+const API_COMMITS = ''
+const API_PUSH = ''
+const API_BRANCH = ''
 
 // FETCHES THE PROFILE PIC, NAME AND USERNAME
 fetch(API_MY_PROFILE)
 .then((response) => response.json())
 .then((data) => {
-    console.log("my profile", data)
+    // console.log("my profile", data)
     fullName.innerHTML = `Hi! I'm ${data.name}.`
     username.innerHTML = `Username: ${data.login}`
     avatar.src = data.avatar_url
 })
 
-function addForkedRepoToList(forkedRepo) {
-    list.innerHTML += `<li><a href="${forkedRepo.html_url}">${forkedRepo.name}</a></li>`
-}
+  // FETCHES MY REPOS
+  fetch(API_MY_REPOS)
+  .then((response) => response.json())
+  .then((data) => {
+      //console.log("all the repos", data)
+      const forkedRepos = data.filter(isFork)
+      const sortedForkedRepos = forkedRepos.sort(sortingFunctionFromStackOverflow)
+      sortedForkedRepos.forEach(addForkedRepoToList)
+      
+  })
 
-
-// FETCHES THE PULL REQUESTS
-fetch(API_PULLS)
-.then((response) => response.json())
-.then((data) => {
-    console.log("my pullrequests", data)
-})
-
-
-// the default branch is...
-
-// the last update (push) was .....
-
-// there are ... commits (messages)
-
+ 
+  
+// RETURNS REPOS THAT ARE FORKED AND START WITH PROJECTS
 function isFork(repo) {
     return repo.fork && repo.name.startsWith('project-')
 }
 
+// CREATES THE LIST OF REPOS
+function addForkedRepoToList(forkedRepo) {
+    list.innerHTML += `<li><a href="${forkedRepo.html_url}">${forkedRepo.name}</a></li>`
+}
+
+// SORTS THE REPOS IN CHRONOLOGICAL ORDER BY DATE
 function sortingFunctionFromStackOverflow(a, b) {
     // https://stackoverflow.com/questions/10123953/how-to-sort-an-object-array-by-date-property
     return new Date(a.created_at) - new Date(b.created_at)
   }
 
-fetch(API_MY_REPOS)
+// FETCHES THE PULL REQUESTS
+fetch(API_PULL_REQUESTS)
 .then((response) => response.json())
 .then((data) => {
-    //console.log("all the repos", data)
-    const forkedRepos = data.filter(isFork)
-    const sortedForkedRepos = forkedRepos.sort(sortingFunctionFromStackOverflow)
-    sortedForkedRepos.forEach(addForkedRepoToList)
+    console.log("my pullrequests", data)
 })
+
+// FETCHES THE DEFAULT BRANCH
+fetch(API_BRANCH)
+.then((response) => response.json())
+.then((data) => {
+    console.log("the default branch is..", data)
+    // the default branch is...
+})
+
+// FETCHES THE LATEST PUSH
+fetch(API_PUSH)
+.then((response) => response.json())
+.then((data) => {
+    console.log("the last update (push) was..", data)
+    // the last update (push) was .....
+})
+
+//FETCHES THE COMMIT MESSAGES (latest one or count the number of?)
+fetch(API_COMMITS)
+.then((response) => response.json())
+.then((data) => {
+    console.log("the latest commit was..", data)
+    // there are ... commits (messages)
+})
+
