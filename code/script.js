b8cfa379704e5c93965fe00eda8d220cceed31fa
Amanda Tilly <amandatilly@Amandas-2.lan>
2021-09-28 16:28:22 +0200

    fetched and filtered pull requests

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
 const USER = "amandatilly";
 const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
-const PR_URL = `https://api.github.com/repos/Technigo/${reponame}/pulls`;
+const NEWSSITE_URL =
+  "https://api.github.com/repos/Technigo/project-news-site/pulls/222/commits";
+
 const projectContainer = document.getElementById("projects");
 
 const getRepos = () => {
@@ -8,21 +10,30 @@ const getRepos = () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      //   data.forEach((repo) => console.log(repo.name));
+
       const forkedRepos = data.filter(
         (repo) => repo.fork && repo.name.startsWith("project-")
       );
       forkedRepos.forEach(
         (repo) => (projectContainer.innerHTML += `<h3>${repo.name}</h3>`)
       );
+      getPullRequests(forkedRepos);
     });
 };
 
 const getPullRequests = (repos) => {
   repos.forEach((repo) => {
-    fetch(PR_URL)
+    fetch(
+      `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`
+    )
       .then((res) => res.json())
-      .then((data) => {});
+      .then((data) => {
+        //   console.log(data);
+        const userPullRequests = data.filter(
+          (pull) => pull.user.login === repo.owner.login
+        );
+        console.log(userPullRequests);
+      });
   });
 };
 
