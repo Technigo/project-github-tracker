e96797c86370c8c1f5f6cd126d82481f6a585152
HedvigM <hedvig@mejstedt.se>
2021-09-28 11:23:05 +0200

    fetched the APIS and made a chart

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,28 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+
+
+
+const drawChart = (amount) => {
+    const config = {
+        type: 'doughnut',
+        data: {
+            labels: [
+              'Finished projects',
+              'Projects to go',
+            ],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [amount, 19-amount],
+              backgroundColor: [
+                'rgb(85, 64, 87)',
+                'rgb(138, 189, 144)',
+              ],
+              hoverOffset: 4
+            }]
+        }
+      };
+
+    const chart = new Chart(ctx, config)
+}
--- code/script.js
+++ code/script.js
@@ -1,5 +1,4 @@
-// Endpoint to get all your repos
-// https://api.github.com/users/HedvigM/repos (repos)
+
 
 //Endpoint to get all PRs from a Technigo repo `https://api.github.com/repos/technigo/${reponame}/pulls`
 
@@ -7,48 +6,32 @@
 
 //To get the commits from a PR, you need to get the URL  from the `commits_url` property in the PR json object. It might look something like this:`https://api.github.com/repos/Technigo/project-news-site/pulls/227/commits` and then do a fetch with that url.
 
+
+const projects = document.getElementById("projects")
+const profile = document.getElementById("profile")
+const profileContainer = document.getElementById("profile-container")
+
+//^^^^^^^^^^^^^^^^^^^^^^ REPOS ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
 const repos = 'https://api.github.com/users/HedvigM/repos'
 fetch(repos)
   .then(response => response.json())
-  .then(data => console.log(data[0].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[1].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[2].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[3].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[4].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[5].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[6].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[7].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[8].name));
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data[9].name));
-
-
-  fetch(repos)
-  .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+  //Username and userpic
+  profileContainer.innerHTML+= `
+  <h2>${data[0].owner.login}</h2>
+  <img src="${data[0].owner.avatar_url}" alt="profile picture">  
+  `   
+    const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith('project'))
+      /* console.log(forkedRepos) */
+      forkedRepos.forEach(repo => projects.innerHTML += `
+      <div class="repos">
+        <h3>${repo.name}</h3>
+        <p>${repo.default_branch}</p>
+        <a href="${repo.html_url}">Check the project out!</a>
+        <p>${repo.pushed_at}</p>
+        
+      </div>
+      `)
+      drawChart(forkedRepos.length)
+  })
+  
