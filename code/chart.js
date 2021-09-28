ea5343e78a1b1d0216420bb01751237e910d1355
Sabrina Zancotti <zancotti@gmail.com>
2021-09-28 15:44:13 +0200

    Continued on styling the page in mobile view and finished off all blue step fetch requirements.

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,21 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById("chart").getContext("2d");
 
 //"Draw" the chart here 👇
+
+const config = {
+  type: "pie",
+  data: {
+    labels: ["Finished projects", "Projects left"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [5, 20 - 5],
+        backgroundColor: ["rgb(0,250,154,0.20)", "rgb(250, 250, 250, 0.60)"],
+        hoverOffset: 4,
+      },
+    ],
+  },
+};
+
+const myChart = new Chart(ctx, config);
--- code/script.js
+++ code/script.js
@@ -4,14 +4,16 @@ let filteredRepos = [];
 let repoName, commitsUrl, repoDefaultBranch;
 
 //DOM selectors
+let projectsPullRequest = document.getElementById(`projectsPullRequest`);
 
 // Used APIs
 const myReposApi = `https://api.github.com/users/${username}/repos`;
-const usernameAndPictureApi = `https://api.github.com/users/${username}`;
+const usernamePictureAndBioApi = `https://api.github.com/users/${username}`;
 let pullRequestsApi;
 
+//Function in which we fetch the username, picture and bio from github and change the innerhtml of userInformation to contain these values.
 const getUsernameAndPicture = () => {
-  fetch(usernameAndPictureApi)
+  fetch(usernamePictureAndBioApi)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
@@ -27,6 +29,7 @@ const getUsernameAndPicture = () => {
     });
 };
 
+// Function in which we fetch the repository data and filter them to the ones that includes `project` in the name.
 const getRepos = () => {
   fetch(myReposApi)
     .then((res) => res.json())
@@ -44,80 +47,82 @@ const getRepos = () => {
       );
       filteredRepos = filteredRepos.reverse();
 
+      // ForEach function over filteredRepos which picks up default branch and creation date and html Url.
       filteredRepos.forEach((e) => {
         repoName = e.name;
         repoDefaultBranch = e.default_branch;
         repoCreateDate = e.created_at;
         htmlUrl = e.html_url;
 
+        // Function to make the first letter in the repo name into a uppercase one.
         const repoNameUppercase =
           repoName.charAt(0).toUpperCase() + repoName.slice(1);
 
+        // change the inner html and put the id of the projects__repo-container to the repoName. And make content with the values which we saved.
         projects.innerHTML += `
         <div class="projects__repo-container" id="${repoName}">
           <div class="projects__repo-container__header">
             <div class="projects__repo-container__header__flex-box">
               <div class="projects__repo-container__header__flex-box__repo-name">${repoNameUppercase} ${repoDefaultBranch}</div>
-              <div class="projects__repo-container__header__flex-box__html-url">${htmlUrl}</div>
+              <div class="projects__repo-container__header__flex-box__html-url"><a href="${htmlUrl}" target="_blank">Go to ${repoName}</a></div>
             </div>
-            <div class="projects__repo-container__header__amount-commitmessages">1</div>
+            <div class="projects__repo-container__header__amount-commitmessages" id="${repoName}-amountCommitmessages"></div>
           </div>
         </div>`;
 
         repoDefaultBranch = e.default_branch;
+        // slice to take away the 6 chars not needed to fetch the commitsUrl.
+        commitsUrl = e.commits_url.slice(0, -6);
+        getcommits(repoName);
+
+        // add reponame username and repodefault branch to the api to get into the right api adress depending on which repo.
         pullRequestsApi = `https://api.github.com/repos/technigo/${repoName}/pulls?head=${username}:${repoDefaultBranch}`;
         getPullRequests(repoName);
       });
     });
 };
 
+// function that gets the pullrequests. If data.length > 0 it changes the inner html of "container to change" else it writes "no pull request made"
 const getPullRequests = (repoName) => {
   console.log("pullRequestsApi", pullRequestsApi);
   fetch(pullRequestsApi)
     .then((res) => res.json())
     .then((data) => {
       console.log("pullRequestsApi data", data);
-
-      if (data.length === 1) {
-        commitsUrl = data[0].commits_url;
-        getcommits(repoName);
+      if (data.length > 0) {
+        let prTitle = data[0].title;
+        let prUrl = data[0].url;
+        let containerToChange = document.getElementById(repoName);
+        containerToChange.innerHTML += `<div class="projects__pull-request__content"><div>Pull request link:</div><a href="${prUrl}">${prTitle}</a></div>`;
+      } else {
+        let containerToChange = document.getElementById(repoName);
+        containerToChange.innerHTML += `<div class="projects__pull-request__content">No pull request made</div>`;
       }
     });
 };
 
+// function that fetches the commits and sets the first commit message (which is the last made) until commit message. The last commit message and
+// amount of commits is fetched and printed put by changing the innerHTML.
 const getcommits = (repoName) => {
   fetch(commitsUrl)
     .then((res) => res.json())
     .then((data) => {
       console.log("getcommits data", data);
-      let commitMessage = data[data.length - 1].commit.message;
+      let commitMessage = data[0].commit.message;
       amountOfCommits = data.length;
 
       console.log(commitMessage);
 
-      containerToChange = document.getElementById(repoName);
-      containerToChange.innerHTML += `<div class="projects__repo-container__latest-commit">Latest commit message: ${commitMessage}</div>`;
+      let containerToChange = document.getElementById(repoName);
+      containerToChange.innerHTML += `<div class="projects__repo-container__last-commit">Last commit message: ${commitMessage}</div>`;
+      amountCommitmessages = document.getElementById(
+        `${repoName}-amountCommitmessages`
+      );
+      amountCommitmessages.innerHTML += `${amountOfCommits}`;
     });
 };
 
-// const getPullRequests = (repos) => {
-//   fetch("https://api.github.com/repos/technigo/" + repo.name + PULLS);
-//   repos.forEach((repo) => {});
-// };
-
-/*- A list of all repos that are forked ones from Technigo
-= done
-- Your username and profile picture
-= done
-- Most recent update (push) for each repo
-= done
-- Name of your default branch for each repo
-= done
-- URL to the actual GitHub repo
-=done
-- Number of commit messages for each repo
-- All pull requests
-- A chart of how many projects you've done so far, compared to how many you will do using [Chart.js](https://www.chartjs.org/). [Here](https://www.chartjs.org/docs/latest/getting-started/)'s documentation on how to get started, and in the left menu you can also find [example usage](https://www.chartjs.org/docs/latest/getting-started/usage.html).*/
+// - A chart of how many projects you've done so far, compared to how many you will do using [Chart.js](https://www.chartjs.org/). [Here](https://www.chartjs.org/docs/latest/getting-started/)'s documentation on how to get started, and in the left menu you can also find [example usage](https://www.chartjs.org/docs/latest/getting-started/usage.html).*/
 
 getUsernameAndPicture();
 getRepos();
