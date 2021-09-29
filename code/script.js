004d528188df9fba8c388e41e4c2615c132c6e58
Emelie <Emelie@Jophees-MacBook-Pro.local>
2021-09-29 18:13:32 +0200

    structured the js fetches for the blue requirements and started with the CSS styling

--- code/script.js
+++ code/script.js
@@ -10,9 +10,11 @@ const getUserInfo = () => {
   fetch(USER_INFO_URL)
     .then((response) => response.json())
     .then((data) => {
-      console.log("USER DATA HERE", data)
-      userContainer.html += `<h2>User name: ${data.name}</h2>
-     <img src="https://avatars.githubusercontent.com/u/84201089?v=4" alt="User profile picture">`
+      console.log("USER DATA", data)
+      userContainer.innerHTML += `
+      <img src="https://avatars.githubusercontent.com/u/84201089?v=4" alt="User profile picture"> 
+      <span id="profile-name"> <h2>Emelie Lindblom</h2>
+      <h3>Username: ${data.login}</h3></span>`
     })
 }
 
@@ -20,46 +22,57 @@ const getRepos = () => {
   fetch(REPOS_URL)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
+      console.log("MY REPOS", data)
       let forkedRepos = data.filter(
         (repo) => repo.fork && repo.name.startsWith("project-")
       )
       forkedRepos.forEach(
-        (repo) => (projectsContainer.innerHTML += `<h3>${repo.name}</h3>`)
+        (repo) =>
+          (projectsContainer.innerHTML += `
+        <div class="project-box"> 
+        <a href="${repo.html_url}"> ${repo.name}</a> <p>Default branch: ${
+            repo.default_branch
+          }</p>
+        <p>Most recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+        <p id="commit-${repo.name}">Commits: </p> `)
       )
+
       drawChart(forkedRepos.length)
       getPullRequests(forkedRepos)
     })
 }
 
-//Remember to pass along your filtered repos as an argument when
-//you are calling this function
-
 const getPullRequests = (forkedRepos) => {
   forkedRepos.forEach((repo) => {
-    fetch(
-      `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`
-    )
+    const PULLS_URL = `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`
+
+    fetch(PULLS_URL)
       .then((res) => res.json())
       .then((data) => {
-        const myPr = data.filter((pull) => pull.user.login === repo.owner.login)
-        console.log("MY PRs", myPr)
+        // console.log(`Mother repo for project ${repo.name}`, data)
 
-        fetch(`https://api.github.com/repos/${USER}/${repo.name}/commits`)
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data)
-            const filterCommits = data.filter(
-              (repo = repo.commit.committer.name === "Emelie")
-            )
-          })
+        const myPulls = data.find(
+          (pull) => pull.user.login === repo.owner.login
+        )
+        // console.log(myPulls)
 
-        //2. Now you're able to get the commits for each repo by using
-        // the commits_url as an argument to call another function
-        //3. You can also get the comments for each PR by calling
-        // another function with the review_comments_url as argument
+        getMyCommits(myPulls.commits_url, repo.name)
+
+        // fetch(`https://api.github.com/repos/${USER}/${repo.name}/commits`)
+        //   .then((res) => res.json())
+        //   .then((data) => {
       })
   })
 }
+
+const getMyCommits = (myCommitsUrl, myRepoName) => {
+  fetch(myCommitsUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById(`commit-${myRepoName}`).innerHTML += data.length
+      console.log("my commits!", data)
+    })
+}
+
 getUserInfo()
 getRepos()
