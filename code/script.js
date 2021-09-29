1e9abafd8df4b5d9783cede9087ff30091afa073
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-09-29 14:52:27 +0200

    added commits and pullrequest

--- code/script.js
+++ code/script.js
@@ -22,33 +22,80 @@ getProfile()
 // Repositories
 const getRepos = () => {
     fetch(REPOS_URL)
-    .then(response => response.json())
-    .then(json => {
-        console.log('json:',json)
-        const forkedRepos = json.filter(repo => repo.fork && repo.name.startsWith('project-'))
-        forkedRepos.forEach(repo => projectsContainer.innerHTML += 
+    .then(res => res.json())
+    .then(data => {
+        console.log('json:',data)
+        const forkedRepos = data.filter(
+            (repo) => repo.name.includes('project-') && repo.fork
+            )
+        
+            forkedRepos.forEach((repo) => {
+                projectsContainer.innerHTML += 
             `<div class="project-cards">
             <fieldset>
             <a href="${repo.html_url}"><h3>${repo.name}</h3></a>
              <p>Default branch: ${repo.default_branch}</p>
-             <p>Latest push: ${repo.pushed_at.slice(0, 10)} - ${repo.pushed_at.slice(11,16)}</p>
-            </fieldset>
+             <p>Latest push: ${new Date(repo.pushed_at).toDateString()}</p>
+             <p id="commit-${repo.name}">Commits amount: </p>
+             </fieldset>
              </div>
-             `)
+             `})
+        fetchPullRequestsArray(forkedRepos)     
         drawChart(forkedRepos.length)   
     })
 }
+
+const fetchPullRequestsArray = (allRepositories) => {
+	allRepositories.forEach((repo) => {
+		const PULL_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
+
+		fetch(PULL_URL)
+			.then((res) => res.json())
+			.then((data) => {
+				const myPullRequest = data.find(
+					(pull) => pull.user.login === repo.owner.login
+				);
+
+				// Detect if we have pull request or not.
+				// If yes - call fetchCommits function
+				// If no - inform user that no pull request was yet done
+				if (myPullRequest) {
+					fetchCommits(myPullRequest.commits_url, repo.name);
+				} else {
+					document.getElementById(`commit-${repo.name}`).innerHTML =
+						'No pull request yet done :(';
+				}
+			});
+	});
+};
+
+const fetchCommits = (myCommitsUrl, myRepoName) => {
+	fetch(myCommitsUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			document.getElementById(`commit-${myRepoName}`).innerHTML += data.length;
+		});
+}; 
+
+
 getRepos()
 
-const getCommits = () => {
-    fetch(COMMITS_URL)
-    .then(response => response.json())
-    .then(json => {
-        console.log('Commits:',json.length)
-        const commitsNumber = json.filter(repo => repo.fork && repo.name.startsWith('project-'))
-        commitsNumber.forEach(repo => projectsContainer.innerHTML += 
-            `
-             `)
-    })
-}
-getCommits()
+
+
+
+
+
+
+
+// const getCommits = () => {
+//     fetch(COMMITS_URL)
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log('Commits:',json.length)
+//         const commitsNumber = json.
+//         commitsNumber.forEach(repo => projectsContainer.innerHTML += 
+//             `
+//              `)
+//     })
+// }
+// getCommits()
