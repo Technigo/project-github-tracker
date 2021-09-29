e982366910fa3f8949d29dec9962e19d5962c428
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-09-29 14:54:11 +0200

    fetching pullrequest, commits and review

--- code/chart.js
+++ code/chart.js
@@ -2,19 +2,21 @@
 const ctx = document.getElementById("chart").getContext("2d");
 
 // Chart
-const config = {
-  type: "doughnut",
-  data: {
-    labels: ["Red", "Yellow"], //Change the label
-    datasets: [
-      {
-        label: "My First Dataset", //Change the label
-        data: [5, 19 - 5], // Change the data
-        backgroundColor: ["rgb(255, 99, 132)", "rgb(255, 205, 86)"],
-        hoverOffset: 4,
-      },
-    ],
-  },
-};
+const drawChart = (numberOfProjects) => {
+  const config = {
+    type: "doughnut",
+    data: {
+      labels: ["Red", "Yellow"], //Change the label
+      datasets: [
+        {
+          label: "My First Dataset", //Change the label
+          data: [numberOfProjects, 19 - numberOfProjects], // Change the data
+          backgroundColor: ["rgb(255, 99, 132)", "rgb(255, 205, 86)"],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  };
 
-const reposChart = new Chart(ctx, config);
+  const reposChart = new Chart(ctx, config);
+};
--- code/script.js
+++ code/script.js
@@ -6,10 +6,47 @@ const getRepos = () => {
   fetch(REPOS_URL)
     .then((response) => response.json())
     .then((data) => {
-      console.log("All Repos from the json:", data);
+      //console.log("All Repos from the json:", data);
       const technigoProjects = data.filter((repo) => repo.fork && repo.name.startsWith("project-"));
       technigoProjects.forEach((repo) => (projectsContainer.innerHTML += /*html*/ `<h3>${repo.name}</h3>`));
+      getPullRequests(technigoProjects);
+      drawChart(technigoProjects.length);
     });
 };
 
 getRepos();
+
+const getPullRequests = (repos) => {
+  // Get all the PRs for each project.
+  repos.forEach((repo) => {
+    fetch(`https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`)
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log("Data from pull request:", data);
+
+        const filteredPull = data.find((pull) => pull.user.login === repo.owner.login);
+
+        //console.log("My pull request for:", filteredPull);
+
+        getCommits(filteredPull.commits_url);
+
+        getReview(filteredPull.review_comments_url);
+      });
+  });
+};
+
+const getCommits = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log("My commits:", data);
+    });
+};
+
+const getReview = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log("My review:", data);
+    });
+};
