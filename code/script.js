ad651b7c64813aeefb477351c6d9286a8388d57c
HedvigM <hedvig@mejstedt.se>
2021-09-29 14:52:15 +0200

    Dynamic ID

--- code/script.js
+++ code/script.js
@@ -1,44 +1,101 @@
-
-
-
-//To get the comments from a PR, you need to get the URL from the `review_comments_url` property in the PR json object. It might look something like this: `https://api.github.com/repos/Technigo/project-news-site/pulls/247/comments` and then do a fetch with that url.
-
-//To get the commits from a PR, you need to get the URL  from the `commits_url` property in the PR json object. It might look something like this:`https://api.github.com/repos/Technigo/project-news-site/pulls/227/commits` and then do a fetch with that url.
-
-
 const projects = document.getElementById("projects")
 const profile = document.getElementById("profile")
 const profileContainer = document.getElementById("profile-container")
+const test = document.getElementById("test")
+const userName = 'HedvigM'
+
 
 //^^^^^^^^^^^^^^^^^^^^^^ REPOS ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
-const repos = 'https://api.github.com/users/HedvigM/repos'
+
+const repos = `https://api.github.com/users/${userName}/repos`
 fetch(repos)
   .then(response => response.json())
   .then(data => {
-  //Username and userpic
-  profileContainer.innerHTML+= `
-  <img class="img" src="${data[0].owner.avatar_url}" alt="profile picture"> 
-  <h2 class="profile-name">${data[0].owner.login}</h2>
-  `   
-    const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith('project'))
-      console.log(forkedRepos)
-      forkedRepos.forEach(repo => projects.innerHTML += `
-      <div class="repos">
-        <h3>${repo.name}</h3>
+
+      //Fetching only the forked repos
+      const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith('project'))
+
+    //Username and userpic
+    profileContainer.innerHTML+= `
+      <img class="img" src="${data[0].owner.avatar_url}" alt="profile picture"> 
+      <h2 class="profile-name">${data[0].owner.login}</h2>
+    ` 
+
+    // Repos and pulls
+    forkedRepos.forEach((repo) => projects.innerHTML += `
+      <div class="repos" id="repos">
+        <a href="${repo.html_url}"><h3>${repo.name}</h3></a>
         <p>The default branch is: ${repo.default_branch}</p>
-        <a href="${repo.html_url}">Check the project out!</a>
-        <p>The latest push: ${repo.pushed_at}</p>
-        
+        <p>The latest push: ${repo.pushed_at.substring(0, 10)}</p>
+        <p id="pull-${repo.name}">No pull request is yet made 🤷 </p>
+        <p id="commit-${repo.name}">ooops, there are no commits in this projet</p>
       </div>
-      `)
-      drawChart(forkedRepos.length)
-      //pullRequest(forkedRepos.length)
+    `)
+   
+      // substring(0, 10) - betyder att vi tar bort de tecknen som kommer efter 10st. 
 
-
-      //Trying to get the amount of pullrequests
-      console.log(`https://api.github.com/repos/technigo/${data[5].name}/pulls`)
-
-
+    drawChart(forkedRepos.length)
+    getPullRequests(forkedRepos)
+    getCommitsForPullRequests(forkedRepos)
   })
   
+
+
+
+
+
+//^^^^^^^^^^^^^^^^^ fetching pull requests ^^^^^^^^^^^^^^^^^^^^^^^^^^//
+const getPullRequests = (repos) => {
+  //Get all the PRs for each project.
+  repos.forEach(repo => {
+    fetch('https://api.github.com/repos/technigo/' + repo.name + '/pulls?per_page=100')
+    .then(res => res.json())
+    .then(fetchedPulls => {
+      const hedvigsPulls = fetchedPulls.filter(fetchedPull => fetchedPull.user.login === repo.owner.login) 
+          
+         document.getElementById(`pull-${repo.name}`).innerHTML =
+        `Pull request was made ${hedvigsPulls[0].created_at.substring(0, 10)}`;
+        //allt som skrivs inom parentesen efter get ElementById är en del av   ID:t.
+
+      getCommitsForPullRequests(hedvigsPulls)
+      
+    })
+  })
+}
+
+
+
+const getCommitsForPullRequests = (pullRequests, hedvigsPulls) => {
+  pullRequests.forEach(pullRequest => {
+    fetch(pullRequest.commits_url)
+    .then(res => res.json())
+    .then(fetchedCommits => {
+     /*  console.log('FETCHED COMMITS', fetchedCommits)
+      document.getElementById(`commit-${repo.name}`).innerHTML =
+      `Number of commits in this project: ${fetchedCommits.length}`; */ 
+      projects.innerHTML += `
+      <p>Number of commits in this project: ${fetchedCommits.length}</p>
+      `
+      
+    })
+  })
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
