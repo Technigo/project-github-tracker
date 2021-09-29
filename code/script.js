0f9ffa7f6a899e735768292ce8ffd6508f0717b7
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-09-29 13:49:14 +0200

    create function to fetch commit-data

--- code/script.js
+++ code/script.js
@@ -1,23 +1,43 @@
-const API_PROFILE = 'https://api.github.com/users/sofiawillebrand'
-const API_ALL_REPOS = 'https://api.github.com/users/sofiawillebrand/repos'
+let USER = 'sofiawillebrand'
+let PROJECT = 'project-weather-app'
+let API_PROFILE = `https://api.github.com/users/${USER}`
+let API_ALL_REPOS = `https://api.github.com/users/${USER}/repos`
+let REPO_API = `https://api.github.com/repos/${USER}/${PROJECT}/commits`
 let projectContainer = document.getElementById('project-container')
 let profileContainer = document.getElementById('profile-container')
+let commitMessage
 
-const getRepos = () => {
-fetch(API_ALL_REPOS)
-.then(res => res.json()) 
-.then(repoData => {
-  console.log('Repodata: ', repoData)
-  //try using a filter instead
-  repoData.forEach(project => {
-    if(project.fork && project.name.startsWith('project-')){
-      projectContainer.innerHTML += /*html*/ `
-        <h3>${project.name}</h3>
-      `
-    }
-  });
-})}
-getRepos()
+// const getCommits = (REPO_API) => {
+//   fetch(REPO_API)
+//   .then(res => res.json()) 
+//   .then(commitData => {
+//     console.log('här borde det vara commit', commitData[0].commit.message)
+//     commitMessage = commitData[0].commit.message
+//     projectContainer.innerHTML += /*html*/ `
+//     <h2>There have been ${commitData.length} commits and the latest commit was ${commitMessage}</h2>
+//     `
+//   })
+// }
+
+// const getRepos = () => {
+//   console.log('working')
+//   fetch(API_ALL_REPOS)
+//   .then(res => res.json()) 
+//   .then(reposData => {
+//     console.log('Repodata: ', reposData)
+//     const technigoRepos = reposData.filter((repo) => repo.fork && repo.name.startsWith('project-'))
+//     technigoRepos.forEach(project => {
+//       projectContainer.innerHTML += /*html*/ `
+//         <div>
+//           <a href="${project.html_url}">${project.name} with default branch ${project.default_branch}</a>
+//           <p>Recent push: ${new Date(project.pushed_at).toDateString()}</p>
+//         </div>
+//       `
+//       let REPO_API = `https://api.github.com/repos/${USER}/${project.name}/commits`
+//       getCommits(REPO_API)
+//     })
+//   })
+// }
 
 // const getProfile = () => {
 //   fetch(API_PROFILE)
@@ -30,4 +50,10 @@ getRepos()
 //     `
 //     });
 // }
+
+// getRepos()
 // getProfile()
+
+// create dynamic ID from for eaxmple the name of the project 
+// avoid using global variables
+// use info about reponame to get element from dynamic id and update innerhtml
