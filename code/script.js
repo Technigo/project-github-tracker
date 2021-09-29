bd80617c76adc9866c695b77f61320bcf06eae64
Sabrina Zancotti <zancotti@gmail.com>
2021-09-29 23:27:46 +0200

    added a sort after coding language, redid the code to split fetch and render up

--- code/script.js
+++ code/script.js
@@ -1,7 +1,8 @@
 //Global variables
 let username = "Zancotti";
 let filteredRepos = [];
-let repoName, commitsUrl, repoDefaultBranch;
+let global__UserData, global__ReposData;
+let global_ListCommitData = [];
 
 //DOM selectors
 let projectsPullRequest = document.getElementById(`projectsPullRequest`);
@@ -9,7 +10,6 @@ let projectsPullRequest = document.getElementById(`projectsPullRequest`);
 // Used APIs
 const myReposApi = `https://api.github.com/users/${username}/repos`;
 const usernamePictureAndBioApi = `https://api.github.com/users/${username}`;
-let pullRequestsApi;
 
 // Functions
 const toggleMenu = (menuName) => {
@@ -20,26 +20,58 @@ const toggleMenu = (menuName) => {
   );
 };
 
+const filter = (type) => {
+  console.log(type);
+  filteredRepos = [];
+
+  if (type === `js`) {
+    const projects = document.getElementsByClassName(`projects__HTML`);
+
+    Array.from(projects).forEach((project) => {
+      project.style.display = "none";
+    });
+  } else if (type === `html`) {
+    const projects = document.getElementsByClassName(`projects__JavaScript`);
+
+    Array.from(projects).forEach((project) => {
+      project.style.display = "none";
+    });
+  } else {
+    const projects = document.getElementsByClassName(
+      `projects__repo-container`
+    );
+    Array.from(projects).forEach((project) => {
+      project.style.display = "flex";
+    });
+  }
+};
+
 const toCapitalLetter = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-//Function in which we fetch the username, picture and bio from github and change the innerhtml of userInformation to contain these values.
+//Function in which I fetch data from the json and save the data from the json into a global variable.
 const getUsernameAndPicture = () => {
   fetch(usernamePictureAndBioApi)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      const pictureOfUser = data.avatar_url;
-      const userBio = data.bio;
+      global__UserData = data;
+    })
+    .then(() => renderUsernameAndPicture());
+};
 
-      userInformation.innerHTML += `
-        <img class="user-information__picture" src="${pictureOfUser}" alt="Picture of gitHub-user"/>
-        <div class="user-information__username-bio-container">
-          <div class="user-information__username-bio-container__username">${username}</div>
-          <div class="user-information__username-bio-container__bio">${userBio}</div>
-        </div>`;
-    });
+// Function in which i render the information based on the data that i saved to the global variable.
+const renderUsernameAndPicture = () => {
+  const pictureOfUser = global__UserData.avatar_url;
+  const userBio = global__UserData.bio;
+
+  userInformation.innerHTML += `
+    <img class="user-information__picture" src="${pictureOfUser}" alt="Picture of gitHub-user"/>
+    <div class="user-information__username-bio-container">
+      <div class="user-information__username-bio-container__username">${username}</div>
+      <div class="user-information__username-bio-container__bio">${userBio}</div>
+    </div>`;
 };
 
 // Function in which we fetch the repository data and filter them to the ones that includes `project` in the name.
@@ -48,117 +80,135 @@ const getRepos = () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      data.forEach((e) => {
-        if (e.fork === true && e.name.includes(`project`)) {
-          filteredRepos.push(e);
-        }
-      });
 
-      // sort the filtered repoList after created dates and then reverse it.
-      filteredRepos.sort(
-        (a, b) => new Date(b.created_at) - new Date(a.created_at)
-      );
-      filteredRepos = filteredRepos.reverse();
-
-      // ForEach function over filteredRepos which picks up default branch and creation date and html Url.
-      filteredRepos.forEach((e) => {
-        repoName = e.name;
-        repoDefaultBranch = e.default_branch;
-        repoCreateDate = e.created_at;
-        htmlUrl = e.html_url;
-
-        // Function to make the first letter in the repo name into a uppercase one.
-        const repoNameUppercase =
-          repoName.charAt(0).toUpperCase() + repoName.slice(1);
-
-        // change the inner html and put the id of the projects__repo-container to the repoName. And make content with the values which we saved.
-        projects.innerHTML += `
-        <div class="projects__repo-container" id="${repoName}">
-          <div class="projects__repo-container__header">
-            <div class="projects__repo-container__header__flex-box">
-              <div class="projects__repo-container__header__flex-box__repo-name">${repoNameUppercase} ${repoDefaultBranch}</div>
-              <div class="projects__repo-container__header__flex-box__html-url"><a href="${htmlUrl}" target="_blank">Go to ${repoName}</a></div>
-            </div>
-            <div class="projects__repo-container__header__amount-commitmessages" id="${repoName}-amountCommitmessages"></div>
-          </div>
-          <div class="projects__commit-pulls-container" id="${repoName}-commit-pulls-container"></div>
-        </div>`;
-
-        repoDefaultBranch = e.default_branch;
-        // slice to take away the 6 chars not needed to fetch the commitsUrl.
-        commitsUrl = e.commits_url.slice(0, -6);
-        getcommits(repoName);
-
-        // add reponame username and repodefault branch to the api to get into the right api adress depending on which repo.
-        pullRequestsApi = `https://api.github.com/repos/technigo/${repoName}/pulls?head=${username}:${repoDefaultBranch}`;
-        getPullRequests(repoName);
-      });
-    });
+      global__ReposData = data;
+    })
+    .then(() => renderRepos())
+    .then(() => getCommits())
+    .then(() => getPullRequests());
 };
 
-// function that gets the pullrequests. If data.length > 0 it changes the inner html of "container to change" else it writes "no pull request made"
-const getPullRequests = (repoName) => {
-  console.log("pullRequestsApi", pullRequestsApi);
-  fetch(pullRequestsApi)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("pullRequestsApi data", data);
-      if (data.length > 0) {
-        let prTitle = data[0].title;
-        let prUrl = data[0].url;
-        let containerToChange = document.getElementById(
-          `${repoName}-commit-pulls-container`
-        );
-        containerToChange.innerHTML += `<div class="projects__commit-pulls-container__pull-request"><div>Pull request:</div><a href="${prUrl}">${prTitle}</a></div>`;
-      } else {
-        let containerToChange = document.getElementById(
-          `${repoName}-commit-pulls-container`
-        );
-        containerToChange.innerHTML += `<div class="projects__commit-pulls-container__pull-request">No pull request made</div>`;
-      }
-    });
+const renderRepos = () => {
+  global__ReposData.forEach((e) => {
+    if (e.fork === true && e.name.includes(`project`)) {
+      filteredRepos.push(e);
+    }
+  });
+
+  // sort the filtered repoList after created dates and then reverse it.
+  filteredRepos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+  filteredRepos = filteredRepos.reverse();
+
+  // ForEach function over filteredRepos which picks up default branch and creation date and html Url.
+  filteredRepos.forEach((e) => {
+    const repoName = e.name;
+    const repoNameDisplay = toCapitalLetter(repoName);
+    const repoDefaultBranch = e.default_branch;
+    const htmlUrl = e.html_url;
+    let repoLanguage = e.language;
+
+    if (repoLanguage === null) {
+      repoLanguage = "";
+    }
+
+    // change the inner html and put the id of the projects__repo-container to the repoName. And make content with the values which we saved.
+    projects.innerHTML += `
+    <div class="projects__repo-container projects__${repoLanguage}" id="${repoName}">
+      <div class="projects__repo-container__header">
+        <div class="projects__repo-container__header__flex-box">
+          <div class="projects__repo-container__header__flex-box__repo-name">${repoNameDisplay} ${repoDefaultBranch}</div>
+          <div class="projects__repo-container__header__flex-box__html-url"><a href="${htmlUrl}" target="_blank">Go to ${repoName}</a></div>
+          <div class="projects__repo-container__header__flex-box__language">Programming language: ${repoLanguage}</div> 
+          </div>
+        <div class="projects__repo-container__header__amount-commitmessages" id="${repoName}-amountCommitmessages"></div>
+      </div>
+      <div class="projects__commit-pulls-container" id="${repoName}-commit-pulls-container"></div>
+    </div>`;
+  });
 };
 
 // function that fetches the commits and sets the first commit message (which is the last made) until commit message. The last commit message and
 // amount of commits is fetched and printed put by changing the innerHTML.
-const getcommits = (repoName) => {
-  fetch(commitsUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("getcommits data", data);
-      console.log("getcommits username", username);
-      let commitMessage = data[0].commit.message;
-      amountOfCommits = data.filter(
-        (element) => element.author && element.author.login === username
-      ).length;
+const getCommits = () => {
+  filteredRepos.forEach((repo) => {
+    const repoName = repo.name;
+    const commitsUrl = repo.commits_url.slice(0, -6);
 
-      commitMessage = toCapitalLetter(commitMessage);
+    fetch(commitsUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("getcommits data", data);
+        global_ListCommitData.push(data);
+        return data;
+      })
+      .then((data) => renderCommits(data, repoName));
+  });
+};
 
-      let containerToChange = document.getElementById(
-        `${repoName}-commit-pulls-container`
-      );
+const renderCommits = (data, repoName) => {
+  const commitMessage = toCapitalLetter(data[0].commit.message);
+  const amountOfCommits = data.filter(
+    (element) => element.author && element.author.login === username
+  ).length;
 
-      containerToChange.innerHTML += `
+  let containerToChange = document.getElementById(
+    `${repoName}-commit-pulls-container`
+  );
+
+  containerToChange.innerHTML += `
       <div class="projects__commit-pulls-container__commits-accordion" onclick="toggleMenu('${repoName}')">${commitMessage}
         <ul class="projects__commit-pulls-container__commits-accordion__menu" id="${repoName}-menu"></ul>
       </div>`;
-      amountCommitmessages = document.getElementById(
-        `${repoName}-amountCommitmessages`
-      );
-      amountCommitmessages.innerHTML += `${amountOfCommits}`;
 
-      let menu = document.getElementById(`${repoName}-menu`);
-      data
-        .slice(1)
-        .filter(
-          (element) => element.author && element.author.login === username
-        )
-        .forEach((element) => {
-          menu.innerHTML += `<li class="option">${toCapitalLetter(
-            element.commit.message
-          )}</li>`;
-        });
+  const amountCommitmessages = document.getElementById(
+    `${repoName}-amountCommitmessages`
+  );
+  amountCommitmessages.innerHTML += `${amountOfCommits}`;
+
+  let menu = document.getElementById(`${repoName}-menu`);
+  data
+    .slice(1)
+    .filter((element) => element.author && element.author.login === username)
+    .forEach((element) => {
+      menu.innerHTML += `<li class="option">${toCapitalLetter(
+        element.commit.message
+      )}</li>`;
     });
+};
+
+// function that gets the pullrequests. If data.length > 0 it changes the inner html of "container to change" else it writes "no pull request made"
+const getPullRequests = () => {
+  filteredRepos.forEach((repo) => {
+    const repoDefaultBranch = repo.default_branch;
+    const repoName = repo.name;
+
+    // add reponame username and repodefault branch to the api to get into the right api adress depending on which repo.
+    const pullRequestsApi = `https://api.github.com/repos/technigo/${repoName}/pulls?head=${username}:${repoDefaultBranch}`;
+
+    fetch(pullRequestsApi)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("pullRequestsApi data", data);
+        return data;
+      })
+      .then((data) => renderPullRequest(data, repoName));
+  });
+};
+
+const renderPullRequest = (data, repoName) => {
+  if (data.length > 0) {
+    const prTitle = data[0].title;
+    const prUrl = data[0].url;
+    let containerToChange = document.getElementById(
+      `${repoName}-commit-pulls-container`
+    );
+    containerToChange.innerHTML += `<div class="projects__commit-pulls-container__pull-request"><div>Pull request:</div><a href="${prUrl}">${prTitle}</a></div>`;
+  } else {
+    let containerToChange = document.getElementById(
+      `${repoName}-commit-pulls-container`
+    );
+    containerToChange.innerHTML += `<div class="projects__commit-pulls-container__pull-request">No pull request made</div>`;
+  }
 };
 
 getUsernameAndPicture();
