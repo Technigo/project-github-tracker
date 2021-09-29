dcea7c1a515abaac9a8dfc70695b53ae84d2bbb0
fatigr26 <fatigr2692@gmail.com>
2021-09-29 17:10:57 +0200

    added getPullRequest function

--- code/script.js
+++ code/script.js
@@ -1,37 +1,57 @@
-//GLOBAL VARIABLES
-const userName = "Fatima-Gamero-Romero";
-//APIs
-const API_GITHUB = `https://api.github.com/users/${userName}/repos`; //Here we use backticks instead of quotes because we are injecting a variable
 //DOM SELECTORS
 const projectsContainer = document.getElementById("projects");
+
+const userName = "Fatima-Gamero-Romero";
+const API_GITHUB = `https://api.github.com/users/${userName}/repos`; //Here we use backticks instead of quotes because we are injecting a variable
 
 const getRepos = () => {
   fetch(API_GITHUB)
     .then((resp) => resp.json())
     .then((data) => {
-      // console.log(data)
       const forkedRepos = data.filter((repo) => repo.fork && repo.name.startsWith("project-")); // Filtering the repos that are forked from Technigo
-    //   console.log(forkedRepos[1].name);
-      forkedRepos.forEach((repo) => (projectsContainer.innerHTML += `<h3>${repo.name}</h3>`)); // Inserting the repos in the html file
+      console.log(forkedRepos)
+      forkedRepos.forEach((repo) => {
+      projectsContainer.innerHTML += `
+      <div>
+        <a href= "${repo.html_url}">${repo.name} with default branch ${repo.default_branch}</a>
+        <p>Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+      </div>
+      `;
+    }); 
       drawChart(forkedRepos.length); // Function being called with the length of the repos which is needed in the chart
-
-forkedRepos.forEach((repo)=> {
-
-      const API_PR = `https://api.github.com/repos/technigo/${repo.name}/pulls`
-      fetch(API_PR)
-      .then((resp) => resp.json())
-      .then((data)=> {
-          console.log(data)
-      })
+      getPullRequests(forkedRepos); // Function being called
     })
-
-
-
-    });
 };
-getRepos();
+/////////////////////////////////////////////////////////////////////////////
 
-// repos.forEach(repo => {
-//     fetch('https://api.github.com/repos/technigo/' + repo.name + PULLS)
-//     .then(res => res.json())
-//     .then(data => {
+const getPullRequests = (reposParameter) => {
+  reposParameter.forEach((repo) => {
+    // console.log(repo.owner.login)
+    const API_PR = `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`;
+    fetch(API_PR)
+      .then((resp) => resp.json())
+      .then((data) => {
+        // console.log(data)
+        // console.log(data[0]); ???
+        const myPullRequest = data.filter((pull) => pull.user.login === repo.owner.login) //1. It gets only the PR that I made by comparing pull.user.login with repo.owner.login
+        // console.log(myPullRequest)
+        //2. Now you're able to get the commits for each repo by usingthe commits_url as an argument to call another function
+      // console.log(myPullRequest)
+      const url = myPullRequest[0].commits_url
+      
+      getCommits(url) // Function being called which prints all my commits
+
+      });
+  });
+};
+
+
+
+const getCommits = (commitsParameter) => {
+  projectsContainer.innerHTML += `<h2>${commitsParameter}</h2>`
+
+}
+
+
+
+getRepos();
