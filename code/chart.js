f5016bcc05131da9bbe2e9d45093e05483dbc80c
Gustav Frid <gustav.frid@gmail.com>
2021-09-30 14:42:58 +0200

    added fetch collaborators

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,25 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById('chart').getContext('2d');
 
 //"Draw" the chart here 👇
+const drawPieChart = (pullReqData) => {
+	const { total, done } = pullReqData;
+	const data = {
+		labels: ['Done', 'Soon'],
+		datasets: [
+			{
+				label: 'Technigo project progress',
+				data: [done, total - done],
+				backgroundColor: ['rgb(255, 99, 132)', 'rgb(255, 205, 86)'],
+				hoverOffset: 4,
+			},
+		],
+	};
+
+	const config = {
+		type: 'pie',
+		data: data,
+	};
+
+	new Chart(ctx, config);
+};
--- code/script.js
+++ code/script.js
@@ -6,15 +6,24 @@ const PARENT_OWNER = 'Technigo';
 const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
 const USER_URL = `https://api.github.com/users/${USER}`;
 
+const options = {
+	method: 'GET',
+	headers: { Authorization: `token ${GITHUB_TOKEN}` },
+};
+const pullReqData = {
+	total: 19,
+	done: 0,
+};
+
 const fetchAllReposFromUser = () => {
 	// fetch all repos from user
-	fetch(REPOS_URL)
+	fetch(REPOS_URL, options)
 		.then((res) => res.json())
 		.then((allRepos) => {
 			// filter forked repos
 			let filteredRepos = allRepos.filter((repo) => repo.fork);
-			// console.log('filtered repos: ', filteredRepos);
-			filteredRepos.forEach((repo) => {
+			console.log('filtered repos: ', filteredRepos);
+			filteredRepos.slice(0, 2).forEach((repo) => {
 				// fetch all data for each repo use .slice(0, 2) to limit
 				fetchFullRepo(repo);
 			});
@@ -24,38 +33,39 @@ const fetchAllReposFromUser = () => {
 
 const fetchFullRepo = (repo) => {
 	// fetch all data from each repo
-	fetch(`https://api.github.com/repos/${USER}/${repo.name}`)
+	fetch(`https://api.github.com/repos/${USER}/${repo.name}`, options)
 		.then((res) => res.json())
 		.then((fullRepo) => {
 			// only continue with repos that have PARENT_OWNER as parent
 			if (fullRepo.parent.owner.login === PARENT_OWNER) {
 				// put data in html
-				console.log('fullRepo', fullRepo.name);
+				// console.log('fullRepo', fullRepo.name);
 				projects.innerHTML += /*html*/ `
-				<p><a href=${fullRepo.html_url} target="_blank">${fullRepo.name}</a> from ${fullRepo.parent.owner.login} default branch: ${fullRepo.default_branch}</p>
-				<p>updated: ${fullRepo.pushed_at}</p>
+				<p><a href=${fullRepo.html_url} target="_blank">${fullRepo.name}</a> from ${fullRepo.parent.owner.login} default branch: ${
+					fullRepo.default_branch
+				}</p>
+				<p>updated: ${new Date(fullRepo.pushed_at).toDateString()}</p>
 				<p id="commit-${fullRepo.name}">Commits</p>
 				<p id="pull-${fullRepo.name}">Pull request</p>
 				`;
-				// fetchPullRequestsArray(fullRepo);
-				// fetchCollaborators(fullRepo);
 				const COMMITS_URL = `https://api.github.com/repos/${USER}/${fullRepo.name}/commits?per_page=100`;
 				fetchCommits(COMMITS_URL, fullRepo);
+				fetchCollaborators(fullRepo);
 			}
 		})
 		.catch((err) => console.log('fetchFullRepo error:', err));
 };
 
 const fetchCommits = (myCommitsUrl, repo) => {
-	fetch(myCommitsUrl)
+	fetch(myCommitsUrl, options)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log('fetchCommits data: ', repo.name, data);
+			// console.log('fetchCommits data: ', repo.name, data);
 			document.getElementById(`commit-${repo.name}`).innerHTML += ` ${data.length}`;
 			const authors = data.map((commit) => {
 				return commit.author ? commit.author.login : '';
 			});
-			console.log('fetchCommits authors', authors);
+			// console.log('fetchCommits authors', authors);
 			fetchPullRequestsArray(repo, authors);
 		})
 		.catch((err) => console.log('fetchCommits error: ', repo.name, err));
@@ -64,15 +74,17 @@ const fetchCommits = (myCommitsUrl, repo) => {
 const fetchPullRequestsArray = (repo, authors) => {
 	// fetch all pull requests from repo
 	const PULL_URL = `https://api.github.com/repos/${PARENT_OWNER}/${repo.name}/pulls?per_page=100`;
-	fetch(PULL_URL)
+	fetch(PULL_URL, options)
 		.then((res) => res.json())
 		.then((data) => {
 			console.log('fetchPullRequestsArray data: ', data);
 			// only pick pull requests connected to user
 			const myPullReq = data.find((pull) => authors.includes(pull.user.login)); // pull.user.login === repo.owner.login);
-			console.log('myPullReq', repo.name, myPullReq);
+			// console.log('myPullReq', repo.name, myPullReq);
 			if (myPullReq) {
 				document.getElementById(`pull-${repo.name}`).innerHTML = `<a href=${myPullReq.html_url} target="_blank">Pull request</a>`;
+				pullReqData.pullRequests++;
+				drawPieChart(pullReqData);
 			} else {
 				document.getElementById(`pull-${repo.name}`).innerHTML = 'No pull request yet done :(';
 			}
@@ -81,13 +93,24 @@ const fetchPullRequestsArray = (repo, authors) => {
 };
 
 const fetchUser = () => {
-	fetch(USER_URL)
+	fetch(USER_URL, options)
 		.then((res) => res.json())
 		.then((data) => {
 			// console.log(data);
 			userData.innerHTML += `<a href="${data.html_url}" target="_blank"><img class="avatar" src="${data.avatar_url}"></a><p>${data.login}</p>`;
 		})
 		.catch((err) => console.log('fetchCommits error: ', err));
+};
+
+const fetchCollaborators = (repo) => {
+	const COLLABORATOR_URL = `https://api.github.com/repos/${USER}/${repo.name}/collaborators`;
+	fetch(COLLABORATOR_URL, options)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log('fetchCollaborators', data);
+			//	userData.innerHTML += `<a href="${data.html_url}" target="_blank"><img class="avatar" src="${data.avatar_url}"></a><p>${data.login}</p>`;
+		})
+		.catch((err) => console.log('fetchCollaborators error: ', err));
 };
 
 fetchAllReposFromUser();
