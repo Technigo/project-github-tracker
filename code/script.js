7dda0673a46b581c4e9f911497d10cb84b2e65d0
A1eksa <80787783+A1eksa@users.noreply.github.com>
2021-10-01 00:21:09 +0200

    css, fixed pull requests

--- code/script.js
+++ code/script.js
@@ -15,20 +15,19 @@ const getUserPicture = ()=> {
         const url = data.followers_url;
         const button = document.getElementById("Btn")
 
-        button.onclick = ()=>{
-            window.location='data.followers_url'
-            
-        }
+        
     
 
-        userInfo.innerHTML = `
-        <img class="user-info_picture" src="${picture}" alt="Picture of gitHub user"/>
-        <div class="user-info_username">User name: ${username}</div>
-        <div class="user-info_location">Location: ${location}</div>
-        <div class="user-info_bio">${userBio} 🐒</div>
-
-    
+        userInfo.innerHTML += `
+        
+         <img class="user-info_picture" src="${picture}" alt="Picture of gitHub user"/>
+         <div class="user-info_username">User name: ${username}</div>
+         <div class="user-info_location">Location: ${location}</div>
+         <div class="user-info_bio">${userBio} 🐒</div>
         `
+
+    
+        
     })
 }
 getUserPicture()
@@ -42,17 +41,20 @@ const fetchRepo = ()=> {
         const technigoRepo = data.filter(repo => 
             repo.name.includes('project-') && repo.fork
         );
-        console.log(technigoRepo);
+        //console.log(technigoRepo);
         
         technigoRepo.forEach((repo)=> {
             projectsContainer.innerHTML += `
-            <div>PROJECT: ${repo.name}</div>
-            <div>default branch:${repo.default_branch}</div>
-            <div>url:${repo.html_url}</div>
-            <a href="${repo.html_url}" target="_blank">Go to "${repo.name}"</a>
-            <div>most recent push:"${repo.pushed_at}"`
+            
+             <div class="repo-card">
+              <div>PROJECT: ${repo.name}</div>
+              <div>default branch:${repo.default_branch}</div>
+              <a href="${repo.html_url}" target="_blank">Go to "${repo.name}"</a>
+              <div>most recent push:"${repo.pushed_at}"
+            `
         });
         fetchPull(technigoRepo);
+        
         
     });
     
@@ -66,10 +68,10 @@ const fetchPull = (allRepo)=> {
         fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls`)
         .then((res)=> res.json())
         .then((data)=>{
-            console.log(`mother${repo.name}, data`);
+            console.log("aleksa", data);
 
         });
-        fetchPull()
+        
     
 
     });
