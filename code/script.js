1f2a251b2e40e630020e66873f6a38850bafa171
fatigr26 <fatigr2692@gmail.com>
2021-09-30 17:00:08 +0200

    added fetch commits function

--- code/script.js
+++ code/script.js
@@ -9,12 +9,12 @@ const getRepos = () => {
     .then((resp) => resp.json())
     .then((data) => {
       const forkedRepos = data.filter((repo) => repo.fork && repo.name.startsWith("project-")); // Filtering the repos that are forked from Technigo
-      console.log(forkedRepos)
       forkedRepos.forEach((repo) => {
       projectsContainer.innerHTML += `
       <div>
         <a href= "${repo.html_url}">${repo.name} with default branch ${repo.default_branch}</a>
         <p>Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+        <p id= "commit-${repo.name}">Commits amount: </p>
       </div>
       `;
     }); 
@@ -24,34 +24,27 @@ const getRepos = () => {
 };
 /////////////////////////////////////////////////////////////////////////////
 
-const getPullRequests = (reposParameter) => {
-  reposParameter.forEach((repo) => {
+const getPullRequests = (allRepositories) => {
+  allRepositories.forEach((repo) => {
     // console.log(repo.owner.login)
     const API_PR = `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`;
     fetch(API_PR)
       .then((resp) => resp.json())
       .then((data) => {
-        // console.log(data)
-        // console.log(data[0]); ???
-        const myPullRequest = data.filter((pull) => pull.user.login === repo.owner.login) //1. It gets only the PR that I made by comparing pull.user.login with repo.owner.login
+        const myPullRequest = data.find((pull) => pull.user.login === repo.owner.login) //1. It gets only the PR that I made by comparing pull.user.login with repo.owner.login
         // console.log(myPullRequest)
-        //2. Now you're able to get the commits for each repo by usingthe commits_url as an argument to call another function
-      // console.log(myPullRequest)
-      const url = myPullRequest[0].commits_url
-      
-      getCommits(url) // Function being called which prints all my commits
-
+        fetchCommits(myPullRequest.commits_url, repo.name)
       });
   });
 };
-
-
-
-const getCommits = (commitsParameter) => {
-  projectsContainer.innerHTML += `<h2>${commitsParameter}</h2>`
-
+////////////////////////////////
+const fetchCommits = (myCommitsUrl,myRepoName) => {
+  fetch(myCommitsUrl)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    document.getElementById(`commit-${myRepoName}`).innerHTML += data.length;
+  })
 }
-
-
 
 getRepos();
