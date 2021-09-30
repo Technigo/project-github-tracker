7b2df0cb7f4affa6e15de0141751c69fed2f1820
Sarah Mottram <smelbows@gmail.com>
2021-09-30 10:47:52 +0200

    added last commit message

--- code/script.js
+++ code/script.js
@@ -1,19 +1,19 @@
 // DOM selectors
 const main = document.getElementById("projects");
-const userInfo = document.getElementById("user-info")
+const userInfo = document.getElementById("user-info");
 
 const username = "Smelbows";
 const USERS_REPOS_API = `https://api.github.com/users/${username}/repos`;
 const TEST_USERS_REPOS_API = `/code/test_data/sarahs-repos-testing.json`;
 
-const fetchUserRepos = (url) => {
-  fetch(url)
+const fetchUserRepos = (userUrl) => {
+  fetch(userUrl)
     .then((res) => res.json())
     .then(filterForTechnigoRepos)
     .then((repos) => {
-        writeHTMLForUser(repos[0])
+      userInfo.innerHTML = createHTMLForUser(repos[0]);
       repos.forEach((repo) => {
-        writeHTMLForRepo(repo);
+        main.innerHTML += createHTMLForRepo(repo);
         fetchPullRequests(repo);
       });
     });
@@ -23,40 +23,42 @@ const filterForTechnigoRepos = (data) => {
   return data.filter((repo) => repo.name.startsWith("project"));
 };
 
-const writeHTMLForUser = (repo) => {
-    userInfo.innerHTML = `<p>${repo.owner.login}</p><img class="avatar" src=${repo.owner.avatar_url} alt="github avatar"/>`
-}
+const createHTMLForUser = (repo) => {
+  return `<p>${repo.owner.login}</p><img class="avatar" src=${repo.owner.avatar_url} alt="github avatar"/>`;
+};
 
-const writeHTMLForRepo = (repo) => {
-  main.innerHTML += `<section class="repo-container" id="${
-    repo.name
-  }"><a href="${repo.html_url}">${repo.name}</a><p>Default branch: ${
+const createHTMLForRepo = (repo) => {
+  return `<section class="repo-container" id="${repo.name}"><a href="${
+    repo.html_url
+  }">${repo.name}</a><p>Default branch: ${
     repo.default_branch
   }</p><p>Last pushed: ${new Date(
     repo.pushed_at
-  ).toDateString()}</p><p id="commit-${
+  ).toDateString()}</p><div id="commit-${
     repo.name
-  }">Number of commits: </p></section>`;
+  }"></div></section>`;
 };
 
 const fetchPullRequests = (repo) => {
   fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
     .then((res) => res.json())
     .then((data) => {
-      let myPullRequest;
-      if (repo.name === "project-weather-app") {
-        myPullRequest = data.find((pull) => pull.user.login === "anndimi");
-        fetchCommits(myPullRequest.commits_url, repo.name);
-      } else if (data === []) {
-        document.getElementById(`commit-${repo.name}`).innerHTML += "0";
+      myPullRequest = findMyPullRequest(data, repo.name);
+      if (myPullRequest === undefined) {
+        document.getElementById(`commit-${repo.name}`).innerHTML += "<p>Number of commits: 0</p>";
       } else {
-        myPullRequest = data.find(
-          (pull) => pull.user.login === repo.owner.login
-        );
         fetchCommits(myPullRequest.commits_url, repo.name);
       }
       console.log(myPullRequest);
     });
+};
+
+const findMyPullRequest = (pullsData, repoName) => {
+  if (repoName === "project-weather-app") {
+    return pullsData.find((pull) => pull.user.login === "anndimi");
+  } else {
+    return pullsData.find((pull) => pull.user.login === username);
+  }
 };
 
 const fetchCommits = (url, name) => {
@@ -64,7 +66,7 @@ const fetchCommits = (url, name) => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      document.getElementById(`commit-${name}`).innerHTML += `${data.length}`;
+      document.getElementById(`commit-${name}`).innerHTML += `<p>Number of commits: ${data.length}</p><p>Last commit message: ${data[data.length-1].commit.message}</p>`;
     });
 };
 
