7e92e0fd8e6d72c9ae451c6d374966a3424565dc
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-10-01 16:20:33 +0200

    create eventlisteners for sorting-btns

--- code/script.js
+++ code/script.js
@@ -2,24 +2,26 @@ let USER = 'sofiawillebrand'
 let PROJECT = 'project-weather-app'
 let API_PROFILE = `https://api.github.com/users/${USER}`
 let API_ALL_REPOS = `https://api.github.com/users/${USER}/repos`
-let REPO_API = `https://api.github.com/repos/${USER}/${PROJECT}/commits`
 let repoContainer = document.getElementById('repo-container')
 let profileContainer = document.getElementById('profile-container')
 let commitsSort = document.getElementById('commit-btn')
+let dateSort = document.getElementById('date-btn')
+let nameSort = document.getElementById('repo-btn')
 let repositorys = []
 let repoNr = 0
 
 const insertRepoInfo = (repository) => {
-    repoContainer.innerHTML += /*html*/ `
-      <div class="repo-card">
-        <h3>${repository.name}</h3>
-        <p>Date of latest push: ${repository.latest_push}</p>
-        <p>Default branch: ${repository.default_branch}</p>
-        <p>Latest commit-message: ${repository.commit_message}</p>
-        <p>Number of commits: ${repository.number_commits}</p>
-        <a>Link to GitHub repo: ${repository.project_url}</a>
-      </div>
-    `
+  let latestPush = new Date(repository.latest_push).toDateString()
+  repoContainer.innerHTML += /*html*/ `
+    <div class="repo-card" id=${repository.name}>
+      <h3>${repository.name}</h3>
+      <p>Date of latest push: ${latestPush}</p>
+      <p>Default branch: ${repository.default_branch}</p>
+      <p>Latest commit-message: ${repository.commit_message}</p>
+      <p>Number of commits: ${repository.number_commits}</p>
+      <a>Link to GitHub repo: ${repository.project_url}</a>
+    </div>
+  `
 }
 
 const getProfile = () => {
@@ -39,28 +41,26 @@ const getRepos = () => {
   .then(reposData => {
     console.log('Repodata: ', reposData)
     const technigoRepos = reposData.filter((repo) => repo.fork && repo.name.startsWith('project-'))
-    technigoRepos.sort((a, b) => 
-    (a.pushed_at > b.pushed_at ? -1 : 1))
     technigoRepos.forEach(project => {
-      let latestPush = new Date(project.pushed_at).toDateString()
+
       repositorys.push({
         'name': project.name,
-        'latest_push': latestPush,
+        'latest_push': project.pushed_at,
         'default_branch': project.default_branch,
         'project_url': project.html_url
       })
 
       let REPO_API = `https://api.github.com/repos/${USER}/${project.name}/commits`
- 
+
       fetch(REPO_API)
       .then(res => res.json()) 
       .then(commitData => {
         let commitMessage = commitData[0].commit.message
         let repository = repositorys[repoNr]
-        repoNr += 1
         repository['commit_message'] = commitMessage
         repository['number_commits'] = commitData.length
         insertRepoInfo(repository)
+        repoNr += 1
       })
     })
     drawChart(technigoRepos.length)
@@ -70,11 +70,27 @@ getRepos()
 getProfile()
 
 commitsSort.addEventListener("click", () => {
-  repoContainer.innerHTML = ''
-  console.log(repositorys)
-
+  repoContainer.innerHTML = '<h2>Projects:</h2>'
   let sortRepo = repositorys.sort((a, b) => 
     (a.number_commits > b.number_commits ? 1 : -1))
+  sortRepo.forEach((repo) => {
+    insertRepoInfo(repo)
+  })
+})
+
+nameSort.addEventListener("click", () => {
+  repoContainer.innerHTML = '<h2>Projects:</h2>'
+  let sortRepo = repositorys.sort((a, b) => 
+    (a.name > b.name ? 1 : -1))
+  sortRepo.forEach((repo) => {
+    insertRepoInfo(repo)
+  })
+})
+
+dateSort.addEventListener("click", () => {
+  repoContainer.innerHTML = '<h2>Projects:</h2>'
+  let sortRepo = repositorys.sort((a, b) => 
+    (a.latest_push > b.latest_push ? 1 : -1))
   sortRepo.forEach((repo) => {
     insertRepoInfo(repo)
   })
