34536feba4e7b6276065068a2c6a1e5a6018816b
pdetli <74938586+pdetli@users.noreply.github.com>
2021-10-02 01:00:20 +0200

    added more style and footer

--- code/chart.js
+++ code/chart.js
@@ -1,40 +1,73 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById("chart").getContext("2d")
 // const ctxtwo = document.getElementById('LanguageChart').getContext('2d')
 //"Draw" the chart here 👇
 
 const drawChart = (amount) => {
   const config = {
-    type: 'bar',
+    type: "bar",
     data: {
-      labels: [
-        'Finished projects',
-        'Projects left'
+      labels: ["Finished projects", "Projects left"],
+      datasets: [
+        {
+          data: [amount, 20 - amount],
+          barPercentage: 20,
+          barThickness: 100,
+          maxBarThickness: 100,
+          borderRadius: 2,
+          backgroundColor: ["#D4B499", "#B4846C"],
+
+          label: "Technigo Bootcamp Projects",
+          hoverOffset: 4,
+        },
       ],
-      datasets: [{
-        data: [amount, 20 - amount],
-        barPercentage: 20,
-        barThickness: 100,
-        maxBarThickness: 100,
-        borderRadius: 2,
-        backgroundColor: [
-          '#F5C6A5',
-          '#FE8F8F',
-        ],
-        label: 'Technigo Bootcamp Projects',
-        hoverOffset: 4
-      }]
     },
-  };
+    options: {
+      plugins: {
+        legend: {
+          position: "bottom",
+        },
+      },
+    },
+  }
 
-  const myChart = new Chart(ctx, config);
-};
+  const myChart = new Chart(ctx, config)
+}
 
+// const drawLanguageChart = (html_percentage, css_percentage, js_percentage)  => {
 
+//   const ctxLang = document.getElementById('languageChart');
+//   const dataLang = {
+//     labels = ['HTML', 'CSS', 'Javascript'],
+//     datasets: [{
+//       label: 'Language',
+//       data: [html_percentage, css_percentage, js_percentage],
+//       backgroundColor: [
+//         'rgba(255, 99, 132, 0.2)',
+//         'rgba(255, 159, 64, 0.2)',
+//         'rgba(255, 205, 86, 0.2)',
 
+//       ],
+//       borderColor: [
+//         'rgb(255, 99, 132)',
+//         'rgb(255, 159, 64)',
+//         'rgb(255, 205, 86)',
 
+//       ],
+//       borderWidth: 1
+//     }]
+//   };
 
-  
-
-
-
+//   const barChart = {
+//     type: 'bar',
+//     data: dataLang,
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true
+//         }
+//       }
+//     },
+//   }
+//   const langChart = new Chart(ctxLang, barChart);
+// }
--- code/script.js
+++ code/script.js
@@ -1,116 +1,117 @@
 // API`S and GLOBALS
 const username = "pdetli"
 const REPOS_URL = `https://api.github.com/users/${username}/repos`
-const USER_BIO_API = `https://api.github.com/users/${username}`;
+const USER_BIO_API = `https://api.github.com/users/${username}`
 
 // DOM SELECTORS
 const projectsContainer = document.getElementById("projects")
-const userInfoContainer = document.getElementById ("userInfoContainer")
-
-
+const userInfoContainer = document.getElementById("userInfoContainer")
 
 // FUNCTIONS
 
-//fetch the username, bio and picture from github with json 
+//fetch the username, bio and picture from github with json
 const getUsernameAndImage = () => {
-    
-    fetch (USER_BIO_API)
-    .then(res => res.json()) .then(data => {
-        console.log(data)
-        const pictureOfUser = data.avatar_url;
-        const userBio = data.bio;
-        const userLocation = data.location;
-        userInfoContainer.innerHTML += `
+  fetch(USER_BIO_API)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      const pictureOfUser = data.avatar_url
+      const userBio = data.bio
+      const userLocation = data.location
+      userInfoContainer.innerHTML += `
         <img class="user-picture" src="${pictureOfUser}" alt="Picture of gitHub-user"/>
         <div class= "user-information-container"> 
             <a class= "user-info-name" href="https://github.com/${username}" target="_blank"> @${username} </a>
             <p class= "user-info-bio"> < ${userBio} />   </p>
-            <p class= "user-info-bio" style= "color:grey"> 📍 ${userLocation}    </p>
+            <p class= "user-info-bio" style= "color:grey"> ${userLocation}    </p>
          </div>
         `
-    }
-    )}
-    
-
-
+    })
+}
 
 const getRepos = () => {
-    fetch(REPOS_URL)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            //to filter the data and to keep all the names start with project-  
-            //repo.fork === true and repo.fork gives the same result with "boolean is true"   
-            const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith("project-"))
-            //dispay the repo names in the browser
-            forkedRepos.forEach((repo) => {
-                projectsContainer.innerHTML += `
-                    <div class= each-project>
-                        <a class= "project-names-with-pr" href="${repo.html_url}" target="_blank">${repo.name}</a>
+  fetch(REPOS_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      //to filter the data and to keep all the names start with project-
+      //repo.fork === true and repo.fork gives the same result with "boolean is true"
+      const forkedRepos = data.filter(
+        (repo) => repo.fork && repo.name.startsWith("project-")
+      )
+      //dispay the repo names in the browser
+      forkedRepos.forEach((repo) => {
+        projectsContainer.innerHTML += `
+                    <div class= project-card>
+                        <a class= "project-names-with-pr" href="${
+                          repo.html_url
+                        }" target="_blank">${repo.name}</a>
+                        <span class="repo-language"></span>
+                        <span class="language-name"> ${repo.language}</span>
                         <p>Default branch: ${repo.default_branch} </p> 
-                        <p> Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+                        <p> Recent push: ${new Date(
+                          repo.pushed_at
+                        ).toDateString()}</p>
                         <p class= "commits-info"id="commit-${repo.name}"> </p>
-                    `
+                     `
 
-            })
+        const repoLanguage = document.querySelectorAll(".repo-language")
+        //An array for only forEach method
+        repoLanguage.forEach((span) => {
+          if (span.nextElementSibling.innerText === "JavaScript") {
+            span.style.backgroundColor = "#f1e05a"
+          } else if (span.nextElementSibling.innerText === "HTML") {
+            span.style.backgroundColor = "#e34c26"
+          } else if (span.nextElementSibling.innerText === "CSS") {
+            span.style.backgroundColor = "#664e88"
+          }
+        })
+      })
 
-{/* <p class= "commits-amount"id="commit-${repo.name}"> </p> */}
-            getPullRequests(forkedRepos)
-            drawChart(forkedRepos.length) // draw chart with the forkedRepos data 
-           
-        });
+      getPullRequests(forkedRepos)
+      drawChart(forkedRepos.length) // draw chart with the forkedRepos data
+    })
 }
-
 
 const getPullRequests = (allRepos) => {
-    //Get all the PRs for each project.
-    allRepos.forEach((repo => {
-        fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
-            .then(res => res.json())
-            .then(data => {
+  //Get all the PRs for each project.
+  allRepos.forEach((repo) => {
+    fetch(
+      `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const myPullRequest = data.find(
+          (pull) => pull.user.login === repo.owner.login
+        )
+        // Detect if we have pull request or not.
+        // If yes - call fetchCommits function
+        // If no - inform user that no pull request was yet done
+        if (myPullRequest) {
+          fetchCommits(myPullRequest.commits_url, repo.name)
+        } else {
+          document.getElementById(`commit-${repo.name}`).innerHTML =
+            "No pull request yet done :("
+        }
 
-                const myPullRequest = data.find(
-                    (pull) => pull.user.login === repo.owner.login
-                );
-                // Detect if we have pull request or not.
-                // If yes - call fetchCommits function
-                // If no - inform user that no pull request was yet done
-                if (myPullRequest) {
-                    fetchCommits(myPullRequest.commits_url, repo.name);
-                } else {
-                    document.getElementById(`commit-${repo.name}`).innerHTML =
-                     'No pull request yet done :(';
-                }
-               
-                /* 1. Find only the PR that you made by comparing pull.user.login with repo.owner.login
+        /* 1. Find only the PR that you made by comparing pull.user.login with repo.owner.login
                    2. Now you're able to get the commits for each repo by using the commits_url as an argument to call another function
                    3. You can also get the comments for each PR by calling another function with the review_comments_url as argument */
-            
-                  
-                })
-               
-    }))
+      })
+  })
 }
 const fetchCommits = (myCommitsUrl, myRepoName) => {
-    fetch(myCommitsUrl)
-        .then((res) => res.json())
-        .then((data) => {
-            let commitMessage = data[0].commit.message;
-            document.getElementById(`commit-${myRepoName}`).innerHTML += 
-            ` <p class= commits-info> Number of commits: ${data.length} </p> `
-             
-/* <h3 class= commits-message> ${commitMessage} </h3>   */
-        });
+  fetch(myCommitsUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let commitMessage = data[0].commit.message
+      document.getElementById(
+        `commit-${myRepoName}`
+      ).innerHTML += ` <p class= commits-info> Number of commits: ${data.length} </p> `
 
-
-};
-
- 
-
-
-
-
-  
+      /* <h3 class= commits-message> ${commitMessage} </h3>   */
+    })
+}
 
 getUsernameAndImage()
 getRepos()
@@ -144,5 +145,3 @@ Make sure you've committed and pushed a version of your project before starting 
 ⚫  Black Level (Advanced Goals)**
 
 - Implement a search field to find a specific repo based on name */
-
-
