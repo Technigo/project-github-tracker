f2da2e5db83d5c61dca20425289323cc480e5bb1
molbimien <43146046+molbimien@users.noreply.github.com>
2021-10-01 13:56:18 +0200

    setup of project and addition of blue level (minimum) requirements except for styling

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,27 @@
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById('chart').getContext('2d');
 
 //"Draw" the chart here 👇
+
+const drawChart = (amount) => {
+    const config = {
+        type: 'doughnut',
+        data: {
+        labels: [
+          'Finished projects',
+          'Projects left',
+        ],
+        datasets: [{
+          label: 'My Technigo projects',
+          data: [amount, 19-amount],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+          ],
+          hoverOffset: 4
+        }]
+      },
+    };
+
+    const projectsChart = new Chart(ctx, config);
+}
