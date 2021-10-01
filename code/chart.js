341134bea65ade2380f4e433e2242228bc8b7af0
Rephili <72921631+Rephili@users.noreply.github.com>
2021-10-01 14:54:22 +0200

    added a chart

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,20 @@
-//DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+//DOM-selector
+const ctx = document.getElementById("chart").getContext("2d");
 
-//"Draw" the chart here 👇
+//The chart
+const config = {
+  type: "pie",
+  data: {
+    labels: ["Finished projects", "Projects left"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [5, 19 - 5],
+        backgroundColor: ["#ee6c4d", "#3d5a80"],
+        hoverOffset: 4,
+      },
+    ],
+  },
+};
+
+const myChart = new Chart(ctx, config);
