54f817f287c25e75816973d0230a52c818787fa9
Jenny Quach <quach.jen@gmail.com>
2021-10-02 21:54:32 +0200

    adding js functionality and mobile styling

--- code/script.js
+++ code/script.js
@@ -1,25 +1,29 @@
 const USER = 'jenquach'
+const USER_URL = `https://api.github.com/users/${USER}`
 const REPOS_URL = `https://api.github.com/users/${USER}/repos`
 
-// const repoNames = [
-//   'project-news-site', 
-//   'project-business-site', 
-//   'project-chatbot', 
-//   'project-guess-who', 
-//   'project-weather-app', 
-// ]
-
-// const commitsURL = [
-//   'https://api.github.com/repos/Technigo/project-news-site/pulls/242/commits', 
-//   'https://api.github.com/repos/Technigo/project-business-site/pulls/222/commits', 
-//   'https://api.github.com/repos/Technigo/project-chatbot/pulls/65/commits',
-//   'https://api.github.com/repos/Technigo/project-guess-who/pulls/120/commits',
-// ]
-
-
+const headerContainer = document.getElementById('topnav')
+const profileContainer = document.getElementById('user-info')
 const projectsContainer = document.getElementById('projects')
 const pullRequests = document.getElementById('pullrequests')
 
+// function to fetch the user data from GitHub API
+const userProfile = () => {
+  fetch(USER_URL)
+    .then(res => res.json())
+    .then(data => {
+      profileContainer.innerHTML = `
+      <div class="picture">
+        <img class="user-picture" src="${data.avatar_url}"
+      </div>
+      <div class="user-information">  
+        <h2>${data.name}</h2>
+        <a href="https://github.com/jenquach">${data.login}</a>
+        <p>${data.location}</p>
+      </div>
+      `
+    })
+}
 
 const getRepositories = () => {
   fetch(REPOS_URL)
@@ -28,22 +32,21 @@ const getRepositories = () => {
       const technigoRepositories = data.filter(
         repo => repo.fork && repo.name.includes('project-')
       )
-      console.log(technigoRepositories)
-
       technigoRepositories.forEach(repo => {
         projectsContainer.innerHTML += `
-          <div>
-            <a href="${repo.html_url}">${repo.name} with default branch ${repo.default_branch}</a>
-            <p>Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
-            <p id="commit-${repo.name}">Amounts of commit:</p>
+          <div class="project">
+            <a href="${repo.html_url}">${repo.name}</a>
+            <p>Recent push ${new Date(repo.pushed_at).toDateString()}</p>
+            <p id="commit-${repo.name}">Commits </p>
+            <p>Branch ${repo.default_branch}</p>
           </div>
         `
       })  
+      // Draw chart with technigoRepositories data
       drawChart(technigoRepositories.length)
       getPullRequests(technigoRepositories)  
     })
 }  
-      
 
 const getPullRequests = (allRepositories) => {
   allRepositories.forEach(repo => {
@@ -52,10 +55,18 @@ const getPullRequests = (allRepositories) => {
     fetch(PULL_URL)
     .then(res => res.json())
     .then(data => { //data is the array of all pull requests
+      // Detect if we have pull request or not.
       const myPullRequest = data.find(
       pull => pull.user.login === repo.owner.login
-    )     
-      getCommits(myPullRequest.commits_url, myRepoName)
+    )    
+		// If yes - call fetchCommits function
+    if (myPullRequest) {
+      getCommits(myPullRequest.commits_url, repo.name);
+      // If no - commits 0 is shown.
+    } else {
+      document.getElementById(`commit-${repo.name}`).innerHTML =
+        'Commits 0';
+      }  
     })    
   })
 }
@@ -64,8 +75,9 @@ const getCommits = (myCommitsUrl, myRepoName) => {
   fetch(myCommitsUrl)
   .then(res => res.json())
   .then(data => {
-    document.getElementById(`commit-${myRepoName}`).innerHTML += data.length
-  })
+    document.getElementById(`commit-${myRepoName}`).innerHTML +=  data.length
+  }) 
 }
 
+userProfile()
 getRepositories()
