cb0fdf3a1bbf0a99791f263090f196a87dbd7fb9
pdetli <74938586+pdetli@users.noreply.github.com>
2021-10-03 00:06:01 +0200

    cleaning the code

--- code/script.js
+++ code/script.js
@@ -22,8 +22,8 @@ const getUsernameAndImage = () => {
         <img class="user-picture" src="${pictureOfUser}" alt="Picture of gitHub-user"/>
         <div class= "user-information-container"> 
             <a class= "user-info-name" href="https://github.com/${username}" target="_blank"> @${username} </a>
-            <p class= "user-info-bio"> < ${userBio} />   </p>
-            <p class= "user-info-bio" style= "color:grey"> ${userLocation}    </p>
+            <p class= "user-info-bio"> < ${userBio} /> </p>
+            <p class= "user-info-bio" style= "color:grey"> ${userLocation} </p>
          </div>
         `
     })
@@ -56,10 +56,12 @@ const getRepos = () => {
                           repo.pushed_at
                         ).toDateString()}</p>
                         <p class= "commits-info"id="commit-${repo.name}"> </p>
+                    </div> 
                      `
 
+        // display the most used language for each project and change the color depending on the language
         const repoLanguage = document.querySelectorAll(".repo-language")
-        //An array for only forEach method
+
         repoLanguage.forEach((span) => {
           if (span.nextElementSibling.innerText === "JavaScript") {
             span.style.backgroundColor = "#f1e05a"
@@ -98,11 +100,13 @@ const getPullRequests = (allRepos) => {
         }
 
         /* 1. Find only the PR that you made by comparing pull.user.login with repo.owner.login
-        2. Now you're able to get the commits for each repo by using the commits_url as an argument to call another function
-        3. You can also get the comments for each PR by calling another function with the review_comments_url as argument */
+        2. Get the commits for each repo by using the commits_url as an argument to call another function
+        3. we can also get the comments for each PR by calling another function with the review_comments_url as argument */
       })
   })
 }
+
+// fetch the number of commits
 const fetchCommits = (myCommitsUrl, myRepoName) => {
   fetch(myCommitsUrl)
     .then((res) => res.json())
@@ -111,40 +115,8 @@ const fetchCommits = (myCommitsUrl, myRepoName) => {
       document.getElementById(
         `commit-${myRepoName}`
       ).innerHTML += ` <p class= commits-info> Number of commits: ${data.length} </p> `
-
-      /* <h3 class= commits-message> ${commitMessage} </h3>   */
     })
 }
 
 getUsernameAndImage()
 getRepos()
-
-/* 
-🔵  Blue Level (Minimum Requirements)**
-
-Your page should include:
-
-- A list of all repos that are forked from Technigo
-- Your username and profile picture
-- Most recent update (push) for each repo
-- Name of your default branch for each repo
-- URL to the actual GitHub repo
-- Number of commits for each repo
-- It should be responsive (mobile first)
-- A visualisation, for example through a pie chart, of how many projects you've done so far, compared to how many you will do (in total it will be 19 weekly projects 🥳) using [Chart.js](https://www.chartjs.org/).
-
-Make sure you've committed and pushed a version of your project before starting with the intermediary and advanced goals.
-
-🔴  Red Level (Intermediary Goals)**
-
-- Sort your list in different ways.
-**Example of what you can sort by:** creation date, name, most commits, followers or stargazers
-- Create the opportunity to filter the repos based on a condition, e.g. only JavaScript repos
-- Display the actual commit messages for each repo in a good way. Maybe by adding a modal (popup) or an accordion?
-- Display the comments you got from each pull request
-- When you're browsing through the repo object, you'll see that there's a lot of data that we can use. Create a chart over some of the data.
-**Example of data to use:** Repo size, people that starred your repo (stargazers) or amount of commits on each repo.
-
-⚫  Black Level (Advanced Goals)**
-
-- Implement a search field to find a specific repo based on name */
