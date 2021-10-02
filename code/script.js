6feedee021bc7b6d5119de6e406eafd1ddeecc6c
unknown <elsa.bjelkedal@gmail.com>
2021-10-02 12:28:36 +0200

    added an API key to my github page. added and filtered to only the projects for this bootcamp

---
+++ code/script.js
@@ -0,0 +1,15 @@
+const REPOS_URL = `https://api.github.com/users/Elsa-Johanna/repos`
+const projectsContainer = document.getElementById('projects')
+
+const getRepos = () => {
+    fetch(REPOS_URL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        //data.forEach(repo => console.log(repo.name))
+        const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith('project-'))
+        forkedRepos.forEach(repo => console.log(repo.name))
+        forkedRepos.forEach(repo => projectsContainer.innerHTML += `<h3> ${repo.name}</h3>`)
+    })
+}
+getRepos()
