5f1ca01519b13ff0594ac6555520a979be17913f
hemmahosjessi <you@example.com>
2021-10-02 15:30:45 +0200

    trying out the tooltip

--- code/script.js
+++ code/script.js
@@ -40,6 +40,8 @@ const getRepos = () => {
 
         forkedRepos.forEach(repo => projectsContainer.innerHTML += `
         <div class="project-card"> 
+            <div class="tooltip">
+                <span class="tooltiptext">“You only live once, but if you do it right, once is enough.” — Mae West.</span>
                 <button class="open-modal" data-open="modal" id="open-modal">
                     <svg class="link-arrow" id="linkArrow" width="356" height="356" viewBox="0 0 356 356" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M175 65.8677L164.931 75.9369C162.548 78.3201 162.548 82.1842 164.931 84.5679L243.826 163.462H70.1829C66.8126 163.462 64.0801 166.195 64.0801 169.565V183.805C64.0801 187.175 66.8126 189.908 70.1829 189.908H243.826L164.931 268.802C162.548 271.186 162.548 275.05 164.931 277.433L175 287.503C177.383 289.886 181.247 289.886 183.631 287.503L290.133 181.001C292.516 178.617 292.516 174.753 290.133 172.37L183.63 65.8677C181.247 63.484 177.383 63.484 175 65.8677Z" fill="#dedede"/>
@@ -48,6 +50,7 @@ const getRepos = () => {
                     </mask>
                     </svg>
                 </button>
+            </div>
                 <a class="break" href="${repo.html_url}">${repo.name}</a>
                 <div class="most-recent">
                     <p>Most recent push</p>
