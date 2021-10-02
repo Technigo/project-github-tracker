35994227ec961446d6fb14260f3038ff6fdbf245
mamite100 <mamite.andersson@student.kyh.se>
2021-10-02 22:45:24 +0200

    added the last on js-file, started on styling

--- code/chart.js
+++ code/chart.js
@@ -1,27 +1,25 @@
-//DOM-selector for the canvas 👇
 const ctx = document.getElementById('chart').getContext('2d')
 
-//"Draw" the chart here 👇
 
-const config = {
-  type: 'doughnut',
-  data: {
-  labels: [
-    'Red',
-    'Blue',
-    'Yellow'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
-}
-};
 
- const  myChart = new Chart (ctx, config); 
+
+const drawChart = (number) => {
+    const config = {
+      type: 'doughnut',
+      data: {
+        labels: ["Projects done", "Still to do"],
+        datasets: [
+          {
+            label: "My first Dataset", 
+            data: [number, 20 - number], // divide donut to 2 different parts
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+            ],
+            hoverOffset:4,
+          },
+        ],
+      },
+    }
+    const myChart = new Chart(ctx, config);
+  };
--- code/script.js
+++ code/script.js
@@ -1,84 +1,54 @@
-const username = "mamite100";
-const repoApi = `https://api.github.com/users/${username}/repos`;
-const projectsContainer = document.getElementById("projects");
-const profileContainer = document.getElementById("profile-container")
-const image = document.getElementById("image");
-const userName = document.getElementById("userName");
-const chartSection = document.getElementById("projectsInfo");
-const commentsApi = `https://api.github.com/repos/Technigo/project-news-site/pulls/247/comments`;
-const commitApi = `https://api.github.com/repos/Technigo/project-news-site/pulls/227/commits`; 
+const USER= "mamite100";
+const REPOS_URL= `https://api.github.com/users/${USER}/repos`;
+const projectsContainer = document.
+getElementById("projects");
 
-const getRepos = () => {
-    fetch (repoApi)
+const fetchRepositories = () => {
+    fetch (REPOS_URL)
         .then((res) => res.json())
         .then((data) => {
-        console.log(data);
-
-        const forkedRepos = data.filter(
-        (repo) => repo.fork && repo.name.startsWith("project-") 
+        const technigoRepositories = data.filter((repo) => repo.name.includes("project-") && repo.fork 
         );
+      technigoRepositories.forEach(
+        (repo) => {
+        projectsContainer.innerHTML += `
+        <div>
+        <a href="${repo.html_url }" > ${repo.name} with default branch
+        ${repo.default_branch}</a>   
+        <p> Recent push: ${new Date (repo.pushed_at) .toDateString() } </p> 
+        <p id="commit-${repo.name}"> Commits: </p>   
+         </div> 
+        `;
+        });
+        });
+};
 
-        //console.log(forkedRepos)
-
-        forkedRepos.forEach(
-        (repo) =>
-          (projects.innerHTML += 
-            `<div class="repo" id="${repo.name}"> 
-               <a class="repo-item1" href="${repo.html_url}" target="_blank">${
-              repo.name}</a>
-               <span class="repo-item2">${repo.default_branch}</span>
-               <h4 class="repo-item3">Last updated: ${new Date(
-                 repo.updated_at).toLocaleDateString()}</h4>
-               </div>`)
-        );
-        drawChart(forkedRepos.length);
-   
-        getPullRequest(forkedRepos);
-      });
-    };
-const getInfo =()=> {
-    fetch (repoApi)
-    .then ((res) => res.json())
+const fetchPullRequestsArray = ( allRepositories) => {
+  allRepositories.forEach((repo) => {
+   const PULL_URL = 
+   `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
+   fetch (PULL_URL)
+    .then( (res) => res.json())
     .then((data) => {
-    image.src = data.avatar_url;
-    projectsContainer.innerHTML += ` <div>
-    <tr><h1> ${data.name}</h1></tr>
-    <tr><h5> ${ data.login}</h5></tr>
-    <tr><h5> ${ data.bio}</h1></tr>
-    </div>`;
-  });
-};
-getInfo();
-const getPullRequest = (forkedRepos) => {
-  forkedRepos.forEach((repo) => {
-    fetch(
-      `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data);
-        const myPulls = data.filter(
-          (pulls) => pulls.user.login === repo.owner.login
-        );
-        console.log(myPulls);
-        const commitsURL = myPulls[0].commits_url;
-        // console.log(commitsURL);
-        getCommits(commitsURL, repo);
-
-        // const myComments =
-      });
-  });
+              const myPullRequest = data.find(
+                (pull) => pull.user.login === repo.owner.login
+              );
+if (myPullRequest) {
+					fetchCommits(myPullRequest.commits_url, repo.name);
+				} else {
+					document.getElementById(`commit-${repo.name}`).innerHTML =
+						'No pull request yet done : ( ';
+				}
+			});
+	});
 };
 
-const getCommits = (commitsURL, repo) => {
-  fetch(commitsURL)
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data.length);
-      document.getElementById(
-        `${repo.name}`
-      ).innerHTML += `<h4 class="repo-item4"> Number of commits: ${data.length}</h4>`;
-    });
+const fetchCommits = (myCommitsUrl, myRepoName) => {
+	fetch(myCommitsUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			document.getElementById(`commit-${myRepoName}`).innerHTML += data.length;
+		});
 };
 
-getRepos();
+fetchRepositories();
