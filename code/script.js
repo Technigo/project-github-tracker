f6f45d17b92567b732bceb98447391a725105e3c
Elin Elmvik Diczfalusy <elinelmvikdiczfalusy@Elins-iMac.local>
2021-10-02 09:47:16 +0200

    cleaning of code

--- code/script.js
+++ code/script.js
@@ -12,32 +12,31 @@ fetch(REPOS_URL)
     })
     .then ((data) => {
         repoInfo(data)   
-})
+    })
 }
 fetchRepos()
 
-// ------------fetching reop for first lines inside FIRST FETCH FUNCTION------------
+// ------------function for first lines inside FIRST FETCH FUNCTION------------
 
 const repoInfo = ((data) =>{
     first.innerHTML = `
-    <div class=header-image>
-    <h1>Welcome to ${data[0].owner.login}s GitHub Tracker</h1>
-    <img class="profile-img" src="https://avatars.githubusercontent.com/u/84288114?v=4" alt="profile pic Ajliin">
+    <div>
+        <h1>Welcome to ${data[0].owner.login}s GitHub Tracker</h1>
+        <img class="profile-img" src="https://avatars.githubusercontent.com/u/84288114?v=4" alt="profile pic Ajliin">
     </div>
     <div class=info-square>
-    <h2>Elin Diczfalusy</h2>
-    <p>User name: ${data[0].owner.login}</p>
-    <p>GitHub page: <a href="${data[0].owner.html_url}">${data[0].owner.html_url}</a></p>
+        <h2>Elin Diczfalusy</h2>
+        <p>User name: ${data[0].owner.login}</p>
+        <p>GitHub page: <a href="${data[0].owner.html_url}">${data[0].owner.html_url}</a></p>
     </div>`
 
+// ------------function filters out the forked repos from Technigo
     const forkedRepo = data.filter((repo) => repo.fork && repo.name.startsWith('project-'))
-        console.log('outside forkedRepo-function', forkedRepo)
-        drawChart(forkedRepo.length)
+// ------------function to chart.js on how many repos      
+    drawChart(forkedRepo.length)
+// ------------forEach repo --> send to innerHTML   
     forkedRepo.forEach((repo) => {
-        
         let dateString = new Date(repo.pushed_at)
-        console.log(dateString)
-        
         
         projectContainer.innerHTML += `<div class ="project"> 
         <p>Project name: ${repo.name}</p>
@@ -47,59 +46,45 @@ const repoInfo = ((data) =>{
         <p>Last push: ${dateString.toUTCString()}
         <p id="commit-${repo.name}">Commits amount: </p></div>`
     })
-    getProjectInfo(forkedRepo)
+
+getProjectInfo(forkedRepo)
+
 })
 
 getProjectInfo = (allRepositories) => {
-    console.log('from getProjectInfo från första fetchen', allRepositories)
-    allRepositories.forEach((project)=>{
+    allRepositories.forEach((project)=>{    
         const PULL_API = `https://api.github.com/repos/technigo/${project.name}/pulls?per_page=100`
-        console.log(PULL_API)
     
-
 // ------------SECOND FETCH FUNCTION------------
-   
     const fetchProjectinfo = (forkedRepo) => {
         fetch(PULL_API)
             .then ((response) => {
             return response.json()
             })
             .then ((json) => {
-            console.log('json',json)
             const myPullRequest = json.find((pull) => (pull.user.login === project.owner.login))
                 // Första är alla användare, project.owner.login
-                //console.log('project.user.login', project.user.login)
-            
-            console.log('project.owner.login', project.owner.login)
-            //console.log('json.owner.login', json.owner.login)
-            //console.log('forkedRepo.owner.login', forkedRepo.owner.login)
-            console.log('project.name',project.name)
-            console.log('myPullRequest', myPullRequest)
+        
             
             if (myPullRequest) {
              fetchCommits(myPullRequest.url, project.name)
-             console.log('myPullRequest[0].url',myPullRequest.url)
-             console.log('project.Name into myPullRequest', project.name)
-             console.log('fetchCommits(myPullRequest.url, project.name)', (myPullRequest.url, project.name))
+             
             }else{
                 console.log('else part in function works', myPullRequest)
             document.getElementById(`commit-${project.name}`).innerHTML += 'No pull request done yet'
             }
-        })
-        
+        }) 
     } 
   fetchProjectinfo()   
 }) 
 }
 // ------------THRID FETCH FUNCTION------------
 const fetchCommits = ((commitUrl, projectName) => {
-    console.log("console commitUrl", commitUrl, projectName)
     fetch(commitUrl)
     .then ((response) => {
         return response.json()
     })
     .then ((data) => {
-        console.log('I am the master!')
         document.getElementById(`commit-${projectName}`).innerHTML += `${data.commits}`
     })
  
