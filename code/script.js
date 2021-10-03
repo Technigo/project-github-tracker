1d6ff28f9be68c63b710930932b004cce71e1af8
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-10-03 18:09:49 +0200

    update styling and fetch for commits, comment code

--- code/script.js
+++ code/script.js
@@ -9,8 +9,9 @@ let commitsSort = document.getElementById('commit-btn')
 let dateSort = document.getElementById('date-btn')
 let nameSort = document.getElementById('repo-btn')
 let repositorys = []
-let repoNr = 0
 
+
+// create repo-card for each repository
 const insertRepoInfo = (repository) => {
   let latestPush = new Date(repository.latest_push).toDateString()
   repoContainer.innerHTML += /*html*/ `
@@ -18,12 +19,13 @@ const insertRepoInfo = (repository) => {
       <a href=${repository.project_url} class="link" target="_blank">${repository.name}</a>
       <p>Date of latest push: ${latestPush}</p>
       <p>Default branch: ${repository.default_branch}</p>
-      <p>Latest commit-message: ${repository.commit_message}</p>
       <p>Number of commits: ${repository.number_commits}</p>
+      <p>Latest commit-message: ${repository.commit_message}</p>
     </div>
   `
 }
 
+//get info for profile
 const getProfile = () => {
   fetch(API_PROFILE)
   .then(res => res.json()) 
@@ -47,7 +49,7 @@ const getRepos = () => {
     console.log('Repodata: ', reposData)
     const technigoRepos = reposData.filter((repo) => repo.fork && repo.name.startsWith('project-'))
     technigoRepos.forEach(project => {
-
+      // add information about the repo to and object in the array repositorys
       repositorys.push({
         'name': project.name,
         'latest_push': project.pushed_at,
@@ -55,34 +57,38 @@ const getRepos = () => {
         'project_url': project.html_url
       })
 
-      let REPO_API = `https://api.github.com/repos/${USER}/${project.name}/commits`
-
+    // Get info about commits for specific repo
+    let REPO_API = `https://api.github.com/repos/${USER}/${project.name}/commits`
+ 
       fetch(REPO_API)
       .then(res => res.json()) 
       .then(commitData => {
         let commitMessage = commitData[0].commit.message
-        let repository = repositorys[repoNr]
+        // find the object with the same name as the current project in the forEach
+        let repository = repositorys.find(repo => repo.name === project.name)
         repository['commit_message'] = commitMessage
         repository['number_commits'] = commitData.length
         insertRepoInfo(repository)
-        repoNr += 1
       })
     })
     drawChart(technigoRepos.length)
   })
 }
+
 getRepos()
 getProfile()
 
+// Sort by number of commits, descending
 commitsSort.addEventListener("click", () => {
   repoContainer.innerHTML = ''
   let sortRepo = repositorys.sort((a, b) => 
-    (a.number_commits > b.number_commits ? 1 : -1))
+    (a.number_commits > b.number_commits ? -1 : 1))
   sortRepo.forEach((repo) => {
     insertRepoInfo(repo)
   })
 })
 
+// Sort by name of repo, ascending
 nameSort.addEventListener("click", () => {
   repoContainer.innerHTML = ''
   let sortRepo = repositorys.sort((a, b) => 
@@ -92,6 +98,8 @@ nameSort.addEventListener("click", () => {
   })
 })
 
+
+// sort by date, descending
 dateSort.addEventListener("click", () => {
   repoContainer.innerHTML = ''
   let sortRepo = repositorys.sort((a, b) => 
