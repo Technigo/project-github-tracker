dde54209dc95b03e1a622fbdd15463615b17d1ed
Emelie <Emelie@Jophees-MacBook-Pro.local>
2021-10-03 16:04:11 +0200

    cleaned up the code and added comments for clarification

--- code/script.js
+++ code/script.js
@@ -5,22 +5,22 @@ const USER_INFO_URL = `https://api.github.com/users/${USER}`
 const projectsContainer = document.getElementById("projects")
 const userContainer = document.getElementById("user-info")
 
+// function for getting my user info
 const getUserInfo = () => {
   fetch(USER_INFO_URL)
     .then((response) => response.json())
     .then((data) => {
-      console.log("USER DATA", data)
       userContainer.innerHTML += `
       <img class="profile-img" src="https://avatars.githubusercontent.com/u/84201089?v=4" alt="User profile picture">
       <span id="profile-name"> <a class="usr-name" href="https://github.com/waliem"><h2>${data.login}</h2></a> <h3>Front end developer student</h3><h3>Based in Gothenburg</h3></span>`
     })
 }
 
+// function for getting all my repos and filter them to get only technigo projects and displaying them in inner html
 const getRepos = () => {
   fetch(REPOS_URL)
     .then((response) => response.json())
     .then((data) => {
-      console.log("MY REPOS", data)
       let forkedRepos = data.filter(
         (repo) => repo.fork && repo.name.startsWith("project-")
       )
@@ -40,6 +40,7 @@ const getRepos = () => {
     })
 }
 
+//function for fetching my pull requests
 const getPullRequests = (forkedRepos) => {
   forkedRepos.forEach((repo) => {
     const PULLS_URL = `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`
@@ -50,7 +51,7 @@ const getPullRequests = (forkedRepos) => {
         const myPulls = data.find(
           (pull) => pull.user.login === repo.owner.login
         )
-
+        // if/else conditon if I do not have any pull requests or commits it will display "no PR or commits done"
         if (myPulls) {
           getMyCommits(myPulls.commits_url, repo.name)
         } else {
@@ -62,6 +63,7 @@ const getPullRequests = (forkedRepos) => {
   })
 }
 
+// function for getting the number of commits
 const getMyCommits = (myCommitsUrl, myRepoName) => {
   fetch(myCommitsUrl)
     .then((res) => res.json())
