763f814930490e38fefa168f2f093fb4f6d75c77
NinaAlejandra <Niinaa___@hotmail.com>
2021-10-04 01:10:23 +0200

    first draft without media queries

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # GitHub Tracker
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The assignment was to make a github-tracker, a page which shows your profile picture, username and information about your projects, pullrequests and commits. We also learned how to incorporate charts in our work. 
 
 ## The problem
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+
+Fortunately I have a couple of people I study with, who help me a lot when I'm stuck. I started by watching the videos by our teachers, and then the recordings of our classes. This helped since I could rewind and watch again if something was unclear. I've also used StackOverflow for some guidance. 
+
+This week I've used a page to help me with the colorschemes for my page and this is the link: https://coolors.co/
+I've also learned how to do shading in css which was cool. Hopefully my javascript aha-moment is just a couple of weeks away!
+
 
 ## View it live
 
---
+++ code/script.js
@@ -0,0 +1,77 @@
+//DOM-selectors
+const profileInfo = document.getElementById("profile")
+const list = document.getElementById("list")
+
+//Github
+const USER = 'NinaAlejandra'
+const REPOS_URL = `https://api.github.com/users/${USER}/repos`
+const API_MY_PROFILE = 'https://api.github.com/users/NinaAlejandra'
+
+//User
+const getUser = () => {
+    fetch(API_MY_PROFILE)
+      .then((response) => response.json())
+        .then((data) => {
+              
+            
+        profileInfo.innerHTML = 
+            `<img src="https://github.com/NinaAlejandra.png" alt="Profile picture">
+            <h4> ${data.name}</h4>
+            <h4> <a href="${data.html_url}">${data.login}</a></h4>`
+            
+      })
+}
+
+  //Fetch
+const getRepos = () => {
+    fetch(REPOS_URL)
+    .then(response => response.json())
+    .then(data => {
+    
+        const forkedRepos = data.filter(repo => repo.fork && repo.name.startsWith('project-'))
+        const forkedSortedRepos = forkedRepos.sort(sortingFunctionFromStackOverflow)
+        forkedSortedRepos.forEach(repo => list.innerHTML += `
+                    <div class="projects">
+                    <h3><a href="${repo.html_url}">${repo.name} 
+                    with default branch ${repo.default_branch}</h3></a>
+                    <p>Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+                    <p id="pull-request-${repo.name}"></p>
+                    <p id="commits-${repo.name}"></p>
+                    </div>` )
+        drawChart(forkedSortedRepos.length)
+        fetchPullRequest(forkedSortedRepos)
+        addCommits(forkedSortedRepos)
+    })
+}
+
+const fetchPullRequest = (allRepos) => {
+    allRepos.forEach(repo => {
+        fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
+        .then((res) => res.json())
+        .then((data) => { 
+            const myPullRequests = data.filter((pullRequest) => pullRequest.user.login === USER)
+            document.getElementById(`pull-request-${repo.name}`).innerHTML = `Pull Requests: ${myPullRequests.length}`
+        
+        })
+    })
+}
+
+
+
+const addCommits = (allRepos) => {
+    allRepos.forEach(repo => {
+        fetch(`https://api.github.com/repos/${USER}/${repo.name}/commits`)
+        .then((res) => res.json())
+        .then((data) => { 
+            document.getElementById(`commits-${repo.name}`).innerHTML = `commits: ${data.length}`
+        
+        })
+    })
+}
+
+function sortingFunctionFromStackOverflow(a, b) {
+    // https://stackoverflow.com/questions/10123953/how-to-sort-an-object-array-by-date-property
+    return new Date(a.created_at) - new Date(b.created_at)
+}
+getUser()
+getRepos()
