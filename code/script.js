52004ed4ac46bc9c116a314011458be981e98c40
annathunberg <83536062+annathunberg@users.noreply.github.com>
2021-10-04 22:54:16 +0200

    removed token

--- code/script.js
+++ code/script.js
@@ -1,13 +1,3 @@
-// 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
-
-const myToken = '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';
-const config = {
-    headers: {
-        'Authorization': `token ${myToken}`
-    }
-};
-
-
 // DOM SELECTORS
 const list = document.getElementById('list')
 const avatar = document.getElementById('avatar')
@@ -20,7 +10,7 @@ const API_MY_REPOS = `https://api.github.com/users/${USER}/repos`// user-variabl
 const API_MY_PROFILE = 'https://api.github.com/users/annathunberg'
 
 // FETCHES THE PROFILE PIC, NAME AND USERNAME
-fetch(API_MY_PROFILE, config)
+fetch(API_MY_PROFILE)
 .then((response) => response.json())
 .then((data) => {
     fullName.innerHTML = `Hi! I'm ${data.name}`
@@ -29,7 +19,7 @@ fetch(API_MY_PROFILE, config)
 })
 
   // FETCHES MY REPOS
-  fetch(API_MY_REPOS, config)
+  fetch(API_MY_REPOS)
   .then((response) => response.json())
   .then((data) => {
         const forkedRepos = data.filter(isFork)
@@ -54,7 +44,7 @@ function addCommits(repos) {
  function addPullRequests(repos) {
     repos.forEach((repo) => {
         const pullRequestsUrl = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
-        fetch(pullRequestsUrl, config)
+        fetch(pullRequestsUrl)
             .then((response) => response.json())
             .then((data) => {
                 const myPullRequests = data.filter((pullRequest) => pullRequest.user.login === USER);
