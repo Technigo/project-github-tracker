a1c6cb76e39f76dee080ebb69fe5fac36225136c
Reinis <reinis@Reiniss-MacBook-Pro.local>
2021-10-04 15:13:34 +0200

    Styling and responsivnes fixed

--- code/chart.js
+++ code/chart.js
@@ -2,19 +2,21 @@
 const ctx = document.getElementById("chart").getContext("2d");
 
 //"Draw" the chart here 👇
-const config = {
-  type: "doughnut",
-  data: {
-    labels: ["Finished", "Upcoming"],
-    datasets: [
-      {
-        label: "Bootcamp projects",
-        data: [5, 15],
-        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
-        hoverOffset: 4,
-      },
-    ],
-  },
-};
+const drawChart = (amount) => {
+  const config = {
+    type: "doughnut",
+    data: {
+      labels: ["Finished", "Upcoming"],
+      datasets: [
+        {
+          label: "Bootcamp projects",
+          data: [amount, 20 - amount],
+          backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  };
 
-const MyChart = new Chart(ctx, config);
+  const MyChart = new Chart(ctx, config);
+};
--- code/script.js
+++ code/script.js
@@ -1,14 +1,11 @@
 const projects = document.getElementById("projects");
 const projectInfo = document.getElementById("projectsInfo");
-const userInfo = document.getElementById("userInfo");
-
-const API_KEY = "https://api.github.com/users/Meeteyes/repos";
-let API_KEY_PULL = "";
-let API_KEY_COMMENTS = [];
+const userInfo = document.getElementById("userHeader");
 
 //first function that runs a fetch and gets all the repos of the user
 // it returns an array with only Meeteyes technigo repos
 const getRepos = async () => {
+  const API_KEY = "https://api.github.com/users/Meeteyes/repos";
   const res = await fetch(API_KEY);
   const data = await res.json();
   let array = data.filter(
@@ -21,12 +18,11 @@ const getRepos = async () => {
 const fetchPulls = async (key) => {
   const res = await fetch(key);
   const data = await res.json();
-  let filteredArray = data.filter((item) => item.user.login === "Meeteyes");
-  return filteredArray[0];
-  // return filteredArray.url;
+  let myPull = data.find((item) => item.user.login === "Meeteyes");
+  return myPull;
 };
 
-// This function will get comments, commits, etc
+// This function will get commits
 const fetchDetails = async (key) => {
   const res = await fetch(key);
   const data = await res.json();
@@ -41,28 +37,27 @@ const callDataInOrder = async () => {
 
   // this displays the user information, picture and username:
   userInfo.innerHTML = `
+  <h1 class="heading-main">GitHub Tracker</h1>
   <img src=${myRepos[0].owner.avatar_url} alt="user picture" class="user-image" />
   <h2 class="user-name">${myRepos[0].owner.login}</h2>
   `;
 
   myRepos.forEach((repo) => {
-    API_KEY_PULL = `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`;
+    const API_KEY_PULL = `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`;
     const mySinglePull = fetchPulls(API_KEY_PULL);
     mySinglePull.then((data) => {
-      console.log(data);
       const API_KEY_COMMITS = data.commits_url;
       const comsPerProject = fetchDetails(API_KEY_COMMITS);
       comsPerProject.then((data) => {
+        console.log(data);
         document.getElementById(repo.name).innerHTML += `
         <p>Number of commits: ${data.length}</p>
+        <div> Last 3 commit messages:
+          <p>1 - ${data[data.length - 1].commit.message}</p>
+          <p>2 - ${data[data.length - 2].commit.message}</p>
+          <p>3 - ${data[data.length - 3].commit.message}</p>
+        </div>
         `;
-      });
-
-      //Haven't done anything with this yet
-      API_KEY_COMMENTS = data.comments_url;
-      const myComments = fetchDetails(API_KEY_COMMENTS);
-      myComments.then((data) => {
-        console.log(data);
       });
     });
 
@@ -75,8 +70,8 @@ const callDataInOrder = async () => {
     <div>
     `;
   });
+
+  drawChart(myRepos.length);
 };
 
 callDataInOrder();
-
-// getPullRequests(repoArray);
