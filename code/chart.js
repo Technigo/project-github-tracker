57beb46c2403cf000d2c266af940e27f4e779e08
mamite100 <mamite.andersson@student.kyh.se>
2021-10-05 00:51:16 +0200

    fixed the repos info

--- code/chart.js
+++ code/chart.js
@@ -1,25 +1,25 @@
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById('chart').getContext('2d');
 
 
-
-
-const drawChart = (number) => {
+const drawChart = (amount) => {
     const config = {
-      type: 'doughnut',
-      data: {
-        labels: ["Projects done", "Still to do"],
-        datasets: [
-          {
-            label: "My first Dataset", 
-            data: [number, 20 - number], // divide donut to 2 different parts
-            backgroundColor: [
-              "rgb(255, 99, 132)",
-              "rgb(54, 162, 235)",
-            ],
-            hoverOffset:4,
-          },
+        type: 'doughnut',
+        data: {
+        labels: [
+          'Completed projects',
+          'Projects left to build',
         ],
+        datasets: [{
+          label: 'My Technigo projects',
+          data: [amount, 19-amount],
+          backgroundColor: [
+            'rgb(219,57,141)',
+            'rgb(0,255,255)',
+          ],
+          hoverOffset: 4
+        }],
       },
-    }
-    const myChart = new Chart(ctx, config);
-  };
+    };
+
+    const projectsChart = new Chart(ctx, config);
+}
--- code/script.js
+++ code/script.js
@@ -1,43 +1,69 @@
-const USER= "mamite100";
-const REPOS_URL= `https://api.github.com/users/${USER}/repos`;
-const projectsContainer = document.
-getElementById("projects");
+/* my user*/
+/*can't get my USER*/ 
+const USER = 'mamite100';
+
+const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
+
+const PROFILE_URL = `https://api.github.com/users/${USER}`
+
+const profileContainer = document.getElementById('profile-container')
+const projectsContainer = document.getElementById('projects');
+
+const fetchProfile = () => {
+    fetch(PROFILE_URL)
+        .then(res => res.json()) 
+        .then(profileData => {
+        profileContainer.innerHTML +=`
+            <img src=${profileData.avatar_url} class='profile-img'>    
+            <h2>${profileData.name}</h2>
+            <p>${profileData.login}</p>`
+        });
+  }
 
 const fetchRepositories = () => {
-    fetch (REPOS_URL)
-        .then((res) => res.json())
-        .then((data) => {
-        const technigoRepositories = data.filter((repo) => repo.name.includes("project-") && repo.fork 
-        );
-      technigoRepositories.forEach(
-        (repo) => {
-        projectsContainer.innerHTML += `
-        <div>
-        <a href="${repo.html_url }" > ${repo.name} with default branch
-        ${repo.default_branch}</a>   
-        <p> Recent push: ${new Date (repo.pushed_at) .toDateString() } </p> 
-        <p id="commit-${repo.name}"> Commits: </p>   
-         </div> 
+	  fetch(REPOS_URL)
+		  .then((res) => res.json())
+		  .then((data) => {
+			const technigoRepositories = data.filter(
+				(repo) => repo.name.includes('project-') && repo.fork
+			);
+
+		technigoRepositories.forEach((repo) => {
+				projectsContainer.innerHTML += `
+          <div>
+            <a href="${repo.html_url}">${repo.name} with default branch ${
+					repo.default_branch
+				}</a>
+            <p>Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+            <p id="commit-${repo.name}">Commits amount: </p>
+          </div>
         `;
-        });
-        });
+
+			});
+
+		fetchPullRequestsArray(technigoRepositories);
+
+			//draw chart with technigoRepos data. I can't get the char? 
+			drawBarChart(technigoRepos.length);
+		});
 };
 
-const fetchPullRequestsArray = ( allRepositories) => {
-  allRepositories.forEach((repo) => {
-   const PULL_URL = 
-   `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
-   fetch (PULL_URL)
-    .then( (res) => res.json())
-    .then((data) => {
-              const myPullRequest = data.find(
-                (pull) => pull.user.login === repo.owner.login
-              );
-if (myPullRequest) {
+const fetchPullRequestsArray = (allRepositories) => {
+	allRepositories.forEach((repo) => {
+		const PULL_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
+
+		fetch(PULL_URL)
+			.then((res) => res.json())
+			.then((data) => {
+				const myPullRequest = data.find(
+					(pull) => pull.user.login === repo.owner.login
+				);
+
+				if (myPullRequest) {
 					fetchCommits(myPullRequest.commits_url, repo.name);
 				} else {
 					document.getElementById(`commit-${repo.name}`).innerHTML =
-						'No pull request yet done : ( ';
+						'No pull request yet done :(';
 				}
 			});
 	});
