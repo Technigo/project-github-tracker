bf3f1eba476f2317c2a1e2b0bd741b7cab9922cc
Emelie <pcruzemelie@gmail.com>
2021-10-08 13:56:50 +0200

    Followed Maks video, fetched repos, commits and chart

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,27 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+const drawChart = (amount) => {
+    const config = {
+        type: 'doughnut',
+        data: {
+        labels: [
+          'Completed projects',
+          'Projects left to build',
+        ],
+        datasets: [{
+          label: 'My Technigo projects',
+          data: [amount, 20-amount],
+          backgroundColor: [
+            'rgb(219,57,141)',
+            'rgb(0,255,255)',
+          ],
+          hoverOffset: 4
+        }],
+      },
+    };
+
+    const projectsChart = new Chart(ctx, config);
+}
+
+
---
+++ code/script.js
@@ -0,0 +1,71 @@
+const USER = 'pcruzem';
+const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
+
+const projectsContainer = document.getElementById('projects');
+
+const fetchRepositories = () => {
+    fetch (REPOS_URL)
+    .then((res)=> res.json())
+    .then((data) => {
+        const technigoRepositories = data.filter(
+            (repo) => repo.name.includes('project-') && repo.fork
+         );
+
+        technigoRepositories.forEach((repo) => {
+            projectsContainer.innerHTML += `
+        <div>
+            <a href="${repo.html_url}">${repo.name} with default branch ${
+                    repo.default_branch
+                }</a>
+            <p>Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+            <p id="commit-${repo.name}">Commits amount: </p>
+        </div>
+    `;
+
+        });
+
+        //Aproach number 2 
+        fetchPullRequestsArray(technigoRepositories);
+
+        //Draw chart with technigoRepos data
+        drawChart(technigoRepositories.length);
+   
+    });
+};
+
+const fetchPullRequestsArray = (allRepositories) => {
+    allRepositories.forEach((repo) => {
+        const PULL_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?
+        per_page=100`;
+
+        fetch(PULL_URL)
+        .then((res) => res.json()) 
+        .then((data) => {
+            const myPullRequest = data.find(
+                (pull) => pull.user.login === repo.owner.login
+            );
+            console.log(myPullRequest);
+
+            // Detect if we have pull request or not.
+			// If yes - call fetchCommits function
+			// If no - inform user that no pull request was yet done
+			if (myPullRequest) {
+				fetchCommits(myPullRequest.commits_url, repo.name);
+			} else {
+				document.getElementById(`commit-${repo.name}`).innerHTML =
+                'This was a group project. No pull requests made by pcruzem.';
+			}
+        });
+    });
+};
+
+const fetchCommits = (myCommitsUrl, myRepoName) => {
+	fetch(myCommitsUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			document.getElementById(`commit-${myRepoName}`).innerHTML += data.length;
+		});
+};
+
+
+fetchRepositories ();
