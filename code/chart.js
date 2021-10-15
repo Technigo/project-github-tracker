529a58bc8095d8536d860cce66222dce99b250bd
isomoth <ic.gonzalez35@hotmail.com>
2021-10-15 21:02:56 +0200

    fixed forkedRepos function and fixed indentation

--- code/chart.js
+++ code/chart.js
@@ -1,49 +1,39 @@
-// DOM-selector for the canvas 
+// DOM-selector for the canvas
 const ctx = document.getElementById('myChart').getContext('2d');
 
 // Generate the chart based on the data fetched on script.js
-  const drawChart = (amount) => {
-    const config = {
-        type: 'bar',
-        data: {
-            labels: [
-              'Finished Projects',
-              'Projects Left',
-            ],
-            datasets: [{
-            label: 'Completed Projects',
-              data: [amount, 19-amount],
-              backgroundColor: [
-                '#D365C8',
-                '#3aafc9',
-              ],
-              hoverOffset: 4
-            }]
+const drawChart = (amount) => {
+  const config = {
+    type: 'bar',
+    data: {
+      labels: ['Finished Projects', 'Projects Left'],
+      datasets: [
+        {
+          label: 'Completed Projects',
+          data: [amount, 19 - amount],
+          backgroundColor: ['#D365C8', '#3aafc9'],
+          hoverOffset: 4,
+        },
+      ],
+    },
+    options: {
+      // The first two options make the chart responsive
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return tooltipItem.yLabel;
+            },
           },
-          options: {
-            // The first two options make the chart responsive
-            responsive: true,
-            maintainAspectRatio: false,
-            plugins: {
-              legend: {
-                display: false,
-              },
-                tooltips: {
-                  callbacks: {
-                    label: function(tooltipItem) {
-                    console.log(tooltipItem)
-                      return tooltipItem.yLabel;
-                    },
-                },
-              }, 
-            }
-          }
-      };
-    
-    const myChart = new Chart(
-        ctx,
-        config
-      );
-  }
+        },
+      },
+    },
+  };
 
-  
+  const myChart = new Chart(ctx, config);
+};
--- code/script.js
+++ code/script.js
@@ -1,5 +1,5 @@
-// The strict mode in JavaScript prevents several bugs. For example, it won't let you use undeclared variables. 
-"use strict";
+// The strict mode in JavaScript prevents several bugs. For example, it won't let you use undeclared variables.
+'use strict';
 
 // Global variables
 const USER = 'isomoth';
@@ -33,7 +33,7 @@ getUserProfile();
 // Fetch all of the user's projects where a pull request has been made
 const getPullRequests = (allRepos) => {
   allRepos.forEach((repo) => {
-    const PULL_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`
+    const PULL_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
     fetch(PULL_URL)
       .then((response) => response.json())
       .then((data) => {
@@ -41,11 +41,12 @@ const getPullRequests = (allRepos) => {
           (pull) => pull.user.login === repo.owner.login
         );
         // Filter out pull requests from other users and fetch commits from them
-        if(userPullRequest){
+        if (userPullRequest) {
           getCommits(userPullRequest.commits_url, repo.name);
         } else {
           // Catch exception when no pull request has been made
-          document.getElementById(`commit-${repo.name}`).innerHTML = 'No pull requests so far.';
+          document.getElementById(`commit-${repo.name}`).innerHTML =
+            'No pull requests so far.';
         }
       });
   });
@@ -53,15 +54,15 @@ const getPullRequests = (allRepos) => {
 
 //Fetch all commits from filtered projects
 const getCommits = (myCommitsUrl, myRepoName) => {
-	fetch(myCommitsUrl) // this will be 'commits_url' from line 45
-		.then((res) => res.json())
-		.then((data) => {
-      // Count amount of commits 
-			document.getElementById(`commit-${myRepoName}`).innerHTML += data.length;
-		});
+  fetch(myCommitsUrl) // this will be 'commits_url' from line 45
+    .then((res) => res.json())
+    .then((data) => {
+      // Count amount of commits
+      document.getElementById(`commit-${myRepoName}`).innerHTML += data.length;
+    });
 };
 
-// Fetch all repositories that the user has forked from Technigo 
+// Fetch all repositories that the user has forked from Technigo
 const getRepositories = () => {
   fetch(REPOS_URL)
     .then((response) => response.json())
@@ -71,27 +72,24 @@ const getRepositories = () => {
       );
       // Sort repos based on date of creation ("created_at")
       forkedRepos.sort(
-        (a, b) => new Date(b.created_at) - new Date(a.created_at)
+        (b, a) => new Date(a.created_at) - new Date(b.created_at)
       );
-      // Display them from earliest to latest
-      forkedRepos = forkedRepos.reverse();
       // Finally, use a loop to generate a card container for each project and fetch other relevant info about it
-      forkedRepos.forEach(
-        (repo) => {
-          projectsContainer.innerHTML += 
-          `<div class="repo-card">
-            <h3><a class="repo-link" href="${repo.html_url}">${repo.name}</a><h3>
+      forkedRepos.forEach((repo) => {
+        projectsContainer.innerHTML += `<div class="repo-card">
+            <h3><a class="repo-link" href="${repo.html_url}" target="_blank">${
+          repo.name
+        }</a><h3>
             <span>
             <p>Default branch: ${repo.default_branch}</p>
             <p>Latest push: ${new Date(repo.pushed_at).toDateString()}</p>
             <p id="commit-${repo.name}">Amount of commits: </p>
             <span>
-          </div>`
-        },
-      );
+          </div>`;
+      });
       getPullRequests(forkedRepos),
-      //Use the data above to generate the chart from chart.js, displaying elapsed vs. remaining projects
-      drawChart(forkedRepos.length);
+        //Use the data above to generate the chart from chart.js, displaying elapsed vs. remaining projects
+        drawChart(forkedRepos.length);
     });
 };
 
