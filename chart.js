73bfa9e3266ae8b49f909de270c691a3086d1d94
Jsfrulle <Jessicamhermansson@gmail.com>
2021-12-16 10:29:26 +0100

    fix

--- chart.js
+++ chart.js
@@ -3,26 +3,24 @@ const ctx = document.getElementById("chart").getContext("2d");
 
 Chart.defaults.font.size = 26;
 
-
-
 const drawChart = () => {
-	const config = {
-		type: "doughnut",
-		data: {
-			labels: ["Finished projects", "projects left"],
-			datasets: [
-				{
-					label: "My First Dataset",
+  const config = {
+    type: "doughnut",
+    data: {
+      labels: ["Finished projects", "projects left"],
+      datasets: [
+        {
+          label: "My First Dataset",
 
-					data: [5, 20 - 5],
+          data: [5, 20 - 5],
 
-					backgroundColor: ["rgba(86, 114, 85, 1)", "rgb(86, 114, 85, 0.7)"],
+          backgroundColor: ["rgba(86, 114, 85, 1)", "rgb(86, 114, 85, 0.7)"],
 
-					hoverOffset: 4,
-				},
-			],
-		},
-	};
+          hoverOffset: 4
+        }
+      ]
+    }
+  };
 
-	const myChart = new Chart(ctx, config);
+  const myChart = new Chart(ctx, config);
 };
