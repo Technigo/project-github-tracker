f23957d1bb28cd3885b6754a5b2977e1c38a87b6
Rebecca Blixt <rebecca.blixt@hotmail.com>
2021-12-21 21:03:26 +0100

    added readme file. added comments in code

--- code/script.js
+++ code/script.js
@@ -2,9 +2,11 @@ const USER = 'BritishSwede';
 const USER_URL = `https://api.github.com/users/${USER}`;
 const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
 
+// Used to write innerHTML after fetches
 const profileInfo = document.getElementById('profileInfo');
 const projectsContainer = document.getElementById('projectsContainer');
 
+// Fetches user info from GitHub profile
 const userProfile = () => {
 	fetch(USER_URL)
 		.then((res) => res.json())
@@ -16,32 +18,37 @@ const userProfile = () => {
             ${data.login}
           </span>
         </a>
-			       `;
+		  `;
 		});
 };
 
 userProfile();
 
-// Fetching all repos done by me
+// Fetching all repos done by user
 const getRepos = () => {
 	fetch(REPOS_URL)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data);
+			// filters Technigo projects from other GitHub repos
 			const forkedRepos = data.filter(
 				(repo) => repo.fork && repo.name.startsWith('project-')
 			);
 
+			// Inserts fetched info from API into HTML
 			forkedRepos.forEach(
 				(repo) =>
 					(projectsContainer.innerHTML += `
-        <div class="repo-card">
-        <a href="${repo.html_url}"><h3>${repo.name}</h3></a>
-        <p>Default Branch: <span>${repo.default_branch}</span></p>
-        <p>Recent Push: ${new Date(repo.pushed_at).toDateString()}</p>
-        <p id="commit-${repo.name}">Amount of Commits: </p>
-        </div>
-        `)
+            <div class="repo-card">
+              <a href="${repo.html_url}">
+                <h3>
+                  ${repo.name}
+                </h3>
+              </a>
+              <p>Default Branch: <span>${repo.default_branch}</span></p>
+              <p>Recent Push: ${new Date(repo.pushed_at).toDateString()}</p>
+              <p id="commit-${repo.name}">Amount of Commits: </p>
+            </div>
+          `)
 			);
 			getPullRequests(forkedRepos);
 			drawChart(forkedRepos.length);
@@ -61,6 +68,7 @@ const getPullRequests = (repos) => {
 					(pull) => pull.user.login === repo.owner.login
 				);
 
+				// If pull request done by user, getCommits function is invoked
 				if (myPullRequest) {
 					getCommits(myPullRequest.commits_url, repo.name);
 				} else {
@@ -71,7 +79,7 @@ const getPullRequests = (repos) => {
 	});
 };
 
-// Commits for each projects
+// If commits done by user, number of commits is added in HTML
 const getCommits = (myCommitsUrl, myRepoName) => {
 	fetch(myCommitsUrl)
 		.then((res) => res.json())
