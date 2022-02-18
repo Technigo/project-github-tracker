b1bf1a53d470e9ba485c0caa9b918cbedec5aff1
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-02-18 17:17:49 +0100

    added a basic graph with projects

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,64 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+
+
+// what's not working below is commented out
+// global options
+// Chart.defaults.global.defaultFontFamily = 'Lato';
+// Chart.defaults.global.defaultFontSize = 18;
+// Chart.defaults.global.defaultFontColor = 'grey';
+
+const drawProjectsChart = (amount) => {
+    new Chart(ctx, {
+        type: 'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data: {
+            labels: ['Completed', 'To do'],
+            datasets: [{
+                barPercentage: 1,
+        barThickness: 100,
+        maxBarThickness: 200,
+        minBarLength: 2,
+                label: 'Projects',
+                data: [
+                    amount,
+                    19 - amount
+                ],
+                backgroundColor: [
+                    'red',
+                    'green'
+                ],
+                borderWidth: 2,
+                borderColor: 'black',
+                hoverBorderWidth: 5,
+                hoverBorderColor: 'brown'
+            }
+        ]
+        }
+        // options: {
+            // title: {
+            //     display: true,
+            //     text: 'Largest Cities in Massachussets',
+            //     fontSize:24
+            // },
+            // legend:{
+            //     display:false,
+            //     position:'right',
+            //     labels:{
+            //         fontColor:'black'
+            //     }
+            // layout: {
+            //     padding: {
+            //         left: 0,
+            //         right: 0,
+            //         bottom: 0,
+            //         top: 0
+            //     }
+                // },
+                // tooltips:{
+                //     enabled:false
+            // }
+        // }
+    })
+}
+
--- code/script.js
+++ code/script.js
@@ -1,47 +1,92 @@
+const username = 'nadialefebvre'
+const API_USER = `https://api.github.com/users/${username}`
+const API_REPOS = `https://api.github.com/users/${username}/repos` // const or let ?
+const profile = document.getElementById('profile')
 const projects = document.getElementById('projects')
-const API_REPOS = 'https://api.github.com/users/nadialefebvre/repos'
 
-const options = {
-    method: 'GET',
-    headers: {
-        Authorization: 'token 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'
-    }
+
+
+
+
+const createProfile = () => {
+    fetch(API_USER)
+        .then(res => res.json())
+        .then(data => {
+            profile.innerHTML = `
+                <h2>${data.name}</h2>
+                <img src="${data.avatar_url}" class="profile-picture">
+            `
+        })
 }
 
-fetch(API_REPOS, options) // options object is passed as 2nd argument to fetch() function.
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        projects.innerHTML = `
-            <img src="${data[0].owner.avatar_url}">
-            <p>Username: ${data[0].owner.login}</p>
-        `
-        const filteredRepos = data.filter(repo => repo.fork === true && repo.name.startsWith('project'))
-        console.log(filteredRepos)
-        filteredRepos.forEach((repo) => {
-            fetch(`https://api.github.com/repos/nadialefebvre/${repo.name}`, options)
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    console.log(repo.pushed_at)
-                    const options = { day: 'numeric', month: 'long', year: 'numeric' }
-                    projects.innerHTML += `
+
+
+
+
+
+
+
+const createRepoCard = () => {
+    fetch(API_REPOS) // options object is passed as 2nd argument to fetch() function. // TO REMOVE BEFORE GIT PUSH
+        .then(res => res.json())
+        .then(data => {
+            const filteredRepos = data.filter(repo => repo.fork === true && repo.name.startsWith('project-'))
+            drawProjectsChart(filteredRepos.length)
+            filteredRepos.forEach(repo => {
+                const options = { day: 'numeric', month: 'long', year: 'numeric' }
+                projects.innerHTML += `
                     <div class="repo" id=${repo.name}>
                         <p>Repo name: ${repo.name}</p>
-                        <p>Repo URL: ${repo.html_url}</p>
+                        <p>Repo URL: <a href="${repo.html_url}">${repo.name}</a></p>
                         <p>Default branch: ${repo.default_branch}</p>
                         <p>Repo last push: ${new Date(repo.pushed_at).toLocaleDateString('en-GB', options)}</p>
                     </div>
                 `
-                })
-            fetch(`https://api.github.com/repos/nadialefebvre/${repo.name}/commits`)
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    console.log(data.length)
-                    document.getElementById(`${repo.name}`).innerHTML += `
-                        <p>Number of commits: ${data.length}</p>
-                    `
-                })
+                fetch(`https://api.github.com/repos/${username}/${repo.name}/commits`)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        const userCommits = data.filter((item) => item.commit.author.name === 'Nadia Lefebvre')
+
+                        document.getElementById(repo.name).innerHTML += `
+                            <p>Number of commits from the user: ${userCommits.length} on a total of ${data.length}</p>
+                            <p>Commit messages: </p>
+                        `
+                        userCommits.forEach(item => {
+                            document.getElementById(repo.name).innerHTML += `
+                            <li>${item.commit.message}</li>
+                        `
+                        })
+                    })
+            })
         })
+}
+
+
+createProfile()
+createRepoCard()
+
+
+
+
+
+
+//Remember to pass along your filtered repos as an argument when
+//you are calling this function
+
+const getPullRequests = (repos) => {
+    //Get all the PRs for each project.
+    repos.forEach(repo => {
+      fetch('https://api.github.com/repos/technigo/' + repo.name + '/pulls')
+      .then(res => res.json())
+      .then(data => {
+              //TODO
+          //1. Find only the PR that you made by comparing pull.user.login
+              // with repo.owner.login
+              //2. Now you're able to get the commits for each repo by using
+              // the commits_url as an argument to call another function
+              //3. You can also get the comments for each PR by calling
+              // another function with the review_comments_url as argument
+          })
     })
+  }
