ca37893d29b6def6f33397fc8f93b04058852245
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2022-02-21 17:15:30 +0100

    fetching userProfile and avatar

--- code/script.js
+++ code/script.js
@@ -1,19 +1,49 @@
 const projects = document.getElementById('projects')
 
 const username = 'SteppbySteph'
-const USER_URL = `https://api.github.com/users/${username}/repos`
+const USER_PROFILE = `https://api.github.com/users/${username}`
+const USER_REPO = `https://api.github.com/users/${username}/repos`
 
 //const USER_URL =('https://api.github.com/users/SteppbySteph/repos')
 // const REPOS_URL = 
 
 
+//----USER NAME & PROFILE----//
+
+const getProfile = () =>{
+    fetch(USER_PROFILE)
+        .then(res => res.json())
+        .then(data => {
+            console.log('user', data)
+
+            const user = data.login
+            const avatar = data.avatar_url
+            projects.innerHTML += `
+            <h1>Username: ${user}</h1>
+            <img class="avatar" src="${avatar}">
+            `
+
+            
+        }) 
+}
+
+getProfile()
+
+
+
+
 //----FETCHING ALL REPOS----//
 const fetchUserRepos = () => {
-    fetch(USER_URL)
+    fetch(USER_REPO)
         .then(res => res.json())
         .then(data => {
             console.log('repos', data)
+
+            const allRepos = data.length
+            projects.innerHTML += `${allRepos}`  
         })
+
+    
 }
 
 fetchUserRepos()
