bb8facabc9fbd3032053c26d63fef5e484d763aa
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-02-21 20:01:21 +0100

    Display repository data

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 const profileDetails = document.getElementById("aside");
 const projects = document.getElementById("projects");
 
-const username = "savannah-hayes"
-const USER_URL = `https://api.github.com/users/${username}`
+const username = "savannah-hayes";
+const USER_URL = `https://api.github.com/users/${username}`;
+const REPOS_URL = `https://api.github.com/users/${username}/repos`;
 
 const displayProfileData = (data) => {
-  console.log(data)
   const image = data.avatar_url;
   const name = data.name;
   const username = data.login;
@@ -24,7 +24,32 @@ const displayProfileData = (data) => {
   `;
 }
 
+const displayRepositories = (repositories) => {
+  console.log(repositories);
+  repositories.filter(repo => {
+    let language = repo.language;
+    const visibility = repo.visibility;
+    const oneDay = 24 * 60 * 60 * 1000;
+    const currentDate = new Date();
+    const projectDate = new Date(repo.pushed_at);
+    let numberOfDays = Math.round(Math.abs((currentDate - projectDate) / oneDay));
+
+    language === "HTML" ? language = `🔴 ${language}` : language = `🟡 ${language}`;
+
+    if (repo.fork === true && repo.name !== "unit-tests") {
+      projects.innerHTML += `
+      <p>${repo.name} ${visibility}</p>
+      <p>Forked from Technigo/project-${repo.name}</p>
+      <p>${language} Updated ${numberOfDays} days ago</p>
+      `;
+    }
+  })
+}
 
 fetch(USER_URL)
   .then(res => res.json())
   .then(displayProfileData)
+
+fetch(REPOS_URL)
+  .then(res => res.json())
+  .then(displayRepositories)
