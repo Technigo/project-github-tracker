dda965468ffe6c9deb6339f94bce13123f1ee4da
EmmaaBerg <emmaberg0819@gmail.com>
2022-02-22 20:00:08 +0100

    Filtered the forked projects from Technigo

--- code/script.js
+++ code/script.js
@@ -5,4 +5,14 @@ fetch(API_URL_REPOS)
 .then((resp) => resp.json())
 .then((allRepos) =>{
     console.log(allRepos)
+
+    //A function for filtering out the forked projects from technigo.
+    //Repo is the "name for each object" in the array. Fork and name are two properties within
+    // the object 
+    const forkedRepos = allRepos.filter((repo) => repo.fork && repo.name.startsWith('project-'))
+        console.log(forkedRepos);
 })
+
+
+
+
