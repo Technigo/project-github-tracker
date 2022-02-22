ca783f4a9d001797edae92d1d996a0f3871f3c33
Emma Högberg <emmahogberg88@gmail.com>
2022-02-22 08:24:03 +0100

    add fetch for commits and comments

--- code/script.js
+++ code/script.js
@@ -1,31 +1,28 @@
 //Global variables
 const username = 'emmahogberg88'
 const URL_REPO = `https://api.github.com/users/${username}/repos`
-
+const TOKEN = 
 //option for authorization
 const options = {
   method: 'GET',
   headers: {
-    Authorization: 'token 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' // you need to paste your token over here.
+    Authorization: `token ${TOKEN}`  // you need to paste your token over here.
   }
 }
 
 
-
-//fetch data from GitHub repo
+//fetch data from GitHub repo//
 const gitHubData = () => {
 
 fetch(URL_REPO, options)
   .then(res => res.json())
   .then(data => {
-  console.log('data:', data)
 
   getUserData(data)
   getRepoData(data)
 
   })
 }
-
 
 
 const getUserData = (data) => {
@@ -40,46 +37,83 @@ const getUserData = (data) => {
   `
 }
 
+
 const getRepoData = (data) => {
-//Filter all technigoProjects to get specific info about each repo.
+
+  //Filter all technigoProjects to get specific info about each repo.
 //Get technigo projects by filtering data by repos that starts with "project-"
   const technigoProjects = data.filter(repo => repo.name.startsWith('project-'))
   console.log('array with technigo projects:', technigoProjects)
 
   //Loop through array to get data about each item in array
-  technigoProjects.forEach(project => {
+  technigoProjects.forEach(repo => {
     
     //Get name of repo
-  let reponame = project.name
-  console.log('project reponame:', reponame)
+  let reponame = repo.name
 
   //Get url to each repo 
-  let projectUrl = project.html_url
-  console.log('project url:', projectUrl)
-
-  let defaultBranch = project.default_branch
-  console.log('default branch:', defaultBranch)
+  let projectUrl = repo.html_url
+  
+  //Get default branch
+  let defaultBranch = repo.default_branch
   
   //Get the date of latest update of the repo
-  let latestUpdateRepo = new Date(project.updated_at).toLocaleDateString('en-GB', {year: 'numeric', month: 'long', day: 'numeric'})
-  console.log('latest update of repo:',latestUpdateRepo)
+  let latestUpdateRepo = new Date(repo.updated_at).toLocaleDateString('en-GB', {year: 'numeric', month: 'long', day: 'numeric'})
   
+  //Display HTML for all GitHub repos on website, setting dynamic ID to be able to add on more HTML in another function
   document.getElementById('section-projects').innerHTML += `
   <div class="section-projects__card flex">
     <a class="reponame-link" href='${projectUrl}'>${reponame}</a>
-    <p>Default branch: ${defaultBranch}</p>
-    <p>Latest update: ${latestUpdateRepo}</p>
+    <p class="repo-info">Default branch: ${defaultBranch}</p>
+    <p class="repo-info">Latest update: ${latestUpdateRepo}</p>
+    <p class="repo-info" id=commits-${repo.name}>Commits</p>
+    <p class="repo-info" id=comments-${repo.name}>Comments</p>
+  
   </div>  
   `
-  
-  const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
 
-  fetch(API_URL_PR, options)
+  //Fetch pull request data
+  fetch(`https://api.github.com/repos/Technigo/${reponame}/pulls?per_page=100`, options)
     .then(res => res.json())
     .then(data => {
-        console.log('data from Technigo', data)
-        // if (data.user.login === )
+      data.forEach( repo => {
+        if(repo.user.login === username) {
+          console.log('data from Technigo', data)
+          let reponame = repo.base.repo.name 
+          //get url for commits to use in new fetch
+          const commitsUrl = repo.commits_url
+          getCommits(commitsUrl, reponame)        
+          //get url for comments to use in new fetch  
+          const commentsUrl = repo.review_comments_url
+          getComments(commentsUrl, reponame) 
+
+        } else if (repo.user.login === 'tiiliu' && reponame === 'project-chatbot') {
+          getCommits(repo.commits_url, 'project-chatbot')        
+            
+        } else {
+          document.getElementById(`commits-${reponame}`).innerHTML = 'No commits for this repo'
+          document.getElementById(`comments-${reponame}`).innerHTML = ''
+        }
+      })
     })
+  })
+}
+
+const getCommits = (commitsUrl, reponame) => {
+  fetch(commitsUrl, options)
+  .then(res => res.json())
+  .then(data => {
+    console.log('commits', data, reponame)
+    document.getElementById(`commits-${reponame}`).innerHTML = `Amount of commits: ${data.length}`
+  })
+}
+
+const getComments = (commentsUrl, reponame) => {
+  fetch(commentsUrl, options)
+  .then(res => res.json())
+  .then(data => {
+    console.log('comments', data, reponame)
+    document.getElementById(`comments-${reponame}`).innerHTML = `Comments from: ${data[0].user.login}`
   })
 }
 
