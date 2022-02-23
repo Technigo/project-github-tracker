25f2c5424a7c243c09724cc48ef2299e4d59f3fc
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-02-23 22:43:39 +0100

    step 2 and 3 done - filter out the forked repos / projects from Technigo

--- code/script.js
+++ code/script.js
@@ -1,9 +1,17 @@
 const username = 'CamillaHallberg'
-const API_REPOS_URL = `https://api.github.com/users/${username}/repos`
+let reponame = ''
 
-fetch(API_REPOS_URL)
+const API_URL = `https://api.github.com/users/${username}/repos`
+const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
+
+
+fetch(API_URL)
 .then(res => res.json())
 .then(data => {
     console.log(data, 'my repos');
-})
+    // below I'm filtering out the forked repos from Technigo.
+    const technigoRepos = data.filter(
+        (repo) => repo.name.includes('project-') && repo.fork);
+        console.log(technigoRepos, 'technigo repos') 
 
+})  
