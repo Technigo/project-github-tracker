e206646408ac0ebde57c42007da1a06548d487ff
anki palviainen <anki.palviainen@gmail.com>
2022-02-23 14:08:45 +0200

    try to code

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,33 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'black',
+      borderColor: 'grey',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+  
+  const myChart = new Chart(
+    document.getElementById('chart'),
+    config
+  );
+
--- code/script.js
+++ code/script.js
@@ -1,39 +1,75 @@
-const API_URL = 
-let reponame
-
-
-fetch(API_URL)
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    console.log(data)
-    const technigoRepos = data.filter((repo) => repo.name.includes('project-'))
-
-    console.log(technigoRepos)
-
-
-    reponame = data[3].name
-    console.log(reponame)
-
-   
- const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`  
-   
-    fetch(API_URL_PR)
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    console.log(data)
-})
+let USER = 'qwer-ctrl'
 
 
 
+const profile = document.getElementById('profile-container')
+const projects = document.getElementById('projects')
 
+const fetchProfile = () => {
+fetch(USER_URL)
+    .then((response) => response.json())
+    .then((data) => {
+     profile.innerHTML += `
+        <div id="profile-container" class="profile">
+        <div class="profile-image">
+        <a href="${USER_URL}">
+            <img src="${data.avatar_url}" alt="Avatar of ${data.login}">
+            </a>
+            </div>
+            <h1 class="username">${data.login}<h1>
+        </div>
+        <div>
+        <p>Public repositories: ${data.public_repos}</p>
+        </div>
+        `
+        })
+}
+fetchProfile()
 
+const fetchRepos = () => {
+fetch(REPOS_URL)
+    .then((response) => response.json())
+    .then((data) => {
+     const technigoRepos = data.filter(
+    (repo) => repo.name.includes('project-') && repo.fork)
+        technigoRepos.forEach((repo) => {
+        projects.innerHTML += `
+            <div class="projects-card" id="${repo.id}">
+                <h1><a href="${repo.html_url}"><b>${repo.name}</b></a></h1>
+                <p>Update: ${new Date(repo.pushed_at).toDateString()}</p>
+                <p>Branch: ${repo.default_branch}</p>
+                <p id="commit_${repo.name}">Commits: </p>
+                <p>${repo.language}</p>
+                </div>
+                `
+            })
+ fetchPullRequests(technigoRepos)
+        })
+}
 
+fetchRepos()
+    /*const fetchPullRequests = (repositories) => {
+     repositories.forEach((repo) => {
+     fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
+        .then((response) => response.json())
+        .then((data) => {
+        const pullRequest = data.find((pull) => pull.user.login === repo.owner.login)
+            if (pullRequest) {
+                fetchCommits(pullRequest.commits_url, repo.name)
+            } else {
+                document.getElementById(`commit_${repo.name}`)
+                .innerHTML = 'No pull reguest';
+            }
+                })
+        })
+    }
 
+        const fetchCommits = (commitsUrl, repoName) => {
+            fetch(commitsUrl)
+                .then((response) => response.json())
+                .then((data) => {
+                    document.getElementById(`commit_${repoName}`).innerHTML += data.length
+                })
+        }
 
-})
-
-
+fetchRepos()*/
