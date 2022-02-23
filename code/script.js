89e9e119f99bcd60dd32bab19a135efd08a6682e
idanaslund <idanaslund@users.noreply.github.com>
2022-02-23 15:49:22 +0100

    removed node, fetched profile and sorted repos

--- code/script.js
+++ code/script.js
@@ -1,50 +1,57 @@
 // DOM selectors
 const projects = document.getElementById('projects')
 const username = 'idanaslund'
-
-
-//const API_TOKEN = TOKEN OR process.env.API_KEY
-//console.log(TOKEN)
+const profile = document.getElementById('profile')
 
 let API_URL = `https://api.github.com/users/${username}/repos`
 
+
 //Get the token here!!
-//const options = {
-   // method: 'GET',
-   // headers: {
-        //Authorization: `token ${API_TOKEN}`
-    //}
-//}
+const options = {
+    method: 'GET',
+    headers: {
+        Authorization: `token ${API_TOKEN}`
+    }
+}
 
 const addingProfile = () => {
-    profile.innerHTML += `Username: ${username}`
+    fetch(`https://api.github.com/users/${username}`, options)
+    .then((res) => res.json())
+    .then((profileInfo) => {
+        console.log (profileInfo)
+        profile.innerHTML += `
+        <img src="${profileInfo.avatar_url}">
+         <a href="${profileInfo.html_url}">${profileInfo.login}</a>`
+    }) 
 }
 addingProfile()
 
 const findingAllRepos = () => {
 
-fetch(API_URL)
+fetch(API_URL, options)
 .then((res) => res.json())
 .then((repos) => {
     console.log(repos)
+    console.log(API_TOKEN)
 
-
-    repos.forEach((repo) => {
+    const forkedRepos = repos.filter(
+        repo => repo.fork && repo.name.startsWith('project-')
+        ) 
+        
+    forkedRepos.forEach((repo) => {
     projects.innerHTML += `<div class="repos"><h4>${repo.name}<h4></div>`}) 
 })
 
  //How to get the reponame here?
- let API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
+ //let API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
 
-    fetch(API_URL_PR)
-     .then((res) => res.json())
-     .then((pulls) => {
-    console.log(pulls)
+   // fetch(API_URL_PR)
+    // .then((res) => res.json())
+    // .then((pulls) => {
+   // console.log(pulls)
     
 
- })
-
-}
+ }
 findingAllRepos()
 
 
