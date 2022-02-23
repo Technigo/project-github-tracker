1fe3a5380a77844832014e7e67b0858ed411ec3a
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-02-23 16:07:39 +0100

    called out the token varable

--- code/script.js
+++ code/script.js
@@ -2,7 +2,7 @@ const username = "Mimmisen";
 let reponame = "";
 const API_URL = `https://api.github.com/users/${username}/repos`;
 
-const API_TOKEN = TOKEN || process.env.API_KEY; // variable called TOKEN is local OR API_KEY is on the deployed site
+//const API_TOKEN = TOKEN || process.env.API_KEY; // variable called TOKEN is local OR API_KEY is on the deployed site
 console.log(TOKEN);
 
 const options = {
