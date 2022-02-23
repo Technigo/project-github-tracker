95fb7754f7cff315c98907691bf1ebe50e9be867
idanaslund <idanaslund@users.noreply.github.com>
2022-02-23 10:06:27 +0100

    tried another fetch

--- code/script.js
+++ code/script.js
@@ -18,11 +18,23 @@ fetch(API_URL)
 
 
     repos.forEach((repo) => {
-    projects.innerHTML += `<div class="repos"><h4>${repo.name}<h4></div>`})
-  })
+    projects.innerHTML += `<div class="repos"><h4>${repo.name}<h4></div>`}) 
+})
+
+ //How to get the reponame here?
+ let API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
+
+    fetch(API_URL_PR)
+     .then((res) => res.json())
+     .then((pulls) => {
+    console.log(pulls)
+    
+
+ })
 
 }
-
 findingAllRepos()
+
+
 
 // Eventlisteners
