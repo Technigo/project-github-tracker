9bae35495cbda2d98cb4dc13406bd949177f5625
TereseClaesson <terese.claesson@gmail.com>
2022-02-23 21:54:56 +0100

    Added innerHTML to the javascript to display the different projects and default branch,link to github projects  on the page

--- code/script.js
+++ code/script.js
@@ -1,27 +1,25 @@
-const userProfile = document.getElementById('profile')
-
+const userProfile = document.getElementById("profile");
+const allProjects = document.getElementById("projects");
 
 const username = "TereseClaesson";
 //let reponame = ''
 
 //All my repos from github
-const API_USER = `https://api.github.com/users/${username}`
-const API_URL = `https://api.github.com/users/${username}/repos`
+const API_USER = `https://api.github.com/users/${username}`;
+const API_URL = `https://api.github.com/users/${username}/repos`;
 //const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
 
-
-
 const userInfo = () => {
-    fetch(API_USER)
+  fetch(API_USER)
     .then((res) => res.json())
-    .then(profile => {
-        userProfile.innerHTML += `
+    .then((profile) => {
+      console.log(profile);
+      userProfile.innerHTML += `
         <img src = ${profile.avatar_url}>
         <h3>${profile.name}</h3>
-        `
-    }) 
-}
-
+        `;
+    });
+};
 
 fetch(API_URL)
   .then((Response) => Response.json())
@@ -35,36 +33,42 @@ fetch(API_URL)
     //const repoForked = repos.filter(repoFork => repoFork.fork === true)
 
     const repoForked = repos.filter(
-      (repo) => repo.fork && repo.name.startsWith("project-"));
+      (repo) => repo.fork && repo.name.startsWith("project-")
+    );
+    repoForked.forEach((repo) => {
+      allProjects.innerHTML += `
+        <div>
+        <p>${repo.name}</p>
+        <h3>PUSH ${repo.pushed_at}</h3> //slice -take away the last part of push
+        <p> main branch ${repo.default_branch}</p>
+        <a href = ${repo.html_url}><p>${repo.name}</p></a>
+        <p>${repo.commits_url}</p>
+        </div>
+        `;
+    });
     console.log(repoForked);
+
     getPullRequest(repoForked);
-
-    /*reponame = repos.name
-console.log(reponame)*/
-
-    // declare after the data(repos in the case) been fetch, calling the const fetch on the top
-    /*const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame/pulls`
-
-fetch(API_URL_PR)
-.then(Response => Response.json())
-.then(repos => {
-console.log(repos)
-
-
-})*/
   });
 
 const getPullRequest = (repoForked) => {
   repoForked.forEach((repo) => {
-    fetch('https://api.github.com/repos/Technigo/' + repo.name + '/pulls')
+    fetch("https://api.github.com/repos/Technigo/" + repo.name + "/pulls")
       .then((res) => res.json())
       .then((pullreqs) => {
-        //console.log(pullreqs)
+        console.log(pullreqs)
         pullreqs.forEach((pull) => {
           if (pull.user.login === username) {
             console.log(pull);
+            
             comments(pull);
             commits(pull);
+
+            repoForked.innerHTML += `
+            <div> 
+            <p>Commit messages ${pullreqs.commitNumber}</p>
+            </div>
+            `
           }
         });
       });
@@ -72,19 +76,19 @@ const getPullRequest = (repoForked) => {
 };
 
 const comments = (pullReq) => {
-     fetch(pullReq.review_comments_url)
-      .then((res) => res.json())
-      .then((commentrev) => {
-        console.log(commentrev)
-    })
-}
+  fetch(pullReq.review_comments_url)
+    .then((res) => res.json())
+    .then((commentrev) => {
+      console.log(commentrev);
+    });
+};
 
 const commits = (pullReq) => {
-    fetch(pullReq.commits_url)
+  fetch(pullReq.commits_url)
     .then((res) => res.json())
-    .then((commitNumber)=> {
-        console.log(commitNumber)
-    })
-}
+    .then((commitNumber) => {
+      console.log(commitNumber);
+    });
+};
 
-userInfo()
+userInfo();
