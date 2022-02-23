c8ed0da9f76d750759dd71b37fc1e305f3ae97e0
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-02-23 15:57:58 +0100

    added my token, created a varable for it in script.js, added the token to a new secret.js and put it in the gitignore

--- code/script.js
+++ code/script.js
@@ -1,9 +1,18 @@
 const username = "Mimmisen";
 let reponame = "";
-
 const API_URL = `https://api.github.com/users/${username}/repos`;
 
-fetch(API_URL)
+const API_TOKEN = TOKEN || process.env.API_KEY; // variable called TOKEN is local OR API_KEY is on the deployed site
+console.log(TOKEN);
+
+const options = {
+  method: "GET",
+  headers: {
+    Authorization: `token ${API_TOKEN}`, // you need to paste your token over here.
+  },
+};
+
+fetch(API_URL, options)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
@@ -11,7 +20,7 @@ fetch(API_URL)
 
     const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`;
 
-    fetch(API_URL_PR)
+    fetch(API_URL_PR, options)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
