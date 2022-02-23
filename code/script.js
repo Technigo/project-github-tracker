b6a5b09d5aac95ad3406377935e1823413718fcc
EmmaaBerg <emmaberg0819@gmail.com>
2022-02-23 18:39:39 +0100

    fetched profile picture and username

--- code/script.js
+++ code/script.js
@@ -1,5 +1,24 @@
+//DOM selectors
+const profileInfo = document.getElementById('profile')
+
 const username = 'EmmaaBerg'
+const API_USER = `https://api.github.com/users/${username}`
 const API_URL_REPOS = `https://api.github.com/users/${username}/repos`
+
+//Function to get the username and profilepicture
+const userInfo = () => {
+    fetch(API_USER)
+        .then((res) => res.json())
+        .then(profile => {
+            profileInfo.innerHTML = `
+        <img src = "${profile.avatar_url}">
+        <h3> ${profile.name}</h3>
+        `
+        })
+}
+
+//Invok the userInfo function
+userInfo()
 
 fetch(API_URL_REPOS)
     .then((resp) => resp.json())
@@ -14,18 +33,42 @@ fetch(API_URL_REPOS)
         pullRequests(forkedRepos);
     })
 
+
+
+
+
 const pullRequests = (forkedRepos) => {
-    forkedRepos.forEach(repo => {
+    forkedRepos.forEach((repo) => {
         fetch('https://api.github.com/repos/Technigo/' + repo.name + '/pulls')
-            .then(res => res.json())
-            .then(pullReqs => {
+            .then((res) => res.json())
+            .then((pullReqs) => {
                 //console.log(pullReqs);// loop foeach data är en array
-                pullReqs.forEach(pull => {
-                    if (pull.user.login === username){
+                pullReqs.forEach((pull) => {
+                    if (pull.user.login === username) {
                         console.log(pull)
-                        
+                        comments(pull);
+                        commits(pull);
                     }
                 })
             })
+
     })
+}
+//function to get review comments for each project
+const comments = (pullReq) => {
+    //console.log(pullReq)
+    fetch(pullReq.review_comments_url)
+        .then((res) => res.json())
+        .then((rewComment) => {
+            console.log(rewComment);
+        })
+}
+
+//function to get commit number for each project
+const commits = (pullReq) => {
+    fetch(pullReq.commits_url)
+        .then((res) => res.json())
+        .then((commitNumber) => {
+            console.log(commitNumber)
+        })
 }
