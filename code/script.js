e629e9e2b46a4776e3e4214ef0cf4bdd280bb7a1
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-02-23 18:28:56 +0200

    added gitignore and script

---
+++ code/script.js
@@ -0,0 +1,31 @@
+
+
+
+//global variables
+const username = 'kolkri'
+//Endpoint to get all your repos:
+const API_URL = `https://api.github.com/users/${username}/repos`
+
+//defining token
+const API_TOKEN = TOKEN || process.env.API_KEY
+
+//defining options
+const options = {
+    method: 'GET',
+    headers: {
+        Authorization: `token ${API_TOKEN}`
+    }
+}
+
+//fetch all of your repos and log them to the console
+fetch(API_URL, options)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        //filter out and only show the forked ones
+        let forkedOnes = data.filter(element => element.fork === true)
+        console.log('Forked ones:', forkedOnes)
+        //filter out only the forks from Technigo.
+        let technigoForks = forkedOnes.filter(element => element.name.startsWith('project'))
+        console.log('Technigo projects:', technigoForks)
+    })
