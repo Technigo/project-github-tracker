7d69e8d017b02ccce10c5f85733333d1f59c395c
TereseClaesson <terese.claesson@gmail.com>
2022-02-23 18:39:34 +0100

    fetched profile picture and username

--- code/script.js
+++ code/script.js
@@ -1,9 +1,27 @@
+const userProfile = document.getElementById('profile')
+
+
 const username = "TereseClaesson";
 //let reponame = ''
 
 //All my repos from github
-const API_URL = `https://api.github.com/users/${username}/repos`;
+const API_USER = `https://api.github.com/users/${username}`
+const API_URL = `https://api.github.com/users/${username}/repos`
 //const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
+
+
+
+const userInfo = () => {
+    fetch(API_USER)
+    .then((res) => res.json())
+    .then(profile => {
+        userProfile.innerHTML += `
+        <img src = ${profile.avatar_url}>
+        <h3>${profile.name}</h3>
+        `
+    }) 
+}
+
 
 fetch(API_URL)
   .then((Response) => Response.json())
@@ -25,7 +43,7 @@ fetch(API_URL)
 console.log(reponame)*/
 
     // declare after the data(repos in the case) been fetch, calling the const fetch on the top
-    /*const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
+    /*const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame/pulls`
 
 fetch(API_URL_PR)
 .then(Response => Response.json())
@@ -38,15 +56,35 @@ console.log(repos)
 
 const getPullRequest = (repoForked) => {
   repoForked.forEach((repo) => {
-    fetch("https://api.github.com/repos/Technigo/" + repo.name + "/pulls")
+    fetch('https://api.github.com/repos/Technigo/' + repo.name + '/pulls')
       .then((res) => res.json())
       .then((pullreqs) => {
         //console.log(pullreqs)
         pullreqs.forEach((pull) => {
           if (pull.user.login === username) {
             console.log(pull);
+            comments(pull);
+            commits(pull);
           }
         });
       });
   });
 };
+
+const comments = (pullReq) => {
+     fetch(pullReq.review_comments_url)
+      .then((res) => res.json())
+      .then((commentrev) => {
+        console.log(commentrev)
+    })
+}
+
+const commits = (pullReq) => {
+    fetch(pullReq.commits_url)
+    .then((res) => res.json())
+    .then((commitNumber)=> {
+        console.log(commitNumber)
+    })
+}
+
+userInfo()
