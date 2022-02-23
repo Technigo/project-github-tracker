d27f80aea979f5d3a7e069ff7849263289500762
Emma Lindell <emma.lindell4@gmail.com>
2022-02-23 21:28:26 +0100

    Fixed fetch and JavaScript styling

--- code/script.js
+++ code/script.js
@@ -10,7 +10,12 @@
 
 
 // DOM selectors
+const projectInfo = document.getElementById('projectInfo')
 const userInfo = document.getElementById('userInfo')
+const aboutUser = document.getElementById('aboutUser')
+const profilePicture = document.getElementById('profilePicture')
+const mainContent = document.getElementById('mainContent')
+
 
 // Github API and token
 const username = 'emmajosefina'
@@ -23,14 +28,28 @@ const options = {
     }
 }
 
-//fetching all repos from user
-const findingAllRepos = (user) => { 
+//fetching all repos to projects
+const findingAllRepos = (repos) => { 
     fetch(API_URL, options) 
        .then((res) => res.json()) 
        .then((data) => { 
+
+        mainContent.innerHTML += `
+        <section class="profile-box"> 
+        <img src="${data[0].owner.avatar_url}" id="profilePicture" class="profile-picture" />
+        <p class="full-name">Emma Lindell</p>
+        <p class="username">emmajosefina</p>
+      </section>
+      <div class="status-box">
+      <span class="inside-status-box">👩‍💻 Edit status</span>
+
+      <p class="presentation-text">Frontend Developer from Technigo Bootcamp. Always curious to learn more about technology and design.</p>
+    </div>
+
+      `
         console.log(data) 
         data.forEach((repo) => 
-        userInfo.innerHTML += 
+        projectInfo.innerHTML += 
         
         `
         <div class="projectContainer">
@@ -52,13 +71,36 @@ const findingAllRepos = (user) => {
             <p>
             <span class="styledHeadingsProject">
             URL: </span>
-            ${repo.topics.url}
+            ${repo.html_url}
         </p>
 
         </div>
+        ` 
+        )
 
-        `)
-       
+
+  
+    
        })
-}
+    }
+
+        // // fetching all info from user
+        // const aboutUser = (user) => { 
+        //     fetch(API_URL, options) 
+        //     .then((res) => res.json()) 
+        //     .then((data) => { 
+        //     console.log(data) 
+            
+        //     data.forEach((repo) => 
+        //     userInfo.innerHTML += 
+        //     aboutUser = (user) => {
+        //     const userPicture = data.owner.avatar_url
+        //     userInfo.innerHTML += `
+        //     console.log("Hello")
+        //     <img src=${userPicture}/>
+        //     `
+        //     )}
+
+        // }
+
 findingAllRepos()
