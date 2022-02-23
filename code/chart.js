21b3a0e473e3eba02cacfa2bf3932cb1521dc7a2
joannaringqvist <joanna.ringqvist@gmail.com>
2022-02-23 14:16:28 +0100

    Added the chart

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,29 @@
-//DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById('chart').getContext('2d');
 
-//"Draw" the chart here 👇
+const labels = [
+    `Finished projects`,
+    `Projects left`
+  ];
+
+const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: ['rgb(245, 217, 237)', 'rgb(217, 245, 239)'],
+      borderColor: 'rgb(66, 66, 66)',
+      data: [7, 12],
+    }]
+  };
+
+
+const config = {
+    type: 'pie',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    ctx,
+    config
+  );
+  
