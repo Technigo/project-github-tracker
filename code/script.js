0f46eea90a7626ecf56228730f00c53dad9b75f6
Louisa <lundislon@gmail.com>
2022-02-24 13:04:54 +0100

    added function for fetching the pull requests and used data.find method to get my pull requests

--- code/script.js
+++ code/script.js
@@ -1,6 +1,7 @@
 const username = 'Kras053'
 const API_URL =`https://api.github.com/users/${username}/repos`
 
+
 //my repos in GitHub filtered on forks from Technigo
 const myRepos = () => {
 
@@ -13,32 +14,40 @@ const myRepos = () => {
     const technigoRepos = data.filter(repo => repo.name.startsWith('project'))
     console.log(forkedRepos)    
     console.log(technigoRepos)
+
+    //need to invoke this next function here already, passing along the filtered repos 
+    //as an argument when calling the pull request function
+    getPullRequests(technigoRepos)
 })
 }
-
 myRepos()
 
 
+const getPullRequests = (technigoRepos) => {
+    //this fetches all the pull requests for each filtered project
+    technigoRepos.forEach(repo => {
+      fetch(`https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`)
+      .then(res => res.json())
+      .then(data => {
+          console.log(data)
+              
+          //this finds the pull requests I made by comparing the user.login from the pull API
+              // with the owner.login in the filtered repo API, and therefore we use pull in the
+              // find array method because that is the name of the array. 
+              // when undefined bc I did not do the pull request but my teammate
 
-
-/* later specific repos list
-const getPullRequests = (repos) => {
-    repos.forEach(repo => {
-        fetch('API_URL')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-        })
-
+              const myPullRequests = data.find((pull) => pull.user.login === repo.owner.login)
+              console.log(myPullRequests)
+              
+              //2. Now you're able to get the commits for each repo by using
+              // the commits_url as an argument to call another function
+              //3. You can also get the comments for each PR by calling
+              // another function with the review_comments_url as argument
+          })   
     })
+        
+}
 
-}*/
-
-/*
-
-https://api.github.com/repos/Technigo/${reponame}/pulls
-
-*/
 
 /*
 step 1 worked: 
