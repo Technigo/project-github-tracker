499e829093bd71c4c39ccbd944823ca8ddf24c96
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-02-25 00:26:23 +0100

    started work on repo grid fetch

--- code/script.js
+++ code/script.js
@@ -16,8 +16,30 @@ const createProfile = () => {
     });
 };
 
-const createRepos = () => {
-  fetch(API_REPOS).then((res) => res.json().then((data) => {}));
+const createRepoGrid = (repp) => {
+  fetch(API_REPOS)
+    .then((res) => res.json())
+    .then((data) => {
+      let forkedRepo = repositories.filter((data) => {
+        return data.fork === true;
+      });
+      // return repoGrid.fork === true;
+      forkedRepo.forEach((repo) => {
+        repoGrid.innerHTML += `
+          <div class="repo-name"></div>
+          <h3 class="repo-name">${data.name}</h3>
+          <span><a class="project-url" href="${data.svn_url}">${
+          data.name
+        }</span></a>
+           <p>Updated: ${new Date(data.pushed_at).toLocaleDateString("en-SE", {
+             year: "numeric",
+             month: "short",
+             day: "numeric",
+           })}</p>
+           <p>Default branch: ${data.default_branch}</p>`;
+      });
+    });
 };
-
+// ${rep.name.substring(rep.name.indexOf("/") + 1)}
 createProfile();
+createRepoGrid();
