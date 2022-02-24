296d634e6c4130c5d1578082893a92bdb41110e7
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-02-24 16:45:58 +0100

    added a chart and continued on fetch commits

--- code/chart.js
+++ code/chart.js
@@ -1,4 +1,40 @@
-//DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+// //DOM-selector for the canvas 👇
+const ctx = document.getElementById("chart").getContext("2d");
+Chart.defaults.font.size = 16;
+Chart.defaults.color = "#1E1E24";
+//Chart.defaults.font.family = 'Spectral', serif;
 
-//"Draw" the chart here 👇
+const drawChart = (amount) => {
+const labels = [
+    'Projects done',
+    'Projects to be done',
+    
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      data: [amount, 19-amount],
+      backgroundColor: ['#DCEED1', '#A18276' ],
+      borderColor: '#FFF8F0',
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        responsive: true,
+        // maintainAspectRatio: false,
+    }
+  };
+
+  const myChart = new Chart( 
+  document.getElementById('chart'),
+  config
+  ); 
+ // myChart()   // varför är denna ej aktiverad? vad gör den?
+  }
+
+  console.log('test')
--- code/script.js
+++ code/script.js
@@ -1,5 +1,5 @@
 //DOM-selectors
-const projectsWrapper = document.getElementById('projects-wrapper')
+const projects = document.getElementById('projects')
 const userInfo = document.getElementById('user-info')
 
 //GITHUB
@@ -38,50 +38,45 @@ const fetchRepos = () => {
         const filteredRepos = data.filter(repo => repo.name.startsWith("project") && repo.fork === true)
         //console.log(filteredRepos)
 
-        filteredRepos.forEach((repo) => {
-            projectsWrapper.innerHTML += `
-              <div class="projects">  
+        filteredRepos.forEach(repo => {
+            projects.innerHTML += `
+              <div class="card">  
                 <h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>
                 <p>Branch: ${repo.default_branch}</p>
                 <p>Latest push: ${new Date(repo.pushed_at).toDateString()}</p>
-                <p id="commit-${repo.name}"> Number of commits: </p>    
+                <p>Number of commits: ${repo.name.length}</p> 
               </div>
             `
         })
+
     getPullRequests(filteredRepos); // varför är denna här??
+    drawChart(filteredRepos.length)
     })
 }
 
-const getPullRequests = (allPullRequests) => {
+const getPullRequests = (allPullRequests) => { // filteredRepos?
   allPullRequests.forEach(repo => {
-      fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
-      .then(res => res.json())
-      .then(data => {
-        const myPullRequest = data.find((pull) => pull.user.login === repo.owner.login);    
-        // here make a conditional if not your pull request   
-        const myCommitsURL = myPullRequest.commits_url
-        const myCommentsURL = myPullRequest.comments_url
-        const repoName = myPullRequest.name
-        console.log(myPullRequest)
-        console.log(myCommitsURL, myCommentsURL)
-        fetchCommits(myCommitsURL, repoName); // varför är denna här??
-
-              //TODO
-              //2. Now you're able to get the commits for each repo by using
-              // the commits_url as an argument to call another function
-              //3. You can also get the comments for each PR by calling
-              // another function with the review_comments_url as argument
+    fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
+    .then(res => res.json())
+    .then(data => {
+      const myPullRequest = data.find((pull) => pull.user.login === repo.owner.login);// här göra en conditional ifall ej mina PR?
+      const myCommitsURL = myPullRequest.commits_url
+      const myCommentsURL = myPullRequest.comments_url
+      //const repoName = filteredRepos.name
+      console.log(myCommentsURL, myCommitsURL, myPullRequest)
+      fetchCommits(myCommitsURL); // varför är denna här??
         })
     })
   }
 
-const fetchCommits = (myCommitsURL, repoName) => {
-    fetch(myCommitsURL, repoName)
+const fetchCommits = (myCommitsURL) => {
+    fetch(myCommitsURL)
     .then((res) => res.json())
     .then (data => {
-        document.getElementById(`commit-${repo.name}`).innerHTML += data.length
+     // console.log(data)
+     //document.getElementById('commit-${repo.name}').innerHTML += data.length
   });
 }
 
-profileInfo() // få in denna någon annanstans?
+profileInfo() 
 fetchRepos()
