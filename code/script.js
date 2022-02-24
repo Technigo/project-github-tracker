24b11827a1d53d7a9de9d99b425ef13645b4898e
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-02-24 14:32:15 +0100

    testing to add .gitignore

--- code/script.js
+++ code/script.js
@@ -4,8 +4,17 @@ let reponame = ''
 const API_URL = `https://api.github.com/users/${username}/repos`
 const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
 
+const API_TOKEN = TOKEN;
+console.log(TOKEN)
 
-fetch(API_URL)
+const options = {
+    method: 'GET',
+    headers: {
+        Authorization: `${API_TOKEN}`
+    }
+}
+
+fetch(API_URL, options)
 .then(res => res.json())
 .then(data => {
     console.log(data, 'my repos');
@@ -13,5 +22,24 @@ fetch(API_URL)
     const technigoRepos = data.filter(
         (repo) => repo.name.includes('project-') && repo.fork);
         console.log(technigoRepos, 'technigo repos') 
-
 })  
+
+    //Remember to pass along your filtered repos as an argument when
+//you are calling this function
+
+const getPullRequests = (repos) => {
+    //Get all the PRs for each project.
+    repos.forEach(repo => {
+      fetch('https://api.github.com/repos/technigo/' + repo.name + '/pulls')
+      .then(res => res.json())
+      .then(data => {
+              //TODO
+          //1. Find only the PR that you made by comparing pull.user.login
+              // with repo.owner.login
+              //2. Now you're able to get the commits for each repo by using
+              // the commits_url as an argument to call another function
+              //3. You can also get the comments for each PR by calling
+              // another function with the review_comments_url as argument
+          })
+    })
+  }
