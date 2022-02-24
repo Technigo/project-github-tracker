6632ae438ea3ac9a9ee8601b28029708c1525cae
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-02-24 21:50:01 +0100

    changes to fetch function pullrequests

--- code/script.js
+++ code/script.js
@@ -1,7 +1,10 @@
 const username = 'CamillaHallberg'
 let reponame = ''
 
-const API_URL = `https://api.github.com/users/${username}/repos`
+
+// API's
+const API_USER = `https://api.github.com/users/${username}`
+const API_URL_REPO = `https://api.github.com/users/${username}/repos`
 const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
 
 const API_TOKEN = TOKEN;
@@ -14,43 +17,34 @@ const options = {
     }
 }
 
-fetch(API_URL, options)
+fetch(API_USER, options)
 .then(res => res.json())
-.then(data => {
-    console.log(data, 'my repos');
-    // below I'm filtering out the forked repos from Technigo.
-    const technigoRepos = data.filter(
-        (repo) => repo.name.includes('project-') && repo.fork);
-        console.log(technigoRepos, 'technigo repos') 
+.then(userdata => {
+    console.log(userdata, 'my user');
+})
 
-    const getPullRequests = (repos) => {
-        repos.forEach(repo => {
-            fetch('https://api.github.com/repos/technigo/' + repo.name + '/pulls?per_page=150', options)
-            .then(res => res.json())
-            .then(data2 => {
-                console.log(data2, 'every pull for each project')
-            })
+const getRepos = () => {
+    fetch(API_URL_REPO, options)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data, 'my repos')
+
+        const forkedRepos = data.filter((repo) => repo.fork && repo.name.startsWith('project-'))
+        console.log(forkedRepos, 'forked repos')
+        getPullRequests(forkedRepos)
+    })
+}
+
+const getPullRequests = (forkedRepos) => {
+    forkedRepos.forEach(repo => {
+        fetch(`https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=150`, options)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data, 'all pull requests on the projects')
+
+            const pulls = data.find((pull) => pull.user.login === repo.owner.login)
+            console.log(pulls, 'my pull requests')
         })
-    }
-        getPullRequests(technigoRepos);
-})  
-
-
-
-
-// const getPullRequests = (repos) => {
-//     //Get all the PRs for each project.
-//     repos.forEach(repo => {
-//       fetch('https://api.github.com/repos/technigo/' + repo.name + '/pulls')
-//       .then(res => res.json())
-//       .then(data => {
-//               //TODO
-//           //1. Find only the PR that you made by comparing pull.user.login
-//               // with repo.owner.login
-//               //2. Now you're able to get the commits for each repo by using
-//               // the commits_url as an argument to call another function
-//               //3. You can also get the comments for each PR by calling
-//               // another function with the review_comments_url as argument
-// //           })
-//     })
-//   }
+    })
+}
+getRepos()
