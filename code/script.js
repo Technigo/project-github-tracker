5e078a0e70ce38a63b05f1c7a174f2be2b469bf5
joannaringqvist <joanna.ringqvist@gmail.com>
2022-02-25 13:26:10 +0100

    Stored the functionality for showing username and picture in an own function. Also added more comments

--- code/script.js
+++ code/script.js
@@ -7,39 +7,55 @@ const username = document.getElementById('username');
 const picture = document.getElementById('picture');
 const numberOfProjects = document.getElementById('numberOfProjects');
 const sort = document.getElementById('sort');
-
 let ownerLogin = '';
+
 //Count amount of repos for the chart
 //let countRepos = 0;
 
 //Function that renders the table
 //const renderTable = () => {}
 
+//Function for showing username and picture
+const showUsernameAndPic = (ownerLogin, userPic) => {
+    username.innerHTML = ownerLogin;
+    picture.src = userPic;
+}
+
+//Function for getting all my repos from Github
 const getRepos = (sort) => {
     fetch(API_URL_REPOS)
         .then((response) => {
             return response.json()
         })
         .then((json) => {
+
+            //Get the name and picture from the user and pass it to the function for showing them
             ownerLogin = json[0].owner.login;
-            username.innerHTML = ownerLogin;
-            picture.src = json[0].owner.avatar_url;
+            userPic = json[0].owner.avatar_url;
+            showUsernameAndPic(ownerLogin, userPic);
+            
+            //Array for storing the repos and counting them
             let arrayWithRepos = [];
 
             json.forEach((repo) => {
                 console.log(repo);
-
+                //I only want to continue working with the repos from Technigo, so they should be forked and start with project
                 if (repo.fork === true && repo.name.startsWith('project')) {
                     arrayWithRepos.push(repo.name);
+                    //Write the table rows and cells
                     projects.innerHTML += `<tr id="tr-${repo.name}"><td>${repo.name}</td><td>${new Date(repo.updated_at).toLocaleDateString('sv-SE')}</td><td>${repo.default_branch}</td><td id="commits-${repo.name}"></td><td><a class="repo-url" href="${repo.html_url}">${repo.html_url}</a></td><td id="open-${repo.name}">Open</td></tr>`
                 }
             });
+
+            //Count the repos and show the chart
             countRepos = arrayWithRepos.length;
             numberOfProjects.innerHTML = `I have finished ${countRepos} projects and have ${19-countRepos} left.`;
-            console.log(arrayWithRepos.length);
             showChart(countRepos);
+            
+            //Get the pull requests for each repo
             getPullRequests(arrayWithRepos);
 
+            
             /*if (sort === 'alphabetically') {
                 console.log('ABCD');
             }*/
@@ -47,8 +63,8 @@ const getRepos = (sort) => {
         })
 }
 
+//Function for getting the pull requests to be able to show the commits and the comments
 const getPullRequests = (repos) => {
-    //Get all the PRs for each project.
     repos.forEach(repo => {
         fetch(`https://api.github.com/repos/technigo/${repo}/pulls?per_page=90`)
         .then(res => res.json())
@@ -57,20 +73,19 @@ const getPullRequests = (repos) => {
             let commitsURL = '';
 
             data.forEach((repoData) => {
-                
+                //Only go on with the PRs that are from the user
                 if (ownerLogin === repoData.user.login) {
                 
-                    //Now you're able to get the commits for each repo by using
-                    // the commits_url as an argument to call another function
+                    //Get the commits for each repo through the commitsURL. Pass it to the fetchCommits function.
                     commitsURL = repoData.commits_url;
                     fetchCommits(commitsURL, repo);
                 } 
             });
-
         })
     })
 }
 
+//Function for showing how many commits are made for each repo
 const fetchCommits = (commitsURL, repoName) => {
     fetch(commitsURL)
     .then(res => res.json())
@@ -79,6 +94,7 @@ const fetchCommits = (commitsURL, repoName) => {
     });
 }
 
+//Invoke the getRepo function
 getRepos();
 
 /*
