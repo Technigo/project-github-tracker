51da6efbb605cfb882cfe1c2881b7d8afadbbc20
Jessica Sandler <jessica.sandler@hotmail.com>
2022-02-25 15:14:17 +0100

    Added chart trial

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,28 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+const labels = [
+    'Done',
+    'Left',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(130, 99, 132)'],
+      borderColor: 'rgb(255, 99, 132)',
+      data: [6, 19],
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('chart'),
+    config
+  );
