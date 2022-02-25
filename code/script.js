9ce748f3af9e90ef455946df85a18acd653985f5
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-02-25 19:43:25 +0100

    added comments

--- code/script.js
+++ code/script.js
@@ -7,6 +7,7 @@ const user = 'josse79'
 const API_USER = `https://api.github.com/users/${user}`
 const API_REPOS_LIST = `https://api.github.com/users/${user}/repos`
 
+//The TOKEN function
 const options = {
   method: 'GET',
   headers: {
@@ -14,6 +15,7 @@ const options = {
   }
 }
 
+//1st function and fetch to get the user details
 const getUser = () => {
   fetch(API_USER, options)
   .then(res => res.json())
@@ -24,14 +26,15 @@ const getUser = () => {
   })
   getRepos ()
 }
-
+//2nd function and fetch to get the repo list
 const getRepos = () => {
   fetch(API_REPOS_LIST, options)
   .then(res => res.json())
   .then(data => {
+    //Filtering out the forked repos & repos starting with project
     const forkedRepos = data.filter((repo) => repo.fork && repo.name.startsWith('project'))
-    console.log(forkedRepos)
      forkedRepos.forEach((repo) => {
+       //Positioning the information in HTML also with dynamic ID repo.name
       projectContainer.innerHTML += `
         <div class='repos' id=${repo.name}>
             <h1>${repo.name}</h1>
@@ -40,18 +43,24 @@ const getRepos = () => {
             <p>Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
          </div>`
       })
+      //Passing the filtered repos to next function
      getPullRequests(forkedRepos)
+     //Passing the filtered repos length to chartfunction in chart.js
      activateChart(forkedRepos.length)
    })
 
+   //3rd function and fetch to get all the pulls with help of repo.name
   const getPullRequests = (repos) => {
     repos.forEach(repo => {
       fetch(`https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`, options)
       .then(res => res.json())
       .then(data => {
+        //Filter my own pulls by comparing user login and repo owner
         const filteredPulls = data.find((pull) => pull.user.login === repo.owner.login)
-        console.log(filteredPulls)
+        //Passing the filtered pulls to next functions
           if (filteredPulls) {
+            //Passing the commits_url and review_comments_url as arguments to next functions
+            //Also passing along the dynamic ID repo.name to be able to positioning in HTML
             getCommits(filteredPulls.commits_url, repo.name)
             getReview(filteredPulls.review_comments_url, repo.name)
           } else {
@@ -62,20 +71,25 @@ const getRepos = () => {
     })
   }
 
+  //4th function and fetch. Fetch (commits_url declared in getPullRequests function
+  //therefore activated by argument URL and repoName
   const getCommits = (URL, repoName) => {
     fetch(URL, options)
     .then(res => res.json())
     .then(data => {
+      //Positioning by dynamic ID repoName
       document.getElementById(repoName).innerHTML += `
         <p>Number of commits: ${data.length}</p>`
     })
   }
 
+  //5th function and fetch. Fetch (commits_url declared in getPullRequests function
+  //therefore activated by argument URL and repoName
   const getReview = (URL, repoName) => {
     fetch(URL, options)
     .then(res => res.json())
     .then(data => {
-     console.log(data)
+      //If any review made - positioning data by dynamic ID repoName
       if (`${data[0].user.login} == ''`) {
         document.getElementById(repoName).innerHTML += `
           <p>Review made by: ${data[0].user.login}</p>`
@@ -84,5 +98,5 @@ const getRepos = () => {
       })
   }
 }
-
+//Invoking first function
 getUser ()
