bf66935cbfb54dd6c49b187e3be5c0f928382edb
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-02-25 21:14:54 +0100

    comments and final changes

--- code/chart.js
+++ code/chart.js
@@ -1,33 +1,29 @@
+Chart.defaults.font.family = "Merriweather Sans, sans-serif";
 //DOM-selector for the canvas 👇
-const ctx = document.getElementById('chart').getContext('2d')
+const ctx = document.getElementById("chart").getContext("2d");
 
 //"Draw" the chart here 👇
 const makeChart = (finishedProjects) => {
-  const labels = [
-    'Finished projects',
-    'Projects to do',
-  ];
+  const labels = ["Finished projects", "Projects to do"];
 
   const data = {
     labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: ['#ffa597','#ffdfda'],
-      borderColor: 'black',
-      borderWidth: '1',
-      data: [finishedProjects, 19-finishedProjects],
-    }]
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: ["#ffa597", "#ffdfda"],
+        borderColor: "black",
+        borderWidth: "1",
+        data: [finishedProjects, 19 - finishedProjects],
+      },
+    ],
   };
 
   const config = {
-    type: 'doughnut',
+    type: "doughnut",
     data: data,
-    options: {}
+    options: {},
   };
 
-  const myChart = new Chart(
-    document.getElementById('chart'),
-    config
-      
-  );
-  }
+  const myChart = new Chart(document.getElementById("chart"), config);
+};
--- code/script.js
+++ code/script.js
@@ -1,85 +1,87 @@
-const username = "SofiePellegrini"
-const API_REPOS = `https://api.github.com/users/${username}/repos`
-const API_PROFILE = `https://api.github.com/users/${username}`
-const projects = document.getElementById('projects')
-const fullName = document.getElementById('full-name')
-const userName = document.getElementById('user-name')
-const profilePic = document.getElementById('profile-pic')
+//DOM selectors
 
+const username = "SofiePellegrini";
+const API_REPOS = `https://api.github.com/users/${username}/repos`;
+const API_PROFILE = `https://api.github.com/users/${username}`;
+const projects = document.getElementById("projects");
+const fullName = document.getElementById("full-name");
+const userName = document.getElementById("user-name");
+const profilePic = document.getElementById("profile-pic");
 
+//Fetch picture, name and username
 
-// Token
-const options = {
-    method: 'GET',
-    headers: {
-          Authorization: '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' 
-      }
-  }
+fetch(API_PROFILE)
+  .then((res) => res.json())
+  .then((data) => {
+    profilePic.src = data.avatar_url;
+    fullName.innerHTML = `<h2>${data.name}</h2>`;
+    userName.innerHTML = `<h3><a href=${data.html_url}>${data.login}</a></h3>`;
+  })
 
-  //Fetch picture, name and username
-  
-  fetch(API_PROFILE, options)
-  .then(res => res.json())
-  .then(data => { 
-    profilePic.src = data.avatar_url
-    fullName.innerHTML = `<h2>${data.name}</h2>`
-    userName.innerHTML = `<h3><a href=${data.html_url}>${data.login}</a></h3>`
-    
-})
- 
-  
-  fetch(API_REPOS, options)
-  .then(res => res.json())
-  .then((data) => { console.log(data)
-    const technigoRepos = data.filter(repo => repo.fork && repo.name.startsWith('project-'))
-    console.log(technigoRepos)
-    makeChart(technigoRepos.length)
+  //Fetch repositories - then filter out only Technigo repositories
 
+fetch(API_REPOS)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    const technigoRepos = data.filter(
+      (repo) => repo.fork && repo.name.startsWith("project-")
+    )
+    console.log(technigoRepos);
+    makeChart(technigoRepos.length);
 
     technigoRepos.sort(function (oldestRepo, newestRepo) {
       return new Date(oldestRepo.pushed_at) - new Date(newestRepo.pushed_at);
-    });
- 
- 
-technigoRepos.forEach((repo) => { 
-  projects.innerHTML+=`
+    })
+
+    //Create div for every repository
+
+    technigoRepos.forEach((repo) => {
+      projects.innerHTML += `
   <div class="repos">
   <a style="font-weight:bold"  href=${repo.html_url}>${repo.name}</a>
-  <p>Last push: ${new Date(repo.pushed_at).toLocaleString("sv-SE", {dateStyle: 'short'})}</p>
+  <p>Last push: ${new Date(repo.pushed_at).toLocaleString("sv-SE", {
+    dateStyle: "short",
+  })}</p>
   <p>Default branch: ${repo.default_branch}</p>
   <p id="${repo.name}-commit">Commits: - </p> 
   <p id="pullrequests-${repo.name}">Pull requests: </p>
   </div>
   `
- 
-  
-  API_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`
-  fetch(API_URL, options)
-  .then ((res) => res.json())
-  .then ((data) => {
-    const myPulls = data.find(
-      (pull) => pull.user.login === repo.owner.login
-    )
-    console.log(myPulls)
-    
 
-    if (myPulls) {
-      getMyCommits(myPulls.commits_url, repo.name)
-      document.getElementById(`pullrequests-${repo.name}`).innerHTML +=  Object.length
-    }else{
-      document.getElementById(`pullrequests-${repo.name}`).innerHTML = `No pull requests were made by ${username}.`
-    }
-  })    
-})
-  }
-  )
-  
-  const getMyCommits = (myCommitsUrl,repo) => {
-    fetch(myCommitsUrl, options)
+
+  //Fetch pullrequests and put them on the page
+
+      API_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100&page=1`;
+      fetch(API_URL)
+        .then((res) => res.json())
+        .then((data) => {
+          const myPulls = data.find(
+            (pull) => pull.user.login === repo.owner.login
+          )
+          console.log(myPulls);
+
+          if (myPulls) {
+            getMyCommits(myPulls.commits_url, repo.name);
+            document.getElementById(`pullrequests-${repo.name}`).innerHTML +=
+              Object.length;
+          } else {
+            document.getElementById(
+              `pullrequests-${repo.name}`
+            ).innerHTML = `No pull requests were made by ${username}.`;
+          }
+        })
+    })
+  })
+
+  //Fetch commits and put them on the page
+
+const getMyCommits = (myCommitsUrl, repo) => {
+  fetch(myCommitsUrl)
     .then((res) => res.json())
     .then((data2) => {
-      document.getElementById(`${repo}-commit`).innerHTML = `Commits: ${data2.length}`      
-    
+      document.getElementById(
+        `${repo}-commit`
+      ).innerHTML = `Commits: ${data2.length}`;
     })
-  }
- 
+}                                                                                                                   
