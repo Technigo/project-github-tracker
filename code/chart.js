7ea4af600ebf5588941b219b1d8a00a5db198967
EmmaSprings <emmasprings@gmail.com>
2022-02-25 17:26:10 +0100

    managed to do css and some js but might need to re-write js

--- code/chart.js
+++ code/chart.js
@@ -2,13 +2,61 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
-const myChart = () => {
-    const config = {
-        type: 'doughnut',
-        backgroundColor: [
-            '#1400D1',
-            '#CEE6FF'
-        ]
-        
+
+const drawChart = (amount) => {
+const labels = [
+    'Projects done',
+    'Projects to be done',
+    
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [ {
+      label: 'My First dataset',
+      backgroundColor: ['rgb(131, 28, 147)', 'rgba(19, 115, 199, 0.776)' ],
+      borderColor: 'none',
+      data: [amount, 19-amount],
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        responsive: true,
+        // maintainAspectRatio: false,
     }
-}
+};
+
+const myChart = new Chart( 
+document.getElementById('chart'),
+config
+
+);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myChart = () => {
+//     const config = {
+//         type: 'doughnut',
+//         backgroundColor: [
+//             '#1400D1',
+//             '#CEE6FF'
+//         ]
+        
+//     }
+// }
--- code/script.js
+++ code/script.js
@@ -1,5 +1,11 @@
+
+
 const username = 'EmmaSprings'
 const projects = document.getElementById('projects')
+const API_URL_USER = `https://api.github.com/users/${username}`
+const API_URL_REPO = `https://api.github.com/users/${username}/repos`
+
+
 
 const fetchUser = () => {
 
@@ -9,66 +15,155 @@ const fetchUser = () => {
               Authorization: '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' 
           }
       }
-    const API_URL_USER = `https://api.github.com/users/${username}`
+
     fetch(API_URL_USER, options)
      .then((res) => res.json())
      .then((data) => {
          projects.innerHTML += `
-         <p>GitHub username = ${data.login}</p>
-         <h2>Projects = [</h2>
-         
-         `
-        //  header.innerHTML += `
-        //  <img width="100" src="https://avatars.githubusercontent.com/u/94292545?v=4"/>
-        //  `
+         <div class="header-container">
+            <img src="https://avatars.githubusercontent.com/u/94292545?v=4"/>
+                <h1>GitHub tracker for Emma Springs</h1>
+                <h2>user: <a href="https://api.github.com/users/EmmaSprings" class="login-name"> ${data.login}</a> <br>
+                ${data.bio}
+                </h2>
+                
+                  <p class="title">projects = [</p>
+         </div>  
+            `
+            console.log(data)  //remove 
+        
         fetchRepos();
+        
+        
     })
-
     
 
-const fetchRepos = () => {
-    const API_URL_REPO = `https://api.github.com/users/${username}/repos`
-    fetch(API_URL_REPO, options)
-      .then((res) => res.json())
-      .then((data) => {
+    
+    const fetchRepos = (myProjects) => {
+        fetch(API_URL_REPO, options)
+        .then((res) => res.json())
+        .then((data) => {
 
-        const myProjects = data.filter((repo) => repo.name.includes("project-") && repo.fork);
-           
+            const myProjects = data.filter((repo) => repo.name.startsWith("project-") && repo.fork);
+       
             myProjects.forEach((repo => {
                 projects.innerHTML += `
-               <li> 
-                   <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                 <p>
-                    This is ${repo.name}
-                 </p>
-              <li>
-           `
-        }))
+                <li> 
+                 <a href="${repo.html_url}" target="_blank" class="repo-link">${repo.name}</a>
+                  <p class="repo-info">
+        <!-- This is ${repo.name} -->
+                   </p>
+                    <p class="latest-push">
+                    latest push: ${new Date(repo.pushed_at).toDateString()}</p>
+                    <p class="extra-info">main language: ${repo.language}<br>
+                     branch: ${repo.default_branch}<br> 
+                     commits amount: ${repo.commits_url.length}<br>
+                     <p id ="${repo.name}">Commits made by team member</p>
+                     pr: ${length}
+                     </p>
+            
+                     <li>
+                `
+            }))
 
-        projects.innerHTML += `<h2 class="sign">]</h2>`
-        
+                        projects.innerHTML += `
+                        <p class="title">]</p>
+                        <h3>hello</h3>
+                        <!--<p>total repos = </p>-->
+                        ` 
+
+            drawChart(myProjects.length);
+            fetchPullRequests(myProjects);
+            console.log(myProjects)
         })
+    }; 
+    
+            const fetchPullRequests = (myProjects) => {
+                // const API_URL_PRS = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`
+                
+                myProjects.forEach(repo => {
+                    fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const myPullRes = data.find(pull => pull.user.login === repo.owner.login);
+
+                        const myCommitsURL = data.commits_url
+                        const repoName = repo.name
+                        fetchCommits(myCommitsURL, repoName);
+                        console.log(myPullRes)
+    
+
+                        
+                    })
+                })
+            }
+         
+        };
         
-    };  
-    myChart(); 
-};
-
-
-fetchUser();    
-
-
-
-
-// const myProjects = data.filter((data) => data.name.includes("project-") && data.fork));
-//   const userRepos = data[0].name;
-//   for (i = 0; i < data.length; i++){
-//       projects.innerHTML += `
-//       <li> 
-//       <a href="${data[i].html_url}" target="_blank">${data[i].name}</a>
-//       <p>
-//       This is ${data[i].name}
-//       </p>
-//       <li>
-//       `
-      
-//     };
+        
+        
+        fetchUser();
+        
+        
+        
+        
+        
+        const fetchCommits = (myCommitsURL, repoName) => {
+            
+            fetch(myCommitsURL, repoName)
+            .then((res) => res.json())
+            .then((data) => {
+                document.getElementById(repoName).innerHTML =`Number of commit: ${data.length}`
+                // document.getElementById(`commit-${dataName}`).innerHTML += data.length
+            })
+        }    
+            
+            
+            
+            
+            
+            
+    
+    
+    // const myProjects = data.filter((data) => data.name.includes("project-") && data.fork));
+    //   const userRepos = data[0].name;
+    //   for (i = 0; i < data.length; i++){
+        //       projects.innerHTML += `
+        //       <li> 
+        //       <a href="${data[i].html_url}" target="_blank">${data[i].name}</a>
+        //       <p>
+        //       This is ${data[i].name}
+        //       </p>
+        //       <li>
+        //       `
+        
+        //     };
+        
+        
+        
+        
+        
+        // const fetchPulls = () => {
+        
+        //     const API_URL_PRS = `https://api.github.com/repos/Technigo/project-chatbot/pulls?per_page=100`
+            
+        //     fetch(API_URL_PRS)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        
+        //         const myPulls = data.filter((repo) => repo.login.includes(`${username}`)
+        
+        //         myPulls.forEach((repo {
+        
+        //         }))
+              
+        //         // projects.innerHTML += `
+        //         // <p>hello total repos = ${data[1].base.label} </p>
+        //         // `
+        //         console.log(data)
+        
+        //     fetchPullRequests(myProjects)
+        
+        //     });
+        
+        // }
