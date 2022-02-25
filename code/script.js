bada1c3963974395c3071e6d7e5ef37074a6c4f5
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-02-25 12:18:54 +0100

    all changes and added gitigonre

--- code/script.js
+++ code/script.js
@@ -1,26 +1,88 @@
-const username = "isabellwastfelt";
-const API_URL = `https://api.github.com/users/${username}/repos`;
+const USER = "isabellwastfelt";
+const REPOS_URL = `https://api.github.com/users/${USER}/repos`;
+const PROFILE_URL = `https://api.github.com/users/${USER}`;
+
+const projectContainer = document.getElementById("projects");
+const profileInfo = document.getElementById("profile");
+
+const API_TOKEN = TOKEN || process.env.API_KEY;
 
 const options = {
   method: "GET",
   headers: {
-    Authorization: "token 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",
+    Authorization: `token ${API_TOKEN}`,
   },
 };
 
-let reponame;
+// `token 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`
 
-fetch(API_URL, options)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    reponame = data[2].name;
+const getProfile = () => {
+  fetch(PROFILE_URL, options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      profileInfo.innerHTML += `
+        <img src=${data.avatar_url}' alt='image of isabellwastfelt at GitHub'>
+        <h3><a href="${data.html_url}">${data.login}</a></h3>
+        `;
+    });
+};
+getProfile(); //invoking
 
-    const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`;
+const getRepos = () => {
+  fetch(REPOS_URL, options)
+    .then((res) => res.json())
+    .then((data) => {
+      const forkedRepos = data.filter(
+        (repo) => repo.fork && repo.name.startsWith("project")
+      );
 
-    fetch(API_URL_PR)
+      forkedRepos.forEach(
+        (repo) =>
+          (projectContainer.innerHTML += `
+            <div class='card'>
+                <a href="${repo.html_url}"><h3>${repo.name}</h3></a>
+                <p> Default branch: ${repo.default_branch}</p>
+                <p> Recent push: ${new Date(repo.pushed_at).toDateString()}</p>
+                <p id='commit-${repo.name}'> Number of commits: </p>
+            </div>
+        `)
+      );
+
+      getPullRequests(forkedRepos);
+      drawChart(forkedRepos.length);
+    });
+};
+
+getRepos();
+
+// Pull requests for each project
+const getPullRequests = (repos) => {
+  repos.forEach((repo) => {
+    const PULLREQUEST_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
+    fetch(PULLREQUEST_URL, options)
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+      .then((pull) => {
+        const myPullRequest = pull.find(
+          (pull) => pull.user.login === repo.owner.login
+        );
+
+        // If pull request done by user, getCommits function is invoked
+        if (myPullRequest) {
+          getCommits(myPullRequest.commits_url, repo.name);
+        } else {
+          document.getElementById(`commit-${repo.name}`).innerHTML =
+            "No pull request done by user";
+        }
       });
   });
+};
+
+const getCommits = (myCommitsUrl, myRepoName) => {
+  fetch(myCommitsUrl)
+    .then((res) => res.json())
+    .then((commit) => {
+      document.getElementById(`commit-${myRepoName}`).innerHTML +=
+        commit.length;
+    });
+};
