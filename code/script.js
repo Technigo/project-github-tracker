4c986884ca54e9b67e009b0c3b0a3e8c341ad5b0
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-02-25 12:32:14 +0100

    trying to push it

--- code/script.js
+++ code/script.js
@@ -5,15 +5,16 @@ const PROFILE_URL = `https://api.github.com/users/${USER}`;
 const projectContainer = document.getElementById("projects");
 const profileInfo = document.getElementById("profile");
 
-const API_TOKEN = TOKEN || process.env.API_KEY;
+// const API_TOKEN = TOKEN || process.env.API_KEY;
 
 const options = {
   method: "GET",
   headers: {
-    Authorization: `token ${API_TOKEN}`,
+    Authorization: `token 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`,
   },
 };
 
+// Authorization: `token ${API_TOKEN}`,
 // `token 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`
 
 const getProfile = () => {
