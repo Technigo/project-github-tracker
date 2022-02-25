f0cee7af35e6cc42979ee732bbb108ec7ef5dd8d
Amanda Tange <amandatange@hotmail.com>
2022-02-25 11:54:59 +0100

    added token and some small details in the other files

--- code/chart.js
+++ code/chart.js
@@ -3,29 +3,17 @@ const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
-
-const data = {
-labels: labels,
-datasets: [{
-    label: 'My First dataset',
-    backgroundColor: ['#FF5722', '#F96D00', '#F0A500', '#CF7500', '#524A4E', '#1A1C20'],
-    borderColor: '#F4F4F4',
-    data: [10, 5, 2, 20, 30, 45],
-}]
-};
-
-const config = {
-type: 'doughnut',
-data: data,
-options: {}
-};
-
-const myChart = new Chart(ctx, config);
+const myChart = (numberOfProjects) => {
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['Completed', 'To be done'],
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: ['#cc5500', '#3a3a3a'],
+        borderColor: '#F4F4F4',
+        data: [numberOfProjects, 19 - numberOfProjects],
+      }]
+    }
+  })
+}
--- code/script.js
+++ code/script.js
@@ -6,14 +6,8 @@ const usr = document.getElementById("usr");
 usrName = 'amandatange';
 let forkedRepos = []; // will contain filtered list of repos that have been forked
 let projectRepos = []; // will contain filtered list of repos with names that start with "project"
-let projectNames = []; // will contain a list with the section IDs for each project
-let pullRequests = [];
-let projectCounter = 0;
-let PRCounter = 0; // counts pullrequests to get the index to match the PR data to the project section id
+
 const API_REPOS = `https://api.github.com/users/${usrName}/repos`
-// const API_PR = `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`
-
-
 const API_TOKEN = TOKEN || process.env.API_KEY;
 
 const options = {
@@ -29,211 +23,77 @@ const fetchData =  async () => {
     .then((data) => { 
         forkedRepos = data.filter(repo => repo.fork);
         projectRepos = forkedRepos.filter(repo => repo.name.startsWith('project'));
-        console.log(projectRepos[0])
-        // console.log(projectRepos[1])
-        // console.log(projectRepos[2])
-        // console.log(projectRepos[3])
-        // console.log(projectRepos[4])
-        // console.log(projectRepos[5])
+        // console.log(projectRepos)
+        myChart(projectRepos.length)
 
         // for the user presentation
         usr.innerHTML = `
-            <img class='avatar_url_pic' src='${projectRepos[0].owner.avatar_url}'/>
-            <h1>@${usrName}</h1>
+            <img class='avatar_url_pic' src='${data[0].owner.avatar_url}'/>
+            <h1><a href='${data[0].owner.html_url}'>@${usrName}</a></h1>
         `;
 
-        ///for the projects sections
-        // console.log(projectRepos.length)
-        // const postInfo = async (dataPR) => {
-        //     for (let i = 0; i < projectRepos.length; i++) {
-        //         let repo = projectRepos[i];
-        //         let pull = dataPR[i];  
-        //         let p_login = pull.user.login;
-        //         let r_login = repo.owner.login;
-        //         console.log(p_login, r_login)
-        //         if (p_login === r_login || p_login === "michaelchangdk" && pull.id === 856976386) {
-        //             console.log(p_login, r_login)
-        //             main.innerHTML += `
-        //                     <section id='${repo.name}_${i+1}' class='project'>
-        //                         <h3>${repo.name}</h3>
-        //                         <p>Name of your default branch for each repo</p>
-        //                         <a href='${repo.html_url}'>Link to repo</a>
-        //                     </section>
-        //                     `;
-        //         }
-        //     }
-        // }
-
-
+    
         const fetchPR =  async () => {
-            for (let i = 0; i < projectRepos.length; i++) {
+            let listWithPR = []
+            for (let i = 0; i < projectRepos.length; i++) { 
                 let repo = projectRepos[i];
                 await fetch(`https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`, options)
-                .then((resPR) => resPR.json())
-                .then((dataPR) => {
-                    // console.log(dataPR)
-                    // let p_login = pull.user.login;
-                    // let r_login = repo.owner.login;
-                    let filteredDataPR = dataPR.filter(pr => pr.user.login === repo.owner.login || pr.user.login === "michaelchangdk" && pr.id === 856976386)
-                    // console.log(filteredDataPR)
-                    // postInfo(dataPR)
-                    // console.log("hej" , i) 
-                    const pull = filteredDataPR;
-                    console.log(pull)
-
-                    // let htmlPR = ""
-                    // if (pull.length !== 0) {
-                    //     htmlPR = pull[0].html_url // fix this in a better way maybe
-                    // }
-                    // console.log(pull)
-                    // let p_login = pull[0].user.login;
-                    // let r_login = repo.owner.login;
-                    // console.log(p_login, r_login)
-                    // if (p_login === r_login || p_login === "michaelchangdk" && pull.id === 856976386) {
-                    // console.log(p_login, r_login)
-                    main.innerHTML += `
-                            <section id='${repo.name}_${i+1}' class='project'>
-                                <h3>${repo.name}</h3>
-                                <p>Default branch: ${repo.default_branch}</p>
-                                <p><a href='${repo.html_url}'>Link to repo</a></p>
-                                <p>Most recent update (push) for each repo</p>
-                                
-                                
-                            </section>
-                            `;
-                    if (pull.length !== 0) {
-                        console.log(pull[0]._links.commits)
-                        document.getElementById(`${repo.name}_${i+1}`).innerHTML += `
-                            <p>Number of commits: ${pull[0]._links.commits.length}</p>
-                            <p><a href='${pull[0].html_url}'>Pull request</a></p>`
+                    .then((resPR) => resPR.json())
+                    .then((dataPR) => {
+                        listWithPR.push(dataPR)
+                        if (listWithPR.length === projectRepos.length) {
+                            postInfo(listWithPR)
+                        }
                     }
-                    // } 
-                })
+                )
             }
-                
-            }
+        }
         fetchPR()
-        
+
+        const postInfo =  async (dataPR) => {
+            for (let i = 0; i < projectRepos.length; i++) {
+                let repo = projectRepos[i];
+                    let pull = dataPR[i].filter(pr => pr.user.login === repo.owner.login || pr.user.login === "michaelchangdk" && pr.id === 856976386)
+                   
+                    // const pull = filteredDataPR;
+                    // console.log("projectrepo[i]", projectRepos[i])
+                    // const API_COMMITS = pull[0]._links.commits.href;
+                    const API_COMMITS = projectRepos[i].commits_url.replace("{/sha}", "");
+
+                        fetch(API_COMMITS, options)
+                        .then((commits_res) => commits_res.json())
+                        .then((commits_data) => {
+                            // document.getElementById(`${repo.name}_${i+1}`).innerHTML += `
+                           
+
+                        main.innerHTML += `
+                                <section id='${repo.name}_${i+1}' class='project'>
+                                    <h3 class='repo-name'>${repo.name}</h3>
+                                    <p class='repo-url'>Link to repo <a href='${repo.html_url}'><span>here</span></a></p>
+                                    <p class='default-branch'>Default branch: <span>${repo.default_branch}</span></p>
+                                    <p class='recent-push'>Most recent update: <span>${new Date(repo.pushed_at).toLocaleDateString('en-GB')}</span></p>
+                                    <p>Number of commits: <span>${commits_data.length}</span></p>
+                                </section>
+                                `;
+                        
+                        //If the repo doesn't have a PR it won't add this data
+                        if (pull.length !== 0) {
+                            // const API_COMMITS = pull[0]._links.commits.href;
+                            // fetch(API_COMMITS, options)
+                            // .then((commits_res) => commits_res.json())
+                            // .then((commits_data) => {
+                            document.getElementById(`${repo.name}_${i+1}`).innerHTML += `
+                            
+                            <p>Pull request <a href='${pull[0].html_url}'><span>here</span></a></p>`
+                            // })
+                        }
+                    }
+                    )
+
+                    
+                }   
+            }
     })
 }
-
-
-// fetch(`https://api.github.com/users/${usrName}/repos`, options) //maybe make it possible to enter your own github username?
-//     .then((res) => res.json())
-//     .then((data) => {
-//         forkedRepos = data.filter(repo => repo.fork);
-//         projectRepos = forkedRepos.filter(repo => repo.name.startsWith('project'));
-//         // console.log(data)
-//         // console.log(forkedRepos)
-//         console.log(projectRepos);
-
-//         usrName = projectRepos[0].owner.login;
-
-//         // for the user presentation
-//         usr.innerHTML = `
-//             <h2>
-//                 ::${usrName} 
-//             </h2>
-//             <!-- <img class='avatar_url_pic' src='${projectRepos[0].owner.avatar_url}'/> -->
-//         `
-//         // console.log(projectRepos[0])
-
-
-//         // getPullRequests(projectRepos);
-//         projectRepos.forEach((repo) => {
-//             main.innerHTML += `
-//                 <section id='${repo.name}_${projectCounter}' class='project'>
-//                     <h3>${repo.name}</h3>
-//                     <p>Name of your default branch for each repo</p>
-//                     <a href='${repo.html_url}'>Link to repo</a>
-//                 </section>
-//             `;
-//             projectCounter++
-//             projectNames.push(`${repo.name}`)
-//         })
-
-
-//         projectRepos.forEach(repo => {
-//             // console.log(repo)
-//             fetch('https://api.github.com/repos/technigo/' + repo.name + '/pulls?per_page=100')
-//             .then(res => res.json())
-//             .then(data => {
-//                 // console.log(data);
-//                 data.forEach(pull => {
-//                     let p_login = pull.user.login;
-//                     let r_login = repo.owner.login;
-//                     if (p_login === r_login || p_login === "michaelchangdk" && pull.id === 856976386) { //the id is for the pull request that is done from michael's account
-//                         pullRequests.push(pull);
-//                         fetch(`${pull.commits_url}`)
-//                         .then(response => response.json())
-//                         .then(json_data => {
-//                             let numberOfCommits = json_data.length
-//                             document.getElementById(`${repo.name}_${PRCounter}`).innerHTML += `
-//                             <p>Most recent update (push) for each repo</p>
-//                             <p>The repo has ${numberOfCommits} commits</p>
-//                             <a href='${pull.html_url}'>Link to pull request</a>
-//                         `;
-//                         }) 
-                        
-//                     };
-//                 })
-//             PRCounter++;
-//             });
-//         });
-        
-//         console.log(pullRequests)
-    
-//         // console.log(projectNames)
-//     }
-        
-// );
-
-// const getPullRequests = async (repos) => {
-//     //Get all the PRs for each project.
-//     await repos.forEach(repo => {
-//         // console.log(repo)
-//         fetch('https://api.github.com/repos/technigo/' + repo.name + '/pulls?per_page=100')
-//         .then(res => res.json())
-//         .then(data => {
-//             // console.log(data);
-//             data.forEach(pull => {
-//                 let p_login = pull.user.login;
-//                 let r_login = repo.owner.login;
-//                 if (p_login === r_login || p_login === "michaelchangdk" && pull.id === 856976386) { //the id is for the pull request that is done from michael's account
-//                     pullRequests.push(pull);
-//                     fetch(`${pull.commits_url}`)
-//                     .then(response => response.json())
-//                     .then(json_data => {
-//                         let numberOfCommits = json_data.length
-//                         document.getElementById(`${repo.name}_${PRCounter}`).innerHTML += `
-//                         <p>Most recent update (push) for each repo</p>
-//                         <p>The repo has ${numberOfCommits} commits</p>
-//                         <a href='${pull.html_url}'>Link to pull request</a>
-//                     `;
-//                     }) 
-                    
-//                 };
-//             })
-//         PRCounter++;
-//         });
-//     });
-    
-//     console.log(pullRequests)
-
-//     // console.log(projectNames)
-// }    
-                //TODO
-                //1. Find only the PR that you made by comparing pull.user.login
-                // with repo.owner.login
-                //2. Now you're able to get the commits for each repo by using
-                // the commits_url as an argument to call another function
-                //3. You can also get the comments for each PR by calling
-                // another function with the review_comments_url as argument
-    
-
-
-
-    // "MDQ6VXNlcjg5OTMwMzM2"
-    // MDQ6VXNlcjg5OTMwMzM2
 
 fetchData()
