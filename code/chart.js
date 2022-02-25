25cff7ae1eea7e770f2337bd77ce4d06e4898cba
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-02-25 13:04:49 +0100

    css styling done

--- code/chart.js
+++ code/chart.js
@@ -1,33 +1,36 @@
+//DOM-selector for the canvas 👇
 const ctx = document.getElementById("chart").getContext("2d");
 
-//"Draw" the chart here
+//"Draw" the chart here 👇
+const fetchChart = (repos) => {
+  const labels = ["Completed Projects", "Remaining Projects"];
 
-const config = {
-  type: "doughnut",
-
-  data: {
-    labels: ["Finished projects", "Projects left"],
-
+  const data = {
+    labels: labels,
     datasets: [
       {
-        label: "My Projects",
-        data: [6, 19 - 6],
-        backgroundColor: ["rgb(132, 207, 189)", "rgb(139, 247, 189)"],
-        hoverOffset: 2,
+        label: "My First dataset",
+        backgroundColor: ["rgb(57, 91, 100)", "rgb(148, 180, 159)"],
+        borderColor: "#f7e9e7",
+        data: [repos, 19 - repos],
       },
     ],
-  },
-  options: {
-    plugins: {
-      legend: {
-        labels: {
-          font: {
-            size: 25,
+  };
+
+  const config = {
+    type: "doughnut",
+    data: data,
+    options: {
+      plugins: {
+        legend: {
+          labels: {
+            // This more specific font property overrides the global property
+            font: {},
           },
         },
       },
     },
-  },
-};
+  };
 
-const myChart = new Chart(ctx, config);
+  const myChart = new Chart(document.getElementById("chart"), config);
+};
--- code/script.js
+++ code/script.js
@@ -18,11 +18,12 @@ const user = () => {
     .then((data) => {
       console.log("data", data);
       userProfile.innerHTML += `
-      <h1> Technigo GitHub Tracker <br> </h1> 
-      <img src = ${data.avatar_url}>
-      <h2 class = "user-name" id= "userName"> ${data.name} </h2> 
+      <h1> GitHub Tracker <br> </h1> 
+      <img class= "user" src = ${data.avatar_url}>
+      <h2 class = "user-name" id= "userName"> ${data.name}  </h2> 
+      <h4> ${data.bio} </h4>  <br>
+      <h4>  Follow <a href="${data.html_url}">   <i class="fa fa-github" aria-hidden="true"></i> </a>   </h4> 
      
-      <button class= "button" id = "button"><a href="${data.html_url}</a>"> Follow </button>
      `;
     });
 };
@@ -39,37 +40,41 @@ const userRepo = () => {
       );
       filteredRepos.forEach((repo) => {
         const date = new Date(repo.pushed_at).toDateString();
-        projects.innerHTML += `
-        <a class = "repo1" id= "repo"href="${repo.html_url}"><b>${repo.name}</b></a>
-        <p class = "repo2" id= "repo"> <b>Default Branch:</b> ${repo.default_branch} </p>
-        <p  class = "repo3"id= "repo"> <b>Latest Push update:</b> ${date} </p>
-        <p class = "repo4" id="commit-${repo.name}"><b>Commits Done:</b> </p>
-    
+        projects.innerHTML += `<div class = "repos" >
+        <h3 class = "repo"><b>${repo.name}</b><a href="${repo.html_url}">  <i class="fa fa-github" aria-hidden="true"></i></a> </h3> 
+        <p class = "repo">  (${repo.default_branch}) </p>
+        <p class = "repo">  <b>Latest update:</b> Pushed on ${date} </p>
+        <p class = "repo"id="commit-${repo.name}""><b>Commits Done:</b> </p>
+        </div>
         `;
       });
+      fetchChart(filteredRepos.length);
       console.log("filteredRepos", filteredRepos);
       pullRequests(filteredRepos);
     });
-};
-
-const pullRequests = (repos) => {
-  repos.forEach((repo) => {
-    console.log("repo", repo);
-    fetch(
-      `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`,
-      options
-    ) // to filter pull requests
-      .then((res) => res.json())
-      .then((data) => {
-        const commits = document.getElementById(`commit-${repo.name}`);
-        console.log("data", data);
-        const myPullRequest = data.find(
-          (pull) => pull.user.login === repo.owner.login
-        ); // pullrequests fetches the entire pullrequest specfic to the project,  filtering out pull requests made by me.
-        console.log("myPullRequest", myPullRequest);
-        fetchCommits(myPullRequest.commits_url, repo.name);
-      });
-  });
+  const pullRequests = (repos) => {
+    repos.forEach((repo) => {
+      console.log("repo", repo);
+      fetch(
+        `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`,
+        options
+      ) // to filter pull requests
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data", data);
+          const myPullRequest = data.find(
+            (pull) => pull.user.login === repo.owner.login
+          ); // pullrequests fetches the entire pullrequest specfic to the project,  filtering out pull requests made by me.
+          console.log("myPullRequest", myPullRequest);
+          if (myPullRequest) {
+            fetchCommits(myPullRequest.commits_url, repo.name);
+          } else {
+            document.getElementById(`commit-${repo.name}`).innerHTML =
+              "<b>Commits Done: </b> 0 (Pull request unavailable/Group project) ";
+          }
+        });
+    });
+  };
 };
 const fetchCommits = (commitsURL, reponame) => {
   fetch(commitsURL, options)
