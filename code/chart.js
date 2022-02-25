5f1c69e491c935048df867b39623997305fe2e0d
Åsa Sieurin <asa.sieurin@gmail.com>
2022-02-25 02:09:00 +0100

    layout & chart

--- code/chart.js
+++ code/chart.js
@@ -1,35 +1,29 @@
 //DOM-selector for the canvas 👇
 const ctx = document.getElementById('chart').getContext('2d')
 
-
-// drawing the chart here
+const myChart=((projects) => {
 const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'Another month',
-  ];
+'completed projects',
+'not yet completed projects',
+];
 
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: ['rgb(255, 99, 132)', 'rgb(255, 99, 132)'] // array with different bg-colors
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-  };
+const data = {
+labels: labels,
+datasets: [{
+	label: 'My First dataset',
+	backgroundColor: ['#2FC5EE', '#FF1E00'], // array with different bg-colors
+	borderColor: '#FEFBF3',
+	data: [projects, 19-projects],
+}]
+};
 
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
+const config = {
+	type: 'doughnut',
+  data: data,
+  options: {}
+};
 
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+new Chart(document.getElementById('chart'), config)
+
+});
+
--- code/script.js
+++ code/script.js
@@ -1,32 +1,35 @@
+// DOM-selectors
 const chart = document.getElementById('chart')
 const projects = document.getElementById('projects')
 const avatar = document.getElementById('avatar')
 const API_REPOS = 'https://api.github.com/users/sieurin/repos'
 const API_USER = 'https://api.github.com/users/sieurin'
-
 const API_TOKEN = TOKEN || process.env.API_KEY;
 
-console.log(TOKEN)
-
+// my token
 const options = {
 	method: 'GET',
   headers: {
-		Authorization: `token ${API_TOKEN}`  // my token
+		Authorization: `token ${API_TOKEN}`  
   }
 }
 
-fetch(API_USER, options) // options object is passed as 2nd argument to fetch() function.
+fetch(API_USER, options) 
 	.then(res => res.json())
 	
 	.then((json) => {
-		console.log(json)
 		avatar.innerHTML += `
-		<img scr="${json.avatar_url}" alt="avatar">
-		<h2>${json.login}</h2>
+		<div class="header">
+			<img src="${json.avatar_url}" alt= "avatar">
+			<p class="user-name">${json.login}</p>
+		</div>
+		<p class="bio"><span>HI!</span> ${json.bio}</p>
 		`
 		})
-	
-fetch(API_REPOS, options) // options object is passed as 2nd argument to fetch() function.
+
+// fetch all information for each project
+
+fetch(API_REPOS, options) 
 	.then(res => res.json())
 	
 	.then((json) => {
@@ -34,44 +37,63 @@ fetch(API_REPOS, options) // options object is passed as 2nd argument to fetch()
 		return project.fork === true && project.name.startsWith('project'); 
 		})
 
-		// fetch all information for each project
+		myChart(filterProjects.length);
+
 		filterProjects.forEach((project) => {
+		
+		//toggle function that add the class active and 
+		function toggle() {
+			this.classList.toggle("active")
+		}
 
 			// print out the project name, branch, latest push, url
 			projects.innerHTML += `
-			<div class ="projects" id=${project.name}>
-				<h1>${project.name}</h1>
-				<h2>${project.default_branch}</h2>
-				<h3>${project.pushed_at}</h3>
-				<h4>${project.git_url}</h4>
+			<div class="projects" id=${project.name}>
+				<div class="project-info">
+					<div class="projects-left">
+						<p class="pushed-at">last push <br>${new Date(project.pushed_at).toLocaleDateString('en-GB', options)}</p>
+					</div>
+					<p class="project-name">${project.name}</p>
+					<div class="projects-right">
+						<p class="branch-name">${project.default_branch} branch</p>
+						<a class="project-url" href="${project.html_url}" alt="link to git" target="_blank">link to project</a>
+					</div>
+					<div class="test"> <h1>TESTAR LITE</h1> </div>
+				</div>
 			<div>
 			`
 
-			// fetch all the pull requests and print it out
+		// Calls a function which will be executed when the element, project section, is clicked.
+		document.getElementById(`${project.name}`).onclick = toggle
+
+			// fetch all the pull requests and print them out
 			const reponame = project.name
 			const API_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls?per_page=100`
-			fetch(API_PR, options) // options object is passed as 2nd argument to fetch() function.
+			fetch(API_PR, options) 
 			.then(res => res.json())
 			
 			.then((json) => {	
 				const filteredPR = json.filter((PR) => {
 				return PR.user.login === 'sieurin'
-				// print out pull requests
 				})
+			
+			console.log(filteredPR)
+			
+			const singleProject = document.getElementById(project.name)
+			singleProject.innerHTML += `<p class="pull-requests">Pull requests: ${filteredPR.length}</p>`
 
-			// fetch all the commits and print it out
+			// fetch all the commits and print them out
 			const API_COMMITS = `https://api.github.com/repos/sieurin/${reponame}/commits`
 			fetch(API_COMMITS, options)
 			.then(res => res.json())
 			
 			.then((json) => {	
-				const singleProject = document.getElementById(project.name)
-				singleProject.innerHTML += `<h4>Commits made for this project: ${json.length}</h4>`
+				//const singleProject = document.getElementById(project.name)
+				singleProject.innerHTML += `<p class="commits">Commits: ${json.length}</p>`
 				})
-				console.log(json)
 			})
 		})		
-			console.log(filterProjects)
+		console.log(filterProjects)
 
 	}) 
 
