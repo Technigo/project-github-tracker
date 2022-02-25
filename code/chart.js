3df9251815c454cfe3e429fef37a89153a693f62
anki palviainen <anki.palviainen@gmail.com>
2022-02-25 13:23:18 +0200

    token

--- code/chart.js
+++ code/chart.js
@@ -1,34 +1,29 @@
 //DOM-selector for the canvas 👇
 const ctx = document.getElementById('chart').getContext('2d')
+const technigoProjects = 19
+
 
 //"Draw" the chart here 👇
-  const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
-
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'black',
-      borderColor: 'grey',
-      data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-  };
-
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-  
-  const myChart = new Chart(
-    document.getElementById('chart'),
-    config
-  );
-
+const myChart = (repos) => {
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+      labels: ['Projects I will do during bootcamp', 'Projects I have done during this bootcamp'],
+      datasets: [{
+          label: '# of Votes',
+          data: [technigoProjects - repos.length,
+            repos.length,],
+          backgroundColor: [
+            'rgba(46, 204, 113, 0.2)',
+            'rgba(34,139,34, 0.5)'
+          ],
+          borderColor: [
+              'rgba(38, 166, 91, 1)',
+              'rgba(0,100,0, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+ 
+})
+};
--- code/script.js
+++ code/script.js
@@ -1,12 +1,22 @@
 let USER = 'qwer-ctrl'
 
+const USER_URL = `https://api.github.com/users/${USER}`
+const REPOS_URL = `https://api.github.com/users/${USER}/repos`
 
+const TOKEN = API_TOKEN 
+
+const options = {
+    method: "GET", 
+    headers: {
+        Authorization: `token ${TOKEN}`
+    }
+}
 
 const profile = document.getElementById('profile-container')
 const projects = document.getElementById('projects')
 
 const fetchProfile = () => {
-fetch(USER_URL)
+fetch(USER_URL, options)
     .then((response) => response.json())
     .then((data) => {
      profile.innerHTML += `
@@ -16,10 +26,10 @@ fetch(USER_URL)
             <img src="${data.avatar_url}" alt="Avatar of ${data.login}">
             </a>
             </div>
-            <h1 class="username">${data.login}<h1>
+            <p class="username">${data.login}<p>
         </div>
         <div>
-        <p>Public repositories: ${data.public_repos}</p>
+        <p class="public-repositories">Public repositories: ${data.public_repos}</p>
         </div>
         `
         })
@@ -27,7 +37,7 @@ fetch(USER_URL)
 fetchProfile()
 
 const fetchRepos = () => {
-fetch(REPOS_URL)
+fetch(REPOS_URL, options)
     .then((response) => response.json())
     .then((data) => {
      const technigoRepos = data.filter(
@@ -35,7 +45,7 @@ fetch(REPOS_URL)
         technigoRepos.forEach((repo) => {
         projects.innerHTML += `
             <div class="projects-card" id="${repo.id}">
-                <h1><a href="${repo.html_url}"><b>${repo.name}</b></a></h1>
+                <h1><a href="${repo.html_url}">${repo.name}</a></h1>
                 <p>Update: ${new Date(repo.pushed_at).toDateString()}</p>
                 <p>Branch: ${repo.default_branch}</p>
                 <p id="commit_${repo.name}">Commits: </p>
@@ -43,14 +53,15 @@ fetch(REPOS_URL)
                 </div>
                 `
             })
+            myChart(technigoRepos)
  fetchPullRequests(technigoRepos)
+ 
         })
 }
 
-fetchRepos()
-    /*const fetchPullRequests = (repositories) => {
+    const fetchPullRequests = (repositories) => {
      repositories.forEach((repo) => {
-     fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
+     fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`, options)
         .then((response) => response.json())
         .then((data) => {
         const pullRequest = data.find((pull) => pull.user.login === repo.owner.login)
@@ -58,18 +69,21 @@ fetchRepos()
                 fetchCommits(pullRequest.commits_url, repo.name)
             } else {
                 document.getElementById(`commit_${repo.name}`)
-                .innerHTML = 'No pull reguest';
+                .innerHTML = 'No pullrequest available';
             }
                 })
         })
     }
 
         const fetchCommits = (commitsUrl, repoName) => {
-            fetch(commitsUrl)
+            fetch(commitsUrl, options)
                 .then((response) => response.json())
                 .then((data) => {
                     document.getElementById(`commit_${repoName}`).innerHTML += data.length
                 })
         }
 
-fetchRepos()*/
+        //I used this API for my project: `https://api.github.com/repos/${username}/${repo.name}/commits`
+
+
+fetchRepos()
