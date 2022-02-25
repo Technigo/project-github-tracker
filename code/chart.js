34e6d4ab43a1a00600c5c6de996f671a325d58d3
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-02-25 13:03:55 +0100

    some chart styling

--- code/chart.js
+++ code/chart.js
@@ -12,22 +12,20 @@ const labels = [
   const data = {
     labels: labels,
     datasets: [{
-      label: 'My First dataset',
+      data: [projects, 19-projects],
       backgroundColor: ['rgb(255, 99, 132)','rgb(255, 69, 0)'], 
       borderColor: ['rgb(255, 99, 132)','rgb(255, 69, 0)'],
-      data: [0, 10, 5, 2, 20, 30, 45],
     }]
   };
 
   const config = {
     type: 'doughnut',
     data: data,
-    options: {}
+//     // options: {}
   };
-  const myChart = new Chart(
-    document.getElementById('chart'),
-    config
-  );
+
+  new Chart(document.getElementById('chart'),config);
+
 }
  
 
