715e9285d0c0ba174bba44b97978efd0a9cd59cf
eyahya-khan <eyahyakhan@gmail.com>
2022-02-26 19:04:53 +0100

    final update

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ const userProfile = () => {
       </div>
       <div class="profileimg"><img src="${data.avatar_url}"/></div>
       </div>`
+      
     })
 }
 userProfile()
@@ -49,7 +50,6 @@ const getPullRequests = (repos) => {
 
   projectsContainer.innerHTML = '';
   repos.forEach(repo => {
-    //console.log(repo.name)
 
     fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`, Auth) 
       .then(res => res.json())
@@ -58,45 +58,30 @@ const getPullRequests = (repos) => {
         const myPullRequest = data.filter((pull) => { return pull.user.login === repo.owner.login })
  
         const commitUrl = myPullRequest[0].commits_url
-        //display commits info
-        const displayInfo = (data) => {
-          //console.log(data)
+
+        //modalDisplay(commitUrl)
+
+        fetch(commitUrl)
+        .then(res => res.json())
+        .then(data => {
+
+          const numberCommit = data.length
+
           //added function so the DOM do not self close tags
           let boxHtml = `<div class="box-repo ${repo.name}">`; 
           boxHtml += `<h3>${repo.name}</h3>`
-          boxHtml += `<p id="commit-${repo.name}">Number of commits: ${data.length}</p>`
+          boxHtml += `<p id="commit-${repo.name}">Number of commits: ${numberCommit}</p>`
           boxHtml += `<p>Created at: ${new Date(repo.created_at).toDateString()}</p>`
           boxHtml += `<p>Last update: ${new Date(repo.pushed_at).toDateString()}</p>`
           boxHtml += `<p>Default branch: ${repo.default_branch}</p>`
+          boxHtml += `<p>First comments: ${data[0].commit.message}</p>`
+          boxHtml += `<p>Last comments: ${data[data.length-1].commit.message}</p>`
           boxHtml += `<p><a href="${repo.html_url}" target="_blank">Go to repo</a></p>`
           boxHtml += '</div>'
           projectsContainer.innerHTML += boxHtml; //closing the div tag
           ldsripple.style.display = 'none' //loading icon
-        }
-        getCommits(commitUrl, displayInfo)
+          
+        })
       })
-  })
-}
-
-//get my commits
-const getCommits = (url, callbackFunction) => {
-  fetch(url, Auth)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data.length)
-      callbackFunction(data)
     })
 }
-
-
-//eventlistener on form-submit
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let searchInput = searchField.value
-  let searchArray = []
-  
-  searchInput = searchArray.push(searchInput)
-
-console.log(typeof(searchArray))
-  //getPullRequests(searchArray);
-})
