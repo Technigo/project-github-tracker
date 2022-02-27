878caa9a601532281ee0eaac79727ff8adf9e177
Jessica Sandler <jessica.sandler@hotmail.com>
2022-02-27 19:47:34 +0100

    Added pull requests for pair and group projects

--- code/script.js
+++ code/script.js
@@ -29,14 +29,17 @@ const getCommits = (pullRequestCommitsUrl, reponame) => {
   .then(res => res.json())
   .then(commits => {
 
-    let repoDiv = document.getElementById(reponame);
+    let repoDiv = document.getElementById(`${reponame}-div`);
+    console.log(repoDiv);
+    let repoDivContent = document.getElementById(`${reponame}-div-content`);
+    console.log(repoDivContent);
 
     let commitParagraph = document.createElement('p');
     commitParagraph.innerHTML = `Number of commits: ${commits.length}`;
-    repoDiv.appendChild(commitParagraph);
+    repoDivContent.appendChild(commitParagraph);
 
     let commitMessageList = document.createElement('ol');
-    // commitMessageList.className = 'message-list';  
+    commitMessageList.className = 'message-list';  
   
     commits.forEach(commit => {
       let commitMessage = document.createElement('li');
@@ -47,57 +50,54 @@ const getCommits = (pullRequestCommitsUrl, reponame) => {
     let showCommitsButton = document.createElement('button');
     showCommitsButton.className = 'commits-button';
     showCommitsButton.innerHTML = 'Show commits';
-    repoDiv.appendChild(showCommitsButton);
+    repoDivContent.appendChild(showCommitsButton);
     
-    const showCommitMessageList = () => repoDiv.innerHTML = commitMessageList.outerHTML;
+    const hideCommits = () => {
+      userSection.style.backgroundColor = 'brown';
+    }
+
+    const showCommitMessageList = () => {
+      repoDivContent.innerHTML = commitMessageList.outerHTML;
+      let knapp = document.createElement('button');
+      knapp.innerHTML = 'en knapp';
+      repoDivContent.appendChild(knapp);
+      knapp.addEventListener('click', hideCommits);
+    }  
+
     showCommitsButton.addEventListener('click', showCommitMessageList);
+    
   })
 }
-
-// const getReviewComment = (myPullRequestUrl, reponame) => {
-//   fetch(`${myPullRequestUrl}/reviews`)
-//   .then(res => res.json())
-//   .then(json => {
-
-//     let repoDiv = document.getElementById(reponame);
-//     let reviewCommentParagraph = document.createElement('p');
-
-//     reviewCommentParagraph.className = 'review-comment';
-
-//     repoDiv.appendChild(reviewCommentParagraph);
-
-//     json.forEach(item => {
-//       reviewCommentParagraph.innerHTML = item.body;
-//     })
-//   })
-// }
 
 const getPullRequests = (repos) => {
   repos.forEach(repo => {
     
-    fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls`, options)
+    fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`, options)
     .then(res => res.json())
     .then(pullRequests => {
 
+      // How can I get the pull requests for the repos that I am not the owner of without hardcoding like this?
+      const firstTitleToSearchFor = 'Week 4 project - Sofie Pellegrini & Jessica Sandler';
+      const secondTitleToSearchFor = 'Week 6 project : Jessica - Maurii - Nadia - Rijad - Terese';
+
       const myPullRequests = pullRequests
-      .filter(pullRequest => pullRequest.user.login === username);
+      .filter(pullRequest => pullRequest.user.login === username || pullRequest.title === firstTitleToSearchFor || pullRequest.title === secondTitleToSearchFor)
 
       //If the length is 1 I have done a pull request
       //If the length is 0 it is an ongoing project or a group project where I haven't done the pull request
       if (myPullRequests.length === 0) {
 
-        let repoDiv = document.getElementById(repo.name);
+        let repoDivContent = document.getElementById(`${repo.name}-div-content`);
         let commitParagraph = document.createElement('p')
 
-        commitParagraph.innerHTML = `Either ongoing or group/pair project where I didn't make the pull request`;
+        commitParagraph.innerHTML = "No pull request found";
         commitParagraph.className = "no-pullrequest";
         
-        repoDiv.appendChild(commitParagraph);
+        repoDivContent.appendChild(commitParagraph);
       }
 
       myPullRequests.forEach(myPullRequest => {
         getCommits(myPullRequest.commits_url, repo.name);
-        // getReviewComment(myPullRequest.url, repo.name);
       })
     })
   })
@@ -115,6 +115,8 @@ const fetchRepos = () => {
   .then(repos => {
     
     const technigoRepos = getTechnigoRepos(repos);
+
+    console.log(technigoRepos)
     
     getPullRequests(technigoRepos);
 
@@ -132,16 +134,16 @@ const fetchRepos = () => {
       const mostRecentPush = new Date(repo.pushed_at).toLocaleDateString('en-GB');
 
       repoContainer.innerHTML += `
-      <div id=${repo.name} class="repo-div">
-        <h4><a href="${repo.html_url}" target="_blank">${repo.name}</a></h4>
-        <p>Most recent push: ${mostRecentPush}</p>
-        <p>Default branch: ${repo.default_branch}</p>
-      </div>
+        <div id=${repo.name}-div class="repo-div">
+          <div id=${repo.name}-div-content class="repo-div-content">
+            <h4><a href="${repo.html_url}" target="_blank">${repo.name}</a></h4>
+            <p>Most recent push: ${mostRecentPush}</p>
+            <p>Default branch: ${repo.default_branch}</p>
+          </div>
+        </div>
       `
-      
-      
+    
     })
-
   });
 }
 
