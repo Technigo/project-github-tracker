8e6cdff68d6a042a8f88734d16ca871c15355940
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-02-28 00:40:14 +0100

    new code to all files

--- code/chart.js
+++ code/chart.js
@@ -2,3 +2,24 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
+
+const drawChart = (amount) => {
+  const config = {
+    type: "doughnut",
+    data: {
+      labels: ["Completed projects", "Projects left to build"],
+      datasets: [{
+        label: "My Technigo projects",
+        data: [amount, 19 - amount],
+        backgroundColor: ["rgb(7,145,96)", "rgba(90, 112, 55, 0.1)"],
+        borderColor: ["rgb(0,0,0)"],
+        borderWidth: 0.3,
+        hoverOffset: 4,
+        },
+        ],
+    },
+  };
+    
+const projectsChart = new Chart(ctx, config);
+}
+    
--- code/script.js
+++ code/script.js
@@ -2,92 +2,84 @@
 
 const projectsContainer = document.getElementById('projects')
 const userContainer = document.getElementById('user-container')
+const userInfo = document.getElementById('user-info')
+const userImg = document.getElementById('user-img')
 
 //APIS TO FETCH FROM
 const username = 'Thereese'
 let reponame
 const API_URL = `https://api.github.com/users/${username}/repos`
 const API_URL_PR = `https://api.github.com/repos/Technigo/${reponame}/pulls`
-//let reponame
+
 
 const options = {
-    method: 'GET',
+  method: 'GET',
     headers: {
-          Authorization: 'API_TOKEN'
-          
-        }
+      Authorization: 'API_TOKEN'
     }
-    console.log(API_TOKEN)
+}
+    
 
+//FETCH USERNAME AND PROFILEPIC 
 const getUser = () => {
-    fetch (`https://api.github.com/users/${username}`, options)
+  fetch (`https://api.github.com/users/${username}`, options)
     .then((res) => res.json())
     .then((data) => {
-        userContainer.innerHTML=
-        `<h1>${data.login}</h1>
-        <img class="user-image" src=${data.avatar_url}/>`
-
-
-  console.log(data)
+      userImg.innerHTML=
+      `<img class="user-image" src=${data.avatar_url}/>`
+      userInfo.innerHTML=
+      `<h1>${data.name}</h1>
+       <h3> | ${data.login}</h3>`
     })
 }
 
-//getUser()
 
-
-//fetch all repos,filter technigo
+//FETCH REPOS AND FILTER TECHNIGOPROJECTS. DATA TO PROJECTCONTAINER.
 const getRepos = () => {
-    fetch (API_URL, options)
+  fetch (API_URL, options)
     .then((res) => res.json())
     .then((data) => {
-     const filterTechnigoProjects = data.filter((repo) => repo.fork && repo.name.startsWith("project"))
-         filterTechnigoProjects.forEach(repo => {
-            console.log(repo.name)
-            let projectID = repo.id
-            projectsContainer.innerHTML+=
-            `<div class="repocard" id=${projectID}>
-            <a href=${data.html_url}><h1> ${repo.name}</h1></a>
-            <p> Default branch: ${repo.default_branch}</p>
-            <p> Latest push: ${new Date(repo.pushed_at).toDateString()}</p>
-            </div>
-            `
+    const filterTechnigoProjects = data.filter((repo) => repo.fork && repo.name.startsWith("project"))
+      filterTechnigoProjects.forEach(repo => {
+        let projectID = repo.id
+        projectsContainer.innerHTML+=
+        `<div class="repocard" id=${projectID}>
+          <a href=${data.html_url}><h3> ${repo.name}</h3></a>
+          <p> Default branch: ${repo.default_branch}</p>
+          <p> Latest push: ${new Date(repo.pushed_at).toDateString()}</p>
+          <p id="commit-${repo.name}">Commits:</p>
+        </div>`
            
          })
-         console.log(filterTechnigoProjects, "här")
          getPullRequests(filterTechnigoProjects)
-        })}
+         drawChart(filterTechnigoProjects.length)
+    })
+}
 
 getRepos()
-
+//FETCH PULLREQUESTS, SORT AND RETURN DATA OR COMMENT
 const getPullRequests = (filterTechnigoProjects) => {
-    filterTechnigoProjects.forEach(repo => {
-        fetch (`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`, options)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data, 'pr')
+  filterTechnigoProjects.forEach(repo => {
+    fetch (`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`, options)
+      .then((res) => res.json())
+      .then((data) => {
         const filterMyPull = data.find((pull) => pull.user.login === repo.owner.login)
         if (filterMyPull) {
-            getCommits(filterMyPull.commits_url, repo.name)
+          getCommits(filterMyPull.commits_url, repo.name)
         } else {
-          projectsContainer.innerHTML += 
-          
-           `<p>No PR made</p>`
-      
-           
+          document.getElementById(`commit-${repo.name}`).innerHTML =
+          '<p>No pull requests made</p>'
         }
-
-        
-        console.log(filterMyPull)
-    })
-    })
+    }) 
+  })
 }
 
 const getCommits = (URL, repoName) => {
-    fetch(URL, options)
+  fetch(URL, options)
     .then ((res) => res.json())
     .then (data => {
-       projectsContainer.innerHTML +=
-       `<p>Number of commits: ${data.length}</p>`
+      document.getElementById(`commit-${repoName}`).innerHTML += data.length;
     })
 }
-    getUser()
+getUser()
+    
