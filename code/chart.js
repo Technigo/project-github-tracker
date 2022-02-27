7c204b1963f25b1cf63033f97ef11064b0246466
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-02-27 22:48:05 +0100

    all done

--- code/chart.js
+++ code/chart.js
@@ -2,39 +2,20 @@
 const ctx = document.getElementById('chart').getContext('2d')
 
 //"Draw" the chart here 👇
-const drawChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
+const drawChart = (amount) => {
+  const configchart = {
+    type: 'doughnut',
+    data: {
+      label: ['Completed', 'Not Completed'],
+      datasets: [
+        {
+          label: 'Total project at Technigo',
+          data: [amount, 19 - amount],
+          backgroundColor: ['#77E327', '#E33527'],
+          hoverOffset: 12,
+        },
+      ],
     },
-  },
-})
+  }
+  const myProjects = new Chart(ctx, configchart)
+}
--- code/script.js
+++ code/script.js
@@ -5,8 +5,8 @@ const API_REPO = `https://api.github.com/users/${username}/repos`
 const projects = document.getElementById('projects')
 const personData = document.getElementById('personData')
 
+//token
 const api_Token = apiToken || process.API_KEY
-//console.log(apiToken)
 
 const options = {
   method: 'GET',
@@ -15,78 +15,87 @@ const options = {
   },
 }
 
-//USER INFO
+//Prints out the user info in the HTML
 
 const getUserData = () => {
   fetch(API_USER)
     .then((res) => res.json())
     .then((user) => {
-      console.log(user)
       personData.innerHTML = `
   <div class="info">
   <img class="img" src="${user.avatar_url}">
-  <h2> ${user.name}</h2>
-  <h2>${user.location}</h2>
-  <h2>${user.bio}</h2>
+  <h2 class="userInfo"> ${user.name}</h2>
+  <h2 class="userInfo">${user.location}</h2>
+  <h2 class="userInfo">${user.bio}</h2>
   </div>`
     })
 }
 getUserData()
 
 // Function to fetch my repositories
+
 const getRepos = () => {
   fetch(API_REPO, options)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
-      // Filter to get only my forked repos from Technigo
-      const filtered = data.filter(
+      // This function filters out my projects from technigo
+
+      const repos = data.filter(
         (repo) => repo.fork && repo.name.startsWith('project-'),
       )
 
-      filtered.forEach((repo) => {
-        // Creating unique ID for each forked repo
+      repos.forEach((repo) => {
+        //Her we create a unique ID for each of my forked repos and print them in the HTML
         let projectID = repo.id
 
         projects.innerHTML += `
               <div class="repoInfo" id="${projectID}">
               <img src="github.png" class="repoimg" alt="logo" width="25px" />
+              <p class="cardInfo" id="commit-${repo.name}"></p>
               <p class="cardInfo">${new Date(repo.pushed_at).toDateString()}</p>
               <p class="cardInfo">Branch ${repo.default_branch}</p> 
               <p class="cardInfo"$> <a href="${
                 repo.html_url
               }" target="blank">Repository ${repo.name}</a></p>
+          <p class="cardInfo" id="commit-${repo.name}"></p>
+          <p class="cardInfo" id="pull-request-${repo.name}">
+      Pull requests:</p>
 
               </div>`
 
         // Invoking function to get the number of commits for the projects
         getCommits(repo, projectID)
       })
-      getPullRequests(filtered)
-      drawChart(filtered.length)
+      getPullRequest(repos)
+      drawChart(repos.length)
     })
 }
-
-const getPullRequests = (filtered) => {
-  filtered.forEach((repo) => {
+// This function
+const getPullRequest = (repos) => {
+  repos.forEach((repo) => {
     fetch(
-      `https://api.github.com/repos/technigo/${repo.name}/pulls?per_page=100`,
+      `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`,
       options,
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        //Filter out my pullrequests
         const pulls = data.find((pull) => pull.user.login === repo.owner.login)
+        const myPullRequests = data.filter((pullRequest) => {
+          return pullRequest.user.login === username
+        })
+        document.getElementById(
+          `pull-request-${repo.name}`,
+        ).innerHTML = `Pull Request: ${myPullRequests.length}`
       })
   })
 }
-
+//  Function to get commits and print them out
 const getCommits = (projects, projectID) => {
   const GIT_COMMIT_API = projects.commits_url.replace('{/sha}', '')
   fetch(GIT_COMMIT_API, options)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
       let numberOfCommits = data.length
       document.getElementById(projectID).innerHTML += `
       <p>Number of commits: ${numberOfCommits}</p>`
