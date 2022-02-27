6efe3fb8deb44c5c29447ceb06fbe3d621a3ed0e
TereseClaesson <terese.claesson@gmail.com>
2022-02-27 22:05:09 +0100

    Added styling and resposivness

--- code/script.js
+++ code/script.js
@@ -10,83 +10,77 @@ const API_URL_REPOS = `https://api.github.com/users/${username}/repos`;
 const userProfile = () => {
   fetch(API_PROFILE)
     .then((res) => res.json())
-    .then(profileData => {
+    .then((profileData) => {
       profileInfo.innerHTML += `
-        <h2>GitHub Tracker</h2>
         <img src = "${profileData.avatar_url}">
         <h3>${profileData.name}</h3>
         <h4> <a href = ${profileData.html_url}> ${profileData.login}</h4>
         `;
-    })
+    });
 };
 
 //Repos
-const repositories = () => { 
-fetch(API_URL_REPOS)
-  .then((res) => res.json())
-  .then((allRepos) => {
-
-    const forkedRepos = allRepos.filter((repo) => repo.fork && repo.name.startsWith("project-"));
-    forkedRepos.forEach((repo) => {
-      allProjects.innerHTML += `
+const repositories = () => {
+  fetch(API_URL_REPOS)
+    .then((res) => res.json())
+    .then((allRepos) => {
+      const forkedRepos = allRepos.filter(
+        (repo) => repo.fork && repo.name.startsWith("project-")
+      );
+      forkedRepos.forEach((repo) => {
+        allProjects.innerHTML += `
         <div>
         <h3> Project name: ${repo.name} </h3>
-        <p> Latest push: ${new Date(repo.pushed_at).toLocaleString('en-GB', { dateStyle:'short',})} </p>
+        <p> Latest push: ${new Date(repo.pushed_at).toLocaleString("en-GB", {
+          dateStyle: "short",
+        })} </p>
         <p> Default branch: ${repo.default_branch}</p>
         <a href = ${repo.html_url}>${repo.name}</a>
         <p id ="pull-${repo.name}"></p>
         <p id ="commit-${repo.name}">Commits:</p>
         </div>
-        `
-        commits(repo.commits_url, repo.name)
+        `;
+        commits(repo.commits_url, repo.name);
+      });
+      getPullRequest(forkedRepos);
+      drawChart(forkedRepos.length);
     });
-    getPullRequest(forkedRepos);
-    drawChart(forkedRepos.length);
-  });
-}
+};
 
 const getPullRequest = (forkedRepos) => {
   forkedRepos.forEach((repo) => {
-    
-    const PULL_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`
-    
+    const PULL_URL = `https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`;
+
     fetch(PULL_URL)
       .then((res) => res.json())
       .then((pullReqs) => {
-      let groupProject = true
+        let groupProject = true;
         pullReqs.forEach((pull) => {
           if (pull.user.login === username) {
-            groupProject = false
+            groupProject = false;
             document.getElementById(`pull-${repo.name}`).innerHTML = `
             <a href = ${pull.html_url}> Go to pull request </a>
-            `
+            `;
           }
         });
 
         if (groupProject === true) {
           document.getElementById(`pull-${repo.name}`).innerHTML = `
           <p> No pull request, group project </p>
-          `
+          `;
         }
       });
   });
 };
 
-/*const comments = (pullReq) => {
-  fetch(pullReq.review_comments_url)
-    .then((res) => res.json())
-    .then((commentrev) => {
-      console.log(commentrev);
-    });
-};*/
-
 const commits = (myCommits, repoName) => {
-  let commitUrl = myCommits.replace('{/sha}','')
+  let commitUrl = myCommits.replace("{/sha}", "");
   fetch(commitUrl)
     .then((res) => res.json())
     .then((commitNumber) => {
-     document.getElementById(`commit-${repoName}`).innerHTML += commitNumber.length;
-    })
+      document.getElementById(`commit-${repoName}`).innerHTML +=
+        commitNumber.length;
+    });
 };
 
 userProfile();
