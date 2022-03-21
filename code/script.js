ec945a394f8573f79af599a0869839ee13fd0fe5
Rawisara Saman <rawiisara@gmail.com>
2022-03-21 15:24:17 +0100

    [edited] autoformat code, removed unused variables and moved some variables into their scope.

--- code/script.js
+++ code/script.js
@@ -1,48 +1,46 @@
-const projectCard = document.getElementById('projectCard')
-const profile = document.getElementById('profile')
-const profileImage = document.getElementById('profileImage')
-
-// Github Profile
-  fetch('https://api.github.com/users/rawisou')
-   .then((res) => res.json()) 
-   .then((data) => {
+// Github profile
+fetch('https://api.github.com/users/rawisou')
+  .then((res) => res.json())
+  .then((data) => {
+    const profile = document.getElementById('profile')
     profile.innerHTML = `
     <h1>GitHub Tracker</h1>
      <h2>${data.name}</h2> 
      <img class="profile-image" id="profileImage" src="${data.avatar_url}" alt="Github Profile Image">
      <p><img class="ghlogo" src=./github.png><a href="${data.html_url}"> Github account: ${data.login}</a></p>
     `
-   })
+  })
   .catch((err) => console.log(err))
 
-  // Repos forked from Technigo
-  fetch('https://api.github.com/users/rawisou/repos') 
-   .then((res) => res.json()) 
-   .then((data) => {
-     const technigoRepos = data.filter(
-        (repos) => repos.fork && repos.full_name.includes('project-')
-      )
-      getPullRequests(technigoRepos)
-      drawChart(technigoRepos.length)
-    })
-   .catch((err) => console.log(err))
+// Repos forked from Technigo
+fetch('https://api.github.com/users/rawisou/repos')
+  .then((res) => res.json())
+  .then((data) => {
+    const technigoRepos = data.filter(
+      (repos) => repos.fork && repos.full_name.includes('project-')
+    )
+    getPullRequests(technigoRepos)
+    drawChart(technigoRepos.length)
+  })
+  .catch((err) => console.log(err))
 
+const getPullRequests = (repos) => {
+  const projectCard = document.getElementById('projectCard')
+  repos.forEach(repo => {
+    fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
+      .then((res) => res.json())
+      .then((pull) => {
+        const myPullRequests = pull.find(
+          (pull) => pull.user.login === repo.owner.login
+        )
 
-   const getPullRequests = (repos) => {
-    repos.forEach(repo => {
-     fetch(`https://api.github.com/repos/Technigo/${repo.name}/pulls?per_page=100`)
-     .then((res) => res.json())
-     .then((pull) => {
-        const myPullRequests = pull.find( 
-          (pull) => pull.user.login === repo.owner.login 
-         )
+        fetch(`https://api.github.com/repos/rawisou/${repo.name}/commits?per_page=100`)
+          .then((res) => res.json())
+          .then((myCommits) => {
+            let num = myCommits.length
 
-         fetch(`https://api.github.com/repos/rawisou/${repo.name}/commits?per_page=100`)
-         .then((res) => res.json())
-         .then((myCommits) => {
-         let num = myCommits.length
-  
-       if (myPullRequests != undefined) {projectCard.innerHTML += `
+            if (myPullRequests != undefined) {
+              projectCard.innerHTML += `
           <div class="card">
 
             <h3><a href="${repo.html_url}"><span>${repo.full_name.replace('rawisou/project-', '').replace('-', ' ')}</span></a></h3>
@@ -51,9 +49,9 @@ const profileImage = document.getElementById('profileImage')
             <p><span class ="bolded"> Number of commits:</span> ${num}</p>
             <p><a href="${myPullRequests.html_url}"><img class="pointing-right" src=./pointing-right.png> View the pull request</a></p>
           </div>   
-        ` 
-       } else if (repo.name === 'project-weather-app') {
-         projectCard.innerHTML += `
+        `
+            } else if (repo.name === 'project-weather-app') {
+              projectCard.innerHTML += `
          <div class="card">
            <h3><a href="${repo.html_url}"><span>${repo.full_name.replace('rawisou/project-', '').replace('-', ' ')}</span></a></h3>
            <p><span class ="bolded"> Default branch: </span> ${repo.default_branch}</p>
@@ -62,20 +60,28 @@ const profileImage = document.getElementById('profileImage')
            <p><a href="https://github.com/Technigo/project-weather-app/pull/215"><img class="pointing-right" src=./pointing-right.png> View the pull request</a></p>
          </div>   
          `
-        } else {
-          projectCard.innerHTML += `
+            } else if (repo.name === 'project-music-releases') {
+              projectCard.innerHTML += `
           <div class="card">
-            <h3><a href="${repo.html_url}"><span>${repo.full_name.replace('rawisou/project-', '').replace('-',' ')}</span></a></h3>
+            <h3><a href="${repo.html_url}"><span>${repo.full_name.replace('rawisou/project-', '').replace('-', ' ')}</span></a></h3>
+            <p><span class ="bolded"> Default branch: </span> ${repo.default_branch}</p>
+            <p><span class ="bolded"> Pushed at:</span> ${new Date(repo.pushed_at).toDateString()}</p>
+            <p><span class ="bolded"> Number of commits:</span> ${num}</p>
+            <p><a href="https://github.com/Technigo/project-music-releases/pull/185"><img class="pointing-right" src=./pointing-right.png> View the pull request</a></p>
+          </div>   
+          `
+            } else {
+              projectCard.innerHTML += `
+          <div class="card">
+            <h3><a href="${repo.html_url}"><span>${repo.full_name.replace('rawisou/project-', '').replace('-', ' ')}</span></a></h3>
             <p><span class ="bolded"> Default branch: </span> ${repo.default_branch}</p>
             <p><span class ="bolded"> Pushed at:</span> ${new Date(repo.pushed_at).toDateString()}</p>
             <p><span class ="bolded"> Number of commits:</span> ${num}</p>
             <p><span class ="bolded"> Pull Request:</span> No pull request </p>
           </div>   
           `
-             }
+            }
           })
-       })
-     })
-   }
-
-  
+      })
+  })
+}
