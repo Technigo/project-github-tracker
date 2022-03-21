e52f1b8c2da501b988577e14408a84edad743d01
Rawisara Saman <rawiisara@gmail.com>
2022-03-21 15:30:30 +0100

    fixed indentations using VS Code autoformat in all js files

--- code/chart.js
+++ code/chart.js
@@ -2,38 +2,37 @@
 const ctx = document.getElementById('myChart').getContext('2d')
 
 //"Draw" the chart here 👇
-const drawChart = (amount) =>
-{
-    const config = {
-      type: "bar",
-      data: {
-        labels: ["Completed projects", "Remaining projects"],
-        datasets: [
-          {
-            label: "Technigo projects",
-            data: [amount, 21-amount],
-            backgroundColor: [
-              "#c8cccf",
-              "#778088",
-            ],
-            barThickness: "30",
-          },
-        ],
-      },
-      options: {
-        scales: {
-          x: {
-            grid: {
-              display: false,
-            }
-          },
-          y: {
-            grid: {
-              display: false
-            }
-          },
-        }
+const drawChart = (amount) => {
+  const config = {
+    type: "bar",
+    data: {
+      labels: ["Completed projects", "Remaining projects"],
+      datasets: [
+        {
+          label: "Technigo projects",
+          data: [amount, 21 - amount],
+          backgroundColor: [
+            "#c8cccf",
+            "#778088",
+          ],
+          barThickness: "30",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          grid: {
+            display: false,
+          }
+        },
+        y: {
+          grid: {
+            display: false
+          }
+        },
       }
     }
- new Chart(ctx, config)
+  }
+  new Chart(ctx, config)
 }
